7f9237e028e65f7288c157c92dc7848a1b7d1f93
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2017-07-08 17:48:05 -0400

    First Commit
    Former-commit-id: 081dd6671bdcf1bbd71fefa8d1a13e705c74c13c [formerly db55ff6b3abdfd53b0d58654e3f50534a51c7129]
    Former-commit-id: 09175c5b5cebe397bdd9136ce0d202a57d02dc53

---
+++ Dynamixel_AX-12_UART/Debug/Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/subdir.mk
@@ -0,0 +1,82 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_f32.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_q15.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_q31.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_q7.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_f32.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_q15.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_q31.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_q7.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_float_to_q15.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_float_to_q31.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_float_to_q7.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q15_to_float.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q15_to_q31.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q15_to_q7.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q31_to_float.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q31_to_q15.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q31_to_q7.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q7_to_float.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q7_to_q15.c \
+../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q7_to_q31.c 
+
+OBJS += \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_f32.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_q15.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_q31.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_q7.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_f32.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_q15.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_q31.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_q7.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_float_to_q15.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_float_to_q31.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_float_to_q7.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q15_to_float.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q15_to_q31.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q15_to_q7.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q31_to_float.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q31_to_q15.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q31_to_q7.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q7_to_float.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q7_to_q15.o \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q7_to_q31.o 
+
+C_DEPS += \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_f32.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_q15.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_q31.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_copy_q7.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_f32.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_q15.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_q31.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_fill_q7.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_float_to_q15.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_float_to_q31.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_float_to_q7.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q15_to_float.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q15_to_q31.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q15_to_q7.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q31_to_float.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q31_to_q15.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q31_to_q7.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q7_to_float.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q7_to_q15.d \
+./Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/arm_q7_to_q31.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/%.o: ../Drivers/CMSIS/DSP_Lib/Source/SupportFunctions/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: MCU GCC Compiler'
+	@echo %cd%
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 '-D__weak=__attribute__((weak))' '-D__packed=__attribute__((__packed__))' -DUSE_HAL_DRIVER -DSTM32F446xx -I"D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Inc" -I"D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc" -I"D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy" -I"D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Device/ST/STM32F4xx/Include" -I"D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include"  -Og -g3 -Wall -fmessage-length=0 -ffunction-sections -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ Dynamixel_AX-12_UART/Debug/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.d
@@ -0,0 +1,80 @@
+Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.o: \
+ ../Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Inc/stm32f4xx_hal_conf.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Inc/main.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/core_cm4.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/core_cmInstr.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/cmsis_gcc.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/core_cmFunc.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/core_cmSimd.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Inc/stm32f4xx_hal_conf.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Inc/main.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/core_cm4.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/core_cmInstr.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/core_cmFunc.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Include/core_cmSimd.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+D:/Users/Tyler/Documents/STM/Workspace/Dynamixel_AX-12_UART/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_uart.h:
