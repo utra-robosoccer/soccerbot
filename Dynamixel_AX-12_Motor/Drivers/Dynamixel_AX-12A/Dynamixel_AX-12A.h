42ef4831442b3672f80292ad781ed15936112d13
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2017-07-09 21:59:10 -0400

    Implemented all possible setters. Getter skeles
    All setters implemented and commented (properly, I think (?)). All
    getters have function prototypes and all have full descriptions in
    Dynamixel_AX-12A.c
    
    
    Former-commit-id: 5326f0f85e47e31959bcf7bcb60f9cf94df98c33 [formerly 22e25f4c7ffb52fb5dfade2b2e23e274cd1d5a01]
    Former-commit-id: 90ed1f7a35ae7cd6b26cd0d75131a762ab445fb5

--- Dynamixel_AX-12_Motor/Drivers/Dynamixel_AX-12A/Dynamixel_AX-12A.h
+++ Dynamixel_AX-12_Motor/Drivers/Dynamixel_AX-12A/Dynamixel_AX-12A.h
@@ -16,39 +16,50 @@ UART_HandleTypeDef Dynamixel_huart;
 
 /*************** Exported types ***************/
 typedef struct{
-	uint8_t					_ID;					/*!< Motor identification (0-252)				*/
-	uint8_t					_BaudRate;				/*!< UART communication baud rate motor uses	*/
-	uint16_t				_lastPosition;			/*!< Position read from motor					*/
-	uint16_t				_lastVelocity;			/*!< Velocity read from motor					*/
-	uint16_t				_lastTemperature;		/*!< Temperature read from motor				*/
-	uint16_t				_lastVoltage;			/*!< Voltage read from motor					*/
-	uint16_t				_lastLoad;				/*!< Load read from motor						*/
-	uint16_t				_isJointMode;			/*!< 1 if motor is joint mode, 0 if wheel mode	*/
-	UART_HandleTypeDef		*_UART_Handle;			/*!< UART handle for motor						*/
+	uint8_t					_ID;					/*!< Motor identification (0-252)					*/
+	uint32_t				_BaudRate;				/*!< UART communication baud rate					*/
+	uint16_t				_lastPosition;			/*!< Position read from motor						*/
+	uint16_t				_lastVelocity;			/*!< Velocity read from motor						*/
+	uint16_t				_lastLoad;				/*!< Load read from motor							*/
+	uint16_t				_lastVoltage;			/*!< Voltage read from motor						*/
+	uint16_t				_lastTemperature;		/*!< Temperature read from motor					*/
+	uint16_t				_isJointMode;			/*!< 1 if motor is joint mode, 0 if wheel mode		*/
+	UART_HandleTypeDef		*_UART_Handle;			/*!< UART handle for motor							*/
 }Dynamixel_HandleTypeDef;
 
 /*************** Private function prototypes ***************/
 // Setters
-void Dynamixel_SetPosition(Dynamixel_HandleTypeDef *hdynamixel, double angle); // (RAM)
-void Dynamixel_SetVelocity(Dynamixel_HandleTypeDef *hdynamixel, double velocity); // (RAM)
-void Dynamixel_SetBaudRate(Dynamixel_HandleTypeDef *hdynamixel, double baud); // (EEPROM)
 void Dynamixel_SetID(Dynamixel_HandleTypeDef *hdynamixel, int ID); // (EEPROM)
-void Dynamixel_SetMaxTorque(Dynamixel_HandleTypeDef *hdynamixel, double maxTorque); // (EEPROM)
-void Dynamixel_SetGoalTorque(Dynamixel_HandleTypeDef *hdynamixel, double goalTorque); // (RAM)
+void Dynamixel_SetBaudRate(Dynamixel_HandleTypeDef *hdynamixel, double baud); // (EEPROM)
+void Dynamixel_SetReturnDelayTime(Dynamixel_HandleTypeDef *hdynamixel, double microSec); // (EEPROM)
 void Dynamixel_SetCWAngleLimit(Dynamixel_HandleTypeDef *hdynamixel, double minAngle); // (EEPROM)
 void Dynamixel_SetCCWAngleLimit(Dynamixel_HandleTypeDef *hdynamixel, double maxAngle); // (EEPROM)
-void Dynamixel_SetCWComplianceMargin(Dynamixel_HandleTypeDef *hdynamixel, uint8_t CWcomplianceMargin); // (RAM) UNIMPLEMENTED
-void Dynamixel_SetCCWComplianceMargin(Dynamixel_HandleTypeDef *hdynamixel, uint8_t CCWcomplianceMargin); // (RAM) UNIMPLEMENTED
-void Dynamixel_SetCWComplianceSlope(Dynamixel_HandleTypeDef *hdynamixel, uint8_t CWcomplianceSlope); // (RAM) UNIMPLEMENTED
-void Dynamixel_SetCCWComplianceMargin(Dynamixel_HandleTypeDef *hdynamixel, uint8_t CCWcomplianceSlope); // (RAM) UNIMPLEMENTED
-void Dynamixel_TorqueEnable(Dynamixel_HandleTypeDef *hdynamixel, uint8_t isEnabled); // (RAM) UNIMPLEMENTED
+void Dynamixel_SetHighestVoltageLimit(Dynamixel_HandleTypeDef *hdynamixel, double highestVoltage); // (EEPROM)
+void Dynamixel_SetLowestVoltageLimit(Dynamixel_HandleTypeDef *hdynamixel, double lowestVoltage); // (EEPROM)
+void Dynamixel_SetMaxTorque(Dynamixel_HandleTypeDef *hdynamixel, double maxTorque); // (EEPROM)
+void Dynamixel_SetStatusReturnLevel(Dynamixel_HandleTypeDef *hdynamixel, uint8_t status_data); // (EEPROM)
+void Dynamixel_SetAlarmLED(Dynamixel_HandleTypeDef *hdynamixel, uint8_t alarm_LED_data); // (EEPROM)
+void Dynamixel_SetAlarmShutdown(Dynamixel_HandleTypeDef *hdynamixel, uint8_t alarm_shutdown_data); // (EEPROM)
+
+void Dynamixel_TorqueEnable(Dynamixel_HandleTypeDef *hdynamixel, uint8_t isEnabled); // (RAM)
+void Dynamixel_LEDEnable(Dynamixel_HandleTypeDef *hdynamixel, uint8_t isEnabled); // (RAM)
+void Dynamixel_SetCWComplianceMargin(Dynamixel_HandleTypeDef *hdynamixel, uint8_t CWcomplianceMargin); // (RAM)
+void Dynamixel_SetCCWComplianceMargin(Dynamixel_HandleTypeDef *hdynamixel, uint8_t CCWcomplianceMargin); // (RAM)
+void Dynamixel_SetCWComplianceSlope(Dynamixel_HandleTypeDef *hdynamixel, uint8_t CWcomplianceSlope); // (RAM)
+void Dynamixel_SetCCWComplianceMargin(Dynamixel_HandleTypeDef *hdynamixel, uint8_t CCWcomplianceSlope); // (RAM)
+void Dynamixel_SetGoalPosition(Dynamixel_HandleTypeDef *hdynamixel, double goalAngle); // (RAM)
+void Dynamixel_SetGoalVelocity(Dynamixel_HandleTypeDef *hdynamixel, double goalVelocity); // (RAM)
+void Dynamixel_SetGoalTorque(Dynamixel_HandleTypeDef *hdynamixel, double goalTorque); // (RAM)
+void Dynamixel_LockEEPROM(Dynamixel_HandleTypeDef *hdynamixel, uint8_t isLocked); // (RAM)
+void Dynamixel_SetPunch(Dynamixel_HandleTypeDef *hdynamixel, double punch); // (RAM)
 
 // Getters
 void Dynamixel_GetPosition(Dynamixel_HandleTypeDef *hdynamixel); // UNIMPLEMENTED
 void Dynamixel_GetVelocity(Dynamixel_HandleTypeDef *hdynamixel); // UNIMPLEMENTED
-void Dynamixel_GetTemperature(Dynamixel_HandleTypeDef *hdynamixel); // UNIMPLEMENTED
 void Dynamixel_GetLoad(Dynamixel_HandleTypeDef *hdynamixel); // UNIMPLEMENTED
 void Dynamixel_GetVoltage(Dynamixel_HandleTypeDef *hdynamixel); // UNIMPLEMENTED
+void Dynamixel_GetTemperature(Dynamixel_HandleTypeDef *hdynamixel); // UNIMPLEMENTED
+uint8_t Dynamixel_IsRegistered(Dynamixel_HandleTypeDef *hdynamixel); // UNIMPLEMENTED
 uint8_t Dynamixel_IsMoving(Dynamixel_HandleTypeDef *hdynamixel); // UNIMPLEMENTED
 uint8_t Dynamixel_IsJointMode(Dynamixel_HandleTypeDef *hdynamixel); // UNIMPLEMENTED
 
@@ -59,6 +70,6 @@ uint8_t Dynamixel_ComputeChecksum(uint8_t *arr, int length);
 void Dynamixel_DataWriter(Dynamixel_HandleTypeDef *hdynamixel, uint8_t arrSize, uint8_t writeAddr, uint8_t param1, uint8_t param2);
 
 // Initialization
-void Dynamixel_Init(Dynamixel_HandleTypeDef *hdynamixel, uint8_t ID, double BaudRate, UART_HandleTypeDef* UART_Handle);
+void Dynamixel_Init(Dynamixel_HandleTypeDef *hdynamixel, uint8_t ID, UART_HandleTypeDef* UART_Handle);
 
 #endif /* __DYNAMIXEL_AX-12A_H */
