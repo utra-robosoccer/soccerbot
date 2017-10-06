bf007ed73e31ea42bbed56bc0749324b554b5ec8
jenny3210 <yangyifan9617@gmail.com>
2017-10-06 17:12:10 -0400

    Update MPU6050.h

--- MPU6050/MPU6050.h
+++ MPU6050/MPU6050.h
@@ -22,7 +22,6 @@
 #define MPU6050_ADDRESS_AD0_LOW     0x68 // address pin low (GND), default for InvenSense evaluation board
 #define MPU6050_ADDRESS_AD0_HIGH    0x69 // address pin high (VCC)
 #define MPU6050_ADDR     0b11010000
-
 #define MPU6050_RA_XG_OFFS_TC       0x00 //[7] PWR_MODE, [6:1] XG_OFFS_TC, [0] OTP_BNK_VLD
 #define MPU6050_RA_YG_OFFS_TC       0x01 //[7] PWR_MODE, [6:1] YG_OFFS_TC, [0] OTP_BNK_VLD
 #define MPU6050_RA_ZG_OFFS_TC       0x02 //[7] PWR_MODE, [6:1] ZG_OFFS_TC, [0] OTP_BNK_VLD
