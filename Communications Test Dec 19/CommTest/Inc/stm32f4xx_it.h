e25c21795bcb8222cadc7208698108ef8a868e3d
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2017-12-19 21:48:31 -0500

    Comm on RTOS -- not tested
    Former-commit-id: ce4f84d74c83420075a21e671d24a4a43f1f3ec2 [formerly c01c93d228ae9688f9b8ef3bc2ea3006715d0647]
    Former-commit-id: 6c25dd9aeefec3f565e1efceef0b53639d0413a3

--- Communications Test Dec 19/CommTest/Inc/stm32f4xx_it.h
+++ Communications Test Dec 19/CommTest/Inc/stm32f4xx_it.h
@@ -46,6 +46,7 @@
 /* Exported functions ------------------------------------------------------- */
 
 void SysTick_Handler(void);
+void TIM1_UP_TIM10_IRQHandler(void);
 void USART3_IRQHandler(void);
 
 #ifdef __cplusplus
--- Communications Test Dec 19/CommTest/Src/stm32f4xx_it.c
+++ Communications Test Dec 19/CommTest/Src/stm32f4xx_it.c
@@ -34,6 +34,7 @@
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx.h"
 #include "stm32f4xx_it.h"
+#include "cmsis_os.h"
 
 /* USER CODE BEGIN 0 */
 
@@ -41,6 +42,8 @@
 
 /* External variables --------------------------------------------------------*/
 extern UART_HandleTypeDef huart3;
+
+extern TIM_HandleTypeDef htim1;
 
 /******************************************************************************/
 /*            Cortex-M4 Processor Interruption and Exception Handlers         */ 
@@ -54,8 +57,7 @@ void SysTick_Handler(void)
   /* USER CODE BEGIN SysTick_IRQn 0 */
 
   /* USER CODE END SysTick_IRQn 0 */
-  HAL_IncTick();
-  HAL_SYSTICK_IRQHandler();
+  osSystickHandler();
   /* USER CODE BEGIN SysTick_IRQn 1 */
 
   /* USER CODE END SysTick_IRQn 1 */
@@ -67,6 +69,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32f4xx.s).                    */
 /******************************************************************************/
+
+/**
+* @brief This function handles TIM1 update interrupt and TIM10 global interrupt.
+*/
+void TIM1_UP_TIM10_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 0 */
+
+  /* USER CODE END TIM1_UP_TIM10_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim1);
+  /* USER CODE BEGIN TIM1_UP_TIM10_IRQn 1 */
+
+  /* USER CODE END TIM1_UP_TIM10_IRQn 1 */
+}
 
 /**
 * @brief This function handles USART3 global interrupt.
