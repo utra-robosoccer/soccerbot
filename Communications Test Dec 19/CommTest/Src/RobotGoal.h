3d439c8c6d83f9ce22614c7b1eef0719502d0abc
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2017-12-19 19:54:21 -0500

    First batch of code for communications w/software

---
+++ Communications Test Dec 19/CommTest/Src/RobotGoal.h
@@ -0,0 +1,16 @@
+/*
+ * RobotGoal.h
+ *
+ *  Created on: Dec 19, 2017
+ *      Author: Admin
+ */
+
+#ifndef ROBOTGOAL_H_
+#define ROBOTGOAL_H_
+
+typedef struct robot_goal{
+	int a;
+	char message[50];
+}RobotGoal;
+
+#endif /* ROBOTGOAL_H_ */
