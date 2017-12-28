18da8cfe5c67b3ee0d228f3859d3446801ef8000
Jason Wang <vuwij@me.com>
2017-12-28 01:17:28 -0500

    Removed metadata

---
+++ Communication/Communication/Inc/robotGoal.h
@@ -0,0 +1,19 @@
+/*
+ * RobotGoal.h
+ *
+ *  Created on: Dec 19, 2017
+ *      Author: Admin
+ */
+
+#ifndef ROBOTGOAL_H_
+#define ROBOTGOAL_H_
+
+#include "main.h"
+#include "stm32l4xx_hal.h"
+
+typedef struct robot_goal{
+	uint32_t id;
+	char message[50];
+} RobotGoal;
+
+#endif /* ROBOTGOAL_H_ */
---
+++ Communication/Communication/Inc/robotState.h
@@ -0,0 +1,19 @@
+/*
+ * RobotState.h
+ *
+ *  Created on: Dec 19, 2017
+ *      Author: Admin
+ */
+
+#ifndef ROBOTSTATE_H_
+#define ROBOTSTATE_H_
+
+#include "main.h"
+#include "stm32l4xx_hal.h"
+
+typedef struct robot_state{
+	uint32_t id;
+	char msg[50];
+}RobotState;
+
+#endif /* ROBOTSTATE_H_ */
