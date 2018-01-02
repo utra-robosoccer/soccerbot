0b859a3fdeb8d6ed2af6678db1416c6b0cb447fe
Jason Wang <vuwij@me.com>
2018-01-01 23:21:19 -0500

    Updates

--- Communication/Communication/Src/serial.c
+++ Communication/Communication/Src/serial.c
@@ -1,7 +1,9 @@
 #include <serial.h>
 
 void send_state(RobotState* robotstate) {
+	HAL_UART_AbortReceive_IT(&huart2);
 	HAL_UART_Transmit(&huart2, (uint8_t *) robotstate, sizeof(RobotState), 10);
+	HAL_UART_Receive_IT(&huart2, robotGoalBuffer, sizeof(RobotGoal));
 }
 
 RobotGoal receive_state() {
