9c84cb25f94482f2cf069078a1ea67fca254b99a
Jason Wang <vuwij@me.com>
2018-01-12 23:47:45 -0500

    Packets!
    Former-commit-id: aaa02e57bb2e3592d68269378dcc2e2e3ec223a6 [formerly 480d7380c8fa9526d8c576d57f4d6ff6ca3e9b39]
    Former-commit-id: dda9e653233607580bb8081607ff8c28f1f89973

--- Communication/Communication/Src/serial.c
+++ Communication/Communication/Src/serial.c
@@ -1,8 +1,27 @@
 #include <serial.h>
+#include <packet.h>
+#include <string.h>
 
 void send_state(RobotState* robotstate) {
+	// Encapsulates the data into a packet
+	Packet p, *p_ptr;
+	p_ptr = &p;
+	p.start_pattern = START_PATTERN;
+	p.byte_count = sizeof(RobotState);
+	memcpy(&p.data, robotstate, sizeof(RobotState));
+
 	HAL_UART_AbortReceive_IT(&huart2);
-	HAL_UART_Transmit(&huart2, (uint8_t *) robotstate, sizeof(RobotState), 10);
+	// Transmit cycle
+
+	int size = sizeof(RobotState) + 8;
+	int i = 0;
+	while(size > 0) {
+		uint8_t* c = (uint8_t * ) (p_ptr) + i;
+		HAL_UART_Transmit(&huart2, (uint8_t * ) (p_ptr) + i, sizeof(uint8_t), 10);
+		size = size - 1;
+		i++;
+	}
+
 	HAL_UART_Receive_IT(&huart2, robotGoalBuffer, sizeof(RobotGoal));
 }
 
@@ -11,3 +30,4 @@ RobotGoal receive_state() {
 
 	return r;
 }
+
