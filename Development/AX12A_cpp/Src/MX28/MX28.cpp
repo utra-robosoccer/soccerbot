fb6d0ecf1057e8295095926566366695481c680f
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-03-17 21:13:17 -0400

    Create MX28 class
    Need to populate this a bit to better understand what the base class (Dynamixel) should have in it
    
    
    Former-commit-id: 723260cfd3f8be0aba906d89b5b89ad5241aa0a7 [formerly d85d3a1973c6b9d1f31ddc868d101266b48e355b]
    Former-commit-id: 92e2f271a5e9c399fd7af0bf9410c22f21292347

---
+++ Development/AX12A_cpp/Src/MX28/MX28.cpp
@@ -0,0 +1,18 @@
+/*
+ * MX28.cpp
+ *
+ *  Created on: Mar 17, 2018
+ *      Author: Admin
+ */
+
+#include "MX28.h"
+
+MX28::MX28() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MX28::~MX28() {
+	// TODO Auto-generated destructor stub
+}
+
