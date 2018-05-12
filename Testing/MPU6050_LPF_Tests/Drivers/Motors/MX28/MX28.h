318602545485e01740f7f176f8c89ded500b0c6c
izaakniksan <izaakniksan@gmail.com>
2018-05-12 17:51:59 -0400

    lpf tests
    lpf tests

---
+++ Testing/MPU6050_LPF_Tests/Drivers/Motors/MX28/MX28.h
@@ -0,0 +1,47 @@
+/****************** Drivers for Dynamixel MX28 Servo Motor ******************/
+
+/******************** Define to prevent recursive inclusion *******************/
+#ifndef __MX28_H__
+#define __MX28_H__
+
+/********************************** Includes **********************************/
+
+/*********************************** Macros ***********************************/
+
+
+/*********************************** Defines **********************************/
+/* Communications. */
+
+
+/* Value limit definitions. */
+
+
+/* Instruction set definitions. */
+
+
+/* Register definitions. */
+
+
+/* Default register value definitions. */
+
+
+/******************************* Public Variables *******************************/
+
+
+/*********************************** Types ************************************/
+
+
+/***************************** Function prototypes ****************************/
+// Setters (use the WRITE DATA instruction)
+
+
+// Getters (use READ DATA instruction)
+
+
+// Computation
+
+
+// Transmission & Reception
+
+
+#endif /* __MX28_H__ */
