6b1aa6500cf9be26d0169c658de2edc84c9d5a15
izaakniksan <izaakniksan@gmail.com>
2018-05-26 15:22:00 -0400

    Basic integration of IMU task and queue into the final freertos.c (robot folder)
    Former-commit-id: 349278efb1eb796719f0ec8808821628b1dacd10 [formerly f45847510407119669c1643df56c621564f4d4db]
    Former-commit-id: 85347bf15596d45c55cfcbc6be3d5ba3085d17f8

--- Robot/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c
+++ Robot/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c
@@ -1725,6 +1725,7 @@ HAL_StatusTypeDef HAL_I2C_Slave_Receive_DMA(I2C_HandleTypeDef *hi2c, uint8_t *pD
   * @retval HAL status
   */
 HAL_StatusTypeDef HAL_I2C_Mem_Write(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size, uint32_t Timeout)
+//HAL_StatusTypeD HAL_I2C_Mem_Write_IT(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData, uint16_t Size)
 {
   uint32_t tickstart = 0U;
 
