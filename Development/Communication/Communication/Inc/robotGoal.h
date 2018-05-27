96c3d56114b85bf25c84e1bbae24710db8f050b0
Jason Wang <vuwij@me.com>
2018-05-27 16:40:06 -0400

    Added working communication code
    Former-commit-id: c1996ba8b379d9cb9e2846827537f89166bc8bb8 [formerly c5d72910a3a085b640279fa832dbdf6500034da4]
    Former-commit-id: b73217a16222f23cbb95575c862be0e8b2c78683

---
+++ Development/Communication/Communication/Inc/robotGoal.h
@@ -0,0 +1,19 @@
+/*
+ * RobotGoal.h
+ *
+ *  Created on: Dec 19, 2017
+ *      Author: Admin
+ */
+
+#ifndef ROBOTGOAL_H_
+#define ROBOTGOAL_H_
+
+#include "main.h"
+#include "stm32l4xx_hal.h"
+
+typedef struct robot_goal{
+	uint32_t id;
+	char msg[18];
+} RobotGoal;
+
+#endif /* ROBOTGOAL_H_ */
---
+++ Development/Communication/Communication/Inc/robotState.h
@@ -0,0 +1,21 @@
+/*
+ * RobotState.h
+ *
+ *  Created on: Dec 19, 2017
+ *      Author: Admin
+ */
+
+#ifndef ROBOTSTATE_H_
+#define ROBOTSTATE_H_
+
+#include "main.h"
+#include "stm32l4xx_hal.h"
+
+typedef struct robot_state {
+	uint32_t start_seq;
+	uint32_t id;
+	char msg[20];
+	uint32_t end_seq;
+} RobotState;
+
+#endif /* ROBOTSTATE_H_ */
