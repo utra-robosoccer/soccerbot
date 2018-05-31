f86962d98d872d5a4d18e550b43e74ecbd1aba9e
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-05-30 21:52:49 -0400

    Made all OS objects static, and moved a few things for comm around
    Made OS objects static because it seems that the APIs work differently this way (specifically for binary semaphores). Basically, when dynamically allocated, semaphores are initialized with a count of 1, and when statically allocated, semaphores are initialized with a count of 0. For our purposes, the second case is more convenient, although of course a call to take or give in either case would make them equivalent.
    
    Static allocation also ensures that all the resources are system would need are guaranteed as long as everything compiles, basically. This is good for our application.

--- Robot/Src/freertos.c
+++ Robot/Src/freertos.c
@@ -67,28 +67,64 @@
 
 /* Variables -----------------------------------------------------------------*/
 osThreadId defaultTaskHandle;
+uint32_t defaultTaskBuffer[ 128 ];
+osStaticThreadDef_t defaultTaskControlBlock;
 osThreadId UART1_Handle;
+uint32_t UART1_Buffer[ 128 ];
+osStaticThreadDef_t UART1_ControlBlock;
 osThreadId UART2_Handle;
+uint32_t UART2_Buffer[ 128 ];
+osStaticThreadDef_t UART2_ControlBlock;
 osThreadId UART3_Handle;
+uint32_t UART3_Buffer[ 128 ];
+osStaticThreadDef_t UART3_ControlBlock;
 osThreadId UART4_Handle;
+uint32_t UART4_Buffer[ 128 ];
+osStaticThreadDef_t UART4_ControlBlock;
 osThreadId UART6_Handle;
+uint32_t UART6_Buffer[ 128 ];
+osStaticThreadDef_t UART6_ControlBlock;
 osThreadId IMUTaskHandle;
+uint32_t IMUTaskBuffer[ 128 ];
+osStaticThreadDef_t IMUTaskControlBlock;
 osThreadId rxTaskHandle;
+uint32_t rxTaskBuffer[ 512 ];
+osStaticThreadDef_t rxTaskControlBlock;
 osThreadId txTaskHandle;
+uint32_t txTaskBuffer[ 128 ];
+osStaticThreadDef_t txTaskControlBlock;
 osMessageQId UART1_reqHandle;
+uint8_t UART1_reqBuffer[ 20 * sizeof( UARTcmd ) ];
+osStaticMessageQDef_t UART1_reqControlBlock;
 osMessageQId UART2_reqHandle;
+uint8_t UART2_reqBuffer[ 20 * sizeof( UARTcmd ) ];
+osStaticMessageQDef_t UART2_reqControlBlock;
 osMessageQId UART3_reqHandle;
+uint8_t UART3_reqBuffer[ 20 * sizeof( UARTcmd ) ];
+osStaticMessageQDef_t UART3_reqControlBlock;
 osMessageQId UART4_reqHandle;
+uint8_t UART4_reqBuffer[ 20 * sizeof( UARTcmd ) ];
+osStaticMessageQDef_t UART4_reqControlBlock;
 osMessageQId UART6_reqHandle;
+uint8_t UART6_reqBuffer[ 20 * sizeof( UARTcmd ) ];
+osStaticMessageQDef_t UART6_reqControlBlock;
 osMessageQId UART_rxHandle;
-osMutexId mutexRobotGoalHandle;
+uint8_t UART_rxBuffer[ 20 * sizeof( UARTcmd ) ];
+osStaticMessageQDef_t UART_rxControlBlock;
 osSemaphoreId semUART1TxHandle;
+osStaticSemaphoreDef_t semUART1TxControlBlock;
 osSemaphoreId semUART2TxHandle;
+osStaticSemaphoreDef_t semUART2TxControlBlock;
 osSemaphoreId semUART3TxHandle;
+osStaticSemaphoreDef_t semUART3TxControlBlock;
 osSemaphoreId semUART4TxHandle;
+osStaticSemaphoreDef_t semUART4TxControlBlock;
 osSemaphoreId semUART6TxHandle;
+osStaticSemaphoreDef_t semUART6TxControlBlock;
 osSemaphoreId semPCRxBuffHandle;
+osStaticSemaphoreDef_t semPCRxBuffControlBlock;
 osSemaphoreId semControlTaskHandle;
+osStaticSemaphoreDef_t semControlTaskControlBlock;
 
 /* USER CODE BEGIN Variables */
 enum motorNames {MOTOR1, MOTOR2, MOTOR3, MOTOR4, MOTOR5,
@@ -163,7 +199,23 @@ void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /* USER CODE END FunctionPrototypes */
 
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
 /* Hook prototypes */
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+  
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}                   
+/* USER CODE END GET_IDLE_TASK_MEMORY */
 
 /* Init FreeRTOS */
 
@@ -172,42 +224,37 @@ void MX_FREERTOS_Init(void) {
        
   /* USER CODE END Init */
 
-  /* Create the mutex(es) */
-  /* definition and creation of mutexRobotGoal */
-  osMutexDef(mutexRobotGoal);
-  mutexRobotGoalHandle = osMutexCreate(osMutex(mutexRobotGoal));
-
   /* USER CODE BEGIN RTOS_MUTEX */
   /* add mutexes, ... */
   /* USER CODE END RTOS_MUTEX */
 
   /* Create the semaphores(s) */
   /* definition and creation of semUART1Tx */
-  osSemaphoreDef(semUART1Tx);
+  osSemaphoreStaticDef(semUART1Tx, &semUART1TxControlBlock);
   semUART1TxHandle = osSemaphoreCreate(osSemaphore(semUART1Tx), 1);
 
   /* definition and creation of semUART2Tx */
-  osSemaphoreDef(semUART2Tx);
+  osSemaphoreStaticDef(semUART2Tx, &semUART2TxControlBlock);
   semUART2TxHandle = osSemaphoreCreate(osSemaphore(semUART2Tx), 1);
 
   /* definition and creation of semUART3Tx */
-  osSemaphoreDef(semUART3Tx);
+  osSemaphoreStaticDef(semUART3Tx, &semUART3TxControlBlock);
   semUART3TxHandle = osSemaphoreCreate(osSemaphore(semUART3Tx), 1);
 
   /* definition and creation of semUART4Tx */
-  osSemaphoreDef(semUART4Tx);
+  osSemaphoreStaticDef(semUART4Tx, &semUART4TxControlBlock);
   semUART4TxHandle = osSemaphoreCreate(osSemaphore(semUART4Tx), 1);
 
   /* definition and creation of semUART6Tx */
-  osSemaphoreDef(semUART6Tx);
+  osSemaphoreStaticDef(semUART6Tx, &semUART6TxControlBlock);
   semUART6TxHandle = osSemaphoreCreate(osSemaphore(semUART6Tx), 1);
 
   /* definition and creation of semPCRxBuff */
-  osSemaphoreDef(semPCRxBuff);
+  osSemaphoreStaticDef(semPCRxBuff, &semPCRxBuffControlBlock);
   semPCRxBuffHandle = osSemaphoreCreate(osSemaphore(semPCRxBuff), 1);
 
   /* definition and creation of semControlTask */
-  osSemaphoreDef(semControlTask);
+  osSemaphoreStaticDef(semControlTask, &semControlTaskControlBlock);
   semControlTaskHandle = osSemaphoreCreate(osSemaphore(semControlTask), 1);
 
   /* USER CODE BEGIN RTOS_SEMAPHORES */
@@ -220,39 +267,39 @@ void MX_FREERTOS_Init(void) {
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityIdle, 0, 128);
+  osThreadStaticDef(defaultTask, StartDefaultTask, osPriorityIdle, 0, 128, defaultTaskBuffer, &defaultTaskControlBlock);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* definition and creation of UART1_ */
-  osThreadDef(UART1_, UART1_Handler, osPriorityNormal, 0, 128);
+  osThreadStaticDef(UART1_, UART1_Handler, osPriorityNormal, 0, 128, UART1_Buffer, &UART1_ControlBlock);
   UART1_Handle = osThreadCreate(osThread(UART1_), NULL);
 
   /* definition and creation of UART2_ */
-  osThreadDef(UART2_, UART2_Handler, osPriorityNormal, 0, 128);
+  osThreadStaticDef(UART2_, UART2_Handler, osPriorityNormal, 0, 128, UART2_Buffer, &UART2_ControlBlock);
   UART2_Handle = osThreadCreate(osThread(UART2_), NULL);
 
   /* definition and creation of UART3_ */
-  osThreadDef(UART3_, UART3_Handler, osPriorityNormal, 0, 128);
+  osThreadStaticDef(UART3_, UART3_Handler, osPriorityNormal, 0, 128, UART3_Buffer, &UART3_ControlBlock);
   UART3_Handle = osThreadCreate(osThread(UART3_), NULL);
 
   /* definition and creation of UART4_ */
-  osThreadDef(UART4_, UART4_Handler, osPriorityNormal, 0, 128);
+  osThreadStaticDef(UART4_, UART4_Handler, osPriorityNormal, 0, 128, UART4_Buffer, &UART4_ControlBlock);
   UART4_Handle = osThreadCreate(osThread(UART4_), NULL);
 
   /* definition and creation of UART6_ */
-  osThreadDef(UART6_, UART6_Handler, osPriorityNormal, 0, 128);
+  osThreadStaticDef(UART6_, UART6_Handler, osPriorityNormal, 0, 128, UART6_Buffer, &UART6_ControlBlock);
   UART6_Handle = osThreadCreate(osThread(UART6_), NULL);
 
   /* definition and creation of IMUTask */
-  osThreadDef(IMUTask, StartIMUTask, osPriorityNormal, 0, 128);
+  osThreadStaticDef(IMUTask, StartIMUTask, osPriorityNormal, 0, 128, IMUTaskBuffer, &IMUTaskControlBlock);
   IMUTaskHandle = osThreadCreate(osThread(IMUTask), NULL);
 
   /* definition and creation of rxTask */
-  osThreadDef(rxTask, StartRxTask, osPriorityIdle, 0, 512);
+  osThreadStaticDef(rxTask, StartRxTask, osPriorityRealtime, 0, 512, rxTaskBuffer, &rxTaskControlBlock);
   rxTaskHandle = osThreadCreate(osThread(rxTask), NULL);
 
   /* definition and creation of txTask */
-  osThreadDef(txTask, StartTxTask, osPriorityRealtime, 0, 128);
+  osThreadStaticDef(txTask, StartTxTask, osPriorityHigh, 0, 128, txTaskBuffer, &txTaskControlBlock);
   txTaskHandle = osThreadCreate(osThread(txTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
@@ -261,27 +308,27 @@ void MX_FREERTOS_Init(void) {
 
   /* Create the queue(s) */
   /* definition and creation of UART1_req */
-  osMessageQDef(UART1_req, 20, UARTcmd);
+  osMessageQStaticDef(UART1_req, 20, UARTcmd, UART1_reqBuffer, &UART1_reqControlBlock);
   UART1_reqHandle = osMessageCreate(osMessageQ(UART1_req), NULL);
 
   /* definition and creation of UART2_req */
-  osMessageQDef(UART2_req, 20, UARTcmd);
+  osMessageQStaticDef(UART2_req, 20, UARTcmd, UART2_reqBuffer, &UART2_reqControlBlock);
   UART2_reqHandle = osMessageCreate(osMessageQ(UART2_req), NULL);
 
   /* definition and creation of UART3_req */
-  osMessageQDef(UART3_req, 20, UARTcmd);
+  osMessageQStaticDef(UART3_req, 20, UARTcmd, UART3_reqBuffer, &UART3_reqControlBlock);
   UART3_reqHandle = osMessageCreate(osMessageQ(UART3_req), NULL);
 
   /* definition and creation of UART4_req */
-  osMessageQDef(UART4_req, 20, UARTcmd);
+  osMessageQStaticDef(UART4_req, 20, UARTcmd, UART4_reqBuffer, &UART4_reqControlBlock);
   UART4_reqHandle = osMessageCreate(osMessageQ(UART4_req), NULL);
 
   /* definition and creation of UART6_req */
-  osMessageQDef(UART6_req, 20, UARTcmd);
+  osMessageQStaticDef(UART6_req, 20, UARTcmd, UART6_reqBuffer, &UART6_reqControlBlock);
   UART6_reqHandle = osMessageCreate(osMessageQ(UART6_req), NULL);
 
   /* definition and creation of UART_rx */
-  osMessageQDef(UART_rx, 20, UARTcmd);
+  osMessageQStaticDef(UART_rx, 20, UARTcmd, UART_rxBuffer, &UART_rxControlBlock);
   UART_rxHandle = osMessageCreate(osMessageQ(UART_rx), NULL);
 
   /* USER CODE BEGIN RTOS_QUEUES */
@@ -322,15 +369,22 @@ void StartDefaultTask(void const * argument)
 
 	UARTcmd Motorcmd[18];
 	for(int i = MOTOR1; i < MOTOR18; i++) {
-        /* Configure motor to return status packets only for read commands */
+        // Configure motor to return status packets only for read commands
         Dynamixel_SetStatusReturnLevel(arrDynamixel[i], 1);
         osDelay(10);
+
+        // Configure motor to return status packets with minimal latency
         Dynamixel_SetReturnDelayTime(arrDynamixel[i], 2);
         osDelay(10);
+
+        // Enable motor torque
         Dynamixel_TorqueEnable(arrDynamixel[i], 1);
         osDelay(10);
+
+        // Set compliance slope such that it has torque setting 7 near the goal position
 		AX12A_SetComplianceSlope(arrDynamixel[i], 7);
 		osDelay(10);
+
 		(Motorcmd[i]).motorHandle = arrDynamixel[i];
 		(Motorcmd[i]).type = cmdWRITE;
 		(Motorcmd[i]).velocity = 10;
@@ -362,7 +416,6 @@ void StartDefaultTask(void const * argument)
 //	MPU6050_manually_set_offsets(&IMUdata);
 
 	setupIsDone = 1;
-
 
 //	// UNCOMMENT TO ITERATE THROUGH JOINT TRAJECTORIES FROM MCU
 //	int size = 1001;
@@ -408,7 +461,7 @@ void StartDefaultTask(void const * argument)
 	uint8_t i;
 	float positions[12];
 	while(1){
-//		xSemaphoreTake(semControlTaskHandle, portMAX_DELAY);
+		xSemaphoreTake(semControlTaskHandle, portMAX_DELAY);
 
 		for(i = 0; i < 12; i++){
             memcpy(&positions[i], &robotGoal.msg[i * 4], 4);
@@ -444,10 +497,6 @@ void StartDefaultTask(void const * argument)
 		    xQueueSend((Motorcmd[i]).qHandle, &(Motorcmd[i]), 0);
         }
     }
-
-	for(;;){
-		osDelay(10);
-	}
   /* USER CODE END StartDefaultTask */
 }
 
@@ -595,6 +644,19 @@ void StartIMUTask(void const * argument)
   /* USER CODE END StartIMUTask */
 }
 
+/* StartRxTask function */
+void StartRxTask(void const * argument)
+{
+  /* USER CODE BEGIN StartRxTask */
+  /* Infinite loop */
+  for(;;)
+  {
+	  HAL_UART_Receive_IT(&huart5, (uint8_t *) &buf, sizeof(buf));
+	  xSemaphoreTake(semPCRxBuffHandle, portMAX_DELAY);
+  }
+  /* USER CODE END StartRxTask */
+}
+
 /* StartTxTask function */
 void StartTxTask(void const * argument)
 {
@@ -602,6 +664,7 @@ void StartTxTask(void const * argument)
   /* Infinite loop */
   for(;;)
   {
+	  // TODO: Implement actual TX task, and use DMA/IT I/O
 	  HAL_UART_Transmit(&huart5, (uint8_t*) &robotState, sizeof(RobotState), 10);
 	  osDelay(pdMS_TO_TICKS(10));
   }
