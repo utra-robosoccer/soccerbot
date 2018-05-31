75a7d11fa8957073c505e4cfb1744bdf4ab9e475
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-05-30 23:25:43 -0400

    Added task notifications so that all initialization will complete prior to unblocking the I/O tasks

--- Robot/Src/dma.c
+++ Robot/Src/dma.c
@@ -83,6 +83,9 @@ void MX_DMA_Init(void)
   /* DMA1_Stream6_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Stream6_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+  /* DMA1_Stream7_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Stream7_IRQn, 5, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Stream7_IRQn);
   /* DMA2_Stream6_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA2_Stream6_IRQn, 5, 0);
   HAL_NVIC_EnableIRQ(DMA2_Stream6_IRQn);
