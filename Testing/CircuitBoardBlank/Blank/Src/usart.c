263861bed8590653231ffe1d2e8ae765b112f078
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-06-04 19:12:53 -0400

    Regenerate blank files
    Former-commit-id: f576031ef0e39d3806188a3e60aee2eea0f7d321 [formerly 90d4d468deb6a5ac26504e937df426aea83410e4]
    Former-commit-id: ebacdaf6c9893052007ba5bf94f6f243d833d778

--- Testing/CircuitBoardBlank/Blank/Src/usart.c
+++ Testing/CircuitBoardBlank/Blank/Src/usart.c
@@ -97,6 +97,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     GPIO_InitStruct.Alternate = GPIO_AF8_UART5;
     HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
+    /* UART5 interrupt Init */
+    HAL_NVIC_SetPriority(UART5_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(UART5_IRQn);
   /* USER CODE BEGIN UART5_MspInit 1 */
 
   /* USER CODE END UART5_MspInit 1 */
@@ -122,6 +125,8 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
 
     HAL_GPIO_DeInit(GPIOD, GPIO_PIN_2);
 
+    /* UART5 interrupt Deinit */
+    HAL_NVIC_DisableIRQ(UART5_IRQn);
   /* USER CODE BEGIN UART5_MspDeInit 1 */
 
   /* USER CODE END UART5_MspDeInit 1 */
