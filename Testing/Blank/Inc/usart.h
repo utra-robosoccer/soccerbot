ebc5e860c20a209923fa7d8bbf2d7bf65bc0f979
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-06-04 21:07:55 -0400

    uart2
    Former-commit-id: a8f2fad98d56dde5df2845d3821a4c41f40fd1a5 [formerly a45e33e5ff6d160e7b574954b4609d78d591c3db]
    Former-commit-id: 9dab5e5622a8812a84de49b833ff3e6a3c54fc95

--- Testing/Blank/Inc/usart.h
+++ Testing/Blank/Inc/usart.h
@@ -52,6 +52,7 @@
 /* USER CODE END Includes */
 
 extern UART_HandleTypeDef huart5;
+extern UART_HandleTypeDef huart2;
 
 /* USER CODE BEGIN Private defines */
 
@@ -60,6 +61,7 @@ extern UART_HandleTypeDef huart5;
 extern void _Error_Handler(char *, int);
 
 void MX_UART5_Init(void);
+void MX_USART2_UART_Init(void);
 
 /* USER CODE BEGIN Prototypes */
 
