e4f5f914a521297d59a934c1170552a653251264
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-06-10 15:32:23 -0400

    Add sharedMacros.h . MPU6050 IT in progress. Some TX stuff has been worked on for receiving positions and IMU data

--- Robot/Drivers/Communication/robotState.h
+++ Robot/Drivers/Communication/robotState.h
@@ -11,8 +11,10 @@
 typedef struct robot_state {
 	uint32_t start_seq;
 	uint32_t id;
-	char msg[80];
+	char msg[80 + 24];
 	uint32_t end_seq;
 } RobotState;
+
+#define ROBOT_STATE_MPU_DATA_OFFSET 80
 
 #endif /* ROBOTSTATE_H_ */
