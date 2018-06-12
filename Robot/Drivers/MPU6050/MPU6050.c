1c0875d394df6e11219c13a8adc4afefed180bd4
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-06-11 21:34:06 -0400

    Added a function called generateClocks that bit-bangs the I2C master bus whenever the BUSY flag is hanging the bus up. It basically sends 1 stop bit to the slave.
    Experimentally, this was seen to relieve bus hang-ups

--- Robot/Drivers/MPU6050/MPU6050.c
+++ Robot/Drivers/MPU6050/MPU6050.c
@@ -137,6 +137,7 @@ void MPU6050_Read_Gyroscope_Withoffset_IT(MPU6050_HandleTypeDef *sMPU6050){
 	BaseType_t status;
 
 	if(MPU6050_READ_DATA_IT(sMPU6050, MPU6050_RA_GYRO_XOUT_H,output_buffer) != HAL_OK){
+		generateClocks(1, 1);
 		return;
 	}
 
@@ -187,6 +188,7 @@ void MPU6050_Read_Accelerometer_Withoffset_IT(MPU6050_HandleTypeDef *sMPU6050){
 	BaseType_t status;
 
 	if(MPU6050_READ_DATA_IT(sMPU6050, MPU6050_RA_ACCEL_XOUT_H,output_buffer) != HAL_OK){
+		generateClocks(1, 1);
 		return;
 	}
 
@@ -362,4 +364,129 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
 	/*output values from gyroscope or accelerometer */
 		/* eg. MPU6050_Get_Val_Gyro(); 	*/
 
+}
+
+
+// Note: The following 2 functions are used as a workaround for an issue where the BUSY flag of the
+// I2C module is erroneously asserted in the hardware (a silicon bug, essentially). This workaround has
+// not been thoroughly tested.
+//
+// Overall, use these functions with EXTREME caution.
+static uint8_t wait_for_gpio_state_timeout(GPIO_TypeDef *port, uint16_t pin, GPIO_PinState state, uint8_t timeout){
+	/* Helper function for I2C_ClearBusyFlagErratum.
+	 *
+	 * Arguments: none
+	 *
+	 * Returns: none
+	 */
+
+    uint32_t Tickstart = HAL_GetTick();
+    uint8_t ret = 0;
+    /* Wait until flag is set */
+    while((state != HAL_GPIO_ReadPin(port, pin)) && (1 == ret)){
+        /* Check for the timeout */
+		if ((timeout == 0U) || (HAL_GetTick() - Tickstart >= timeout)){
+			ret = 0;
+		}
+        asm("nop");
+    }
+    return ret;
+}
+
+void generateClocks(uint8_t numClocks, uint8_t sendStopBits){
+	/* This function big-bangs the I2C master clock
+	 *
+	 * https://electronics.stackexchange.com/questions/267972/i2c-busy-flag-strange-behaviour/281046#281046
+	 * https://community.st.com/thread/35884-cant-reset-i2c-in-stm32f407-to-release-i2c-lines
+	 * https://electronics.stackexchange.com/questions/272427/stm32-busy-flag-is-set-after-i2c-initialization
+	 * http://www.st.com/content/ccc/resource/technical/document/errata_sheet/f5/50/c9/46/56/db/4a/f6/CD00197763.pdf/files/CD00197763.pdf/jcr:content/translations/en.CD00197763.pdf
+	 *
+	 *
+	 * Arguments: numClocks, the number of times to cycle the I2C master clock
+	 * 			  sendStopBits, 1 if stop bits are to be sent on SDA
+	 *
+	 * Returns: none
+	 */
+
+	static struct I2C_Module{
+		I2C_HandleTypeDef*   instance;
+		uint16_t            sdaPin;
+		GPIO_TypeDef*       sdaPort;
+		uint16_t            sclPin;
+		GPIO_TypeDef*       sclPort;
+	}i2cmodule = {&hi2c1, GPIO_PIN_7, GPIOB, GPIO_PIN_6, GPIOB};
+	static struct I2C_Module* i2c = &i2cmodule;
+	static uint8_t timeout = 1;
+
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	I2C_HandleTypeDef* handler = NULL;
+
+	handler = i2c->instance;
+
+	// 1. Clear PE bit.
+	CLEAR_BIT(handler->Instance->CR1, I2C_CR1_PE);
+
+	GPIO_InitStructure.Mode = GPIO_MODE_OUTPUT_OD;
+	GPIO_InitStructure.Pull = GPIO_NOPULL;
+	GPIO_InitStructure.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+
+	GPIO_InitStructure.Pin = i2c->sclPin;
+	HAL_GPIO_Init(i2c->sclPort, &GPIO_InitStructure);
+
+	GPIO_InitStructure.Pin = i2c->sdaPin;
+	HAL_GPIO_Init(i2c->sdaPort, &GPIO_InitStructure);
+
+	for(uint8_t i = 0; i < numClocks; i++){
+		// 3. Check SCL and SDA High level in GPIOx_IDR.
+		if(sendStopBits){HAL_GPIO_WritePin(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_SET);}
+		HAL_GPIO_WritePin(i2c->sclPort, i2c->sclPin, GPIO_PIN_SET);
+
+		wait_for_gpio_state_timeout(i2c->sclPort, i2c->sclPin, GPIO_PIN_SET, timeout);
+		if(sendStopBits){wait_for_gpio_state_timeout(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_SET, timeout);}
+
+		// 4. Configure the SDA I/O as General Purpose Output Open-Drain, Low level (Write 0 to GPIOx_ODR).
+		if(sendStopBits){
+			HAL_GPIO_WritePin(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_RESET);
+			wait_for_gpio_state_timeout(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_RESET, timeout); // 5. Check SDA Low level in GPIOx_IDR
+		}
+
+		// 6. Configure the SCL I/O as General Purpose Output Open-Drain, Low level (Write 0 to GPIOx_ODR).
+		HAL_GPIO_WritePin(i2c->sclPort, i2c->sclPin, GPIO_PIN_RESET);
+		wait_for_gpio_state_timeout(i2c->sclPort, i2c->sclPin, GPIO_PIN_RESET, timeout); // 7. Check SCL Low level in GPIOx_IDR.
+
+		// 8. Configure the SCL I/O as General Purpose Output Open-Drain, High level (Write 1 to GPIOx_ODR).
+		HAL_GPIO_WritePin(i2c->sclPort, i2c->sclPin, GPIO_PIN_SET);
+		wait_for_gpio_state_timeout(i2c->sclPort, i2c->sclPin, GPIO_PIN_SET, timeout); // 9. Check SCL High level in GPIOx_IDR.
+
+		// 10. Configure the SDA I/O as General Purpose Output Open-Drain , High level (Write 1 to GPIOx_ODR).
+		if(sendStopBits){
+			HAL_GPIO_WritePin(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_SET);
+			wait_for_gpio_state_timeout(i2c->sdaPort, i2c->sdaPin, GPIO_PIN_SET, timeout); // 11. Check SDA High level in GPIOx_IDR.
+		}
+	}
+
+	// 12. Configure the SCL and SDA I/Os as Alternate function Open-Drain.
+	GPIO_InitStructure.Mode = GPIO_MODE_AF_OD;
+	GPIO_InitStructure.Alternate = GPIO_AF4_I2C1;
+
+	GPIO_InitStructure.Pin = i2c->sclPin;
+	HAL_GPIO_Init(i2c->sclPort, &GPIO_InitStructure);
+
+	GPIO_InitStructure.Pin = i2c->sdaPin;
+	HAL_GPIO_Init(i2c->sdaPort, &GPIO_InitStructure);
+
+	// 13. Set SWRST bit in I2Cx_CR1 register.
+	SET_BIT(handler->Instance->CR1, I2C_CR1_SWRST);
+	asm("nop");
+
+	/* 14. Clear SWRST bit in I2Cx_CR1 register. */
+	CLEAR_BIT(handler->Instance->CR1, I2C_CR1_SWRST);
+	asm("nop");
+
+	/* 15. Enable the I2C peripheral by setting the PE bit in I2Cx_CR1 register */
+	SET_BIT(handler->Instance->CR1, I2C_CR1_PE);
+	asm("nop");
+
+	HAL_I2C_Init(handler);
 }
