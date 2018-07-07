96b6b3d8432be1d94ebbf4f480a1c9d485cd73ae
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-07-07 15:20:46 -0400

    Fix more warnings (several groups without titles, empty @retval statements)
    Former-commit-id: f1c2909ca09e9e17f454621362fc7d98c0e7a08a [formerly af211b42c36df9f81680c8ec639bc28b7fd7629e]
    Former-commit-id: de935d65828ba4ae4580698c2a822329382ea253

--- Robot/Doc/html/_communication_8c.js
+++ Robot/Doc/html/_communication_8c.js
@@ -1,5 +1,5 @@
 var _communication_8c =
 [
-    [ "robotGoal", "_communication_8c.html#a26279af13a015bc5d25fdff1ed1202fc", null ],
-    [ "robotState", "_communication_8c.html#ac506966b1e773431c55862462858c0a1", null ]
+    [ "robotGoal", "group___communication.html#ga26279af13a015bc5d25fdff1ed1202fc", null ],
+    [ "robotState", "group___communication.html#gac506966b1e773431c55862462858c0a1", null ]
 ];
