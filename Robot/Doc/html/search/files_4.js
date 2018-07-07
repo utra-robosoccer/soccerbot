b9a2605d1d327d393947d2f1cadb5e160f5f39be
han210 <40050591+han210@users.noreply.github.com>
2018-07-07 16:44:11 -0400

    Adding Main Page
    In main.c
    Under  /* USER CODE BEGIN 0 */
    
    
    Former-commit-id: d16ffef82ea650c7cf4c9990580cc27d5b37b426 [formerly 0d1bd983fab35cc918fb5ceb085aa1b663873086]
    Former-commit-id: 5d1647afbdbb2f7ea2fd44bd640761b98d1c663a

--- Robot/Doc/html/search/files_4.js
+++ Robot/Doc/html/search/files_4.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['main_2ec',['main.c',['../main_8c.html',1,'']]],
   ['main_2eh',['main.h',['../main_8h.html',1,'']]],
+  ['mpu6050_2ec',['MPU6050.c',['../_m_p_u6050_8c.html',1,'']]],
   ['mx28_2ec',['MX28.c',['../_m_x28_8c.html',1,'']]],
   ['mx28_2eh',['MX28.h',['../_m_x28_8h.html',1,'']]]
 ];
