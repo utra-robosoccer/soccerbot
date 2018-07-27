e77886aa9a19f83fa8c7c5ddbfc37ce6a13fa730
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-07-26 22:59:49 -0400

    Plotting works now for a test function

--- Development/Foot_Pressure_Sensor_Standalone/PC-Side/plotter.py
+++ Development/Foot_Pressure_Sensor_Standalone/PC-Side/plotter.py
@@ -2,16 +2,34 @@
 Real-time data plotter for a stream of floats from a virtual COM port
 
 Author: Tyler
+
+Resources used:
+- http://zetcode.com/gui/pyqt5/
+- https://stackoverflow.com/questions/9614947/pyqt4-emiting-signals-in-threads-to-slots-in-main-thread
+- http://www.qtcentre.org/threads/4486-QThread-run()-Vs-start()
+- https://stackoverflow.com/questions/12432637/pyqt4-set-windows-taskbar-icon
+- https://matplotlib.org/gallery/user_interfaces/embedding_in_qt_sgskip.html
 '''
 
 import sys
 import numpy as np
 import serial
 import struct
+
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QDesktopWidget, QSizePolicy, QVBoxLayout
+from PyQt5.QtCore import pyqtSignal, QObject, QThread, QDateTime
+
 import ctypes
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QDesktopWidget
 from PyQt5.QtGui import QIcon
-from PyQt5.QtCore import pyqtSignal, QObject, QThread
+
+from matplotlib.backends.qt_compat import is_pyqt5, QtCore
+if is_pyqt5():
+    from matplotlib.backends.backend_qt5agg import (
+        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+else:
+    from matplotlib.backends.backend_qt4agg import (
+        FigureCanvas, NavigationToolbar2QT as NavigationToolbar)
+from matplotlib.figure import Figure
     
 def bytes2fvec(byteArr):
     ''' Transforms a byte array, with entries interpreted as 32-bit floats, to
@@ -32,55 +50,67 @@ def bytes2fvec(byteArr):
 class SerialReader(QThread):
     """ Class that reads data from a serial port """
     signal_serial_status = pyqtSignal(str)
+    signal_serial_update = pyqtSignal(np.ndarray)
     
-    def __init__(self, com_port):
+    def __init__(self, comPort):
         super(SerialReader, self).__init__()
-        self.com_port = com_port
+        self.comPort = comPort
     
     def run(self):
+        self.test()
         self.initReader()
-        #receiveFromMCU()
+        receiveFromMCU()
+        
+    def test(self):
+        while(True):
+            v = np.zeros((1, 1))
+            v[:] = np.sin(QDateTime.currentMSecsSinceEpoch() / 1000)
+            self.signal_serial_update.emit(v)
+            QThread.msleep(100)
         
     def initReader(self):
         connected = False
         num_tries = 0
+        
         while(not connected):
             try:
-                ser = serial.Serial(self.com_port, 230400, timeout=100)
+                ser = serial.Serial(self.comPort, 230400, timeout=100)
                 self.ser.reset_output_buffer()
                 self.ser.reset_input_buffer()
                 
                 connected = True
                 
-                self.signal_serial_status.emit(self.signal_serial_status, 
-                        "SUCCESS: connected to {0}".format(self.com_port))
+                self.signal_serial_status.emit(
+                    "SUCCESS: connected to {0}".format(self.comPort)
+                )
             except:
                 num_tries = num_tries + 1
                 
                 self.signal_serial_status.emit(
                     "FAIL: Could not connect to {0}. Number of tries: {1}".
-                    format(self.com_port, num_tries)
+                    format(self.comPort, num_tries)
                 )
             
             QThread.msleep(100)
         
     def receiveFromMCU():
-        # TODO
-        while(ser.in_waiting < 4):
-            QThread.msleep(1)
-    
-        raw = ser.read(4)
-        v = bytes2fvec(raw)
-        
+        while(True):
+            while(ser.in_waiting < 4):
+                QThread.msleep(1)
+                
+            raw = ser.read(4)
+            v = bytes2fvec(raw)
+            
+            self.signal_serial_update.emit(v)
         
 # In the future, could extend this to accept an argument for the number of
-# channels being multiplexed in the stream
+# channels being time multiplexed in the stream
 class DataPlotter(QMainWindow):
-    def __init__(self, com_port):
+    def __init__(self, comPort):
         super(DataPlotter, self).__init__()
-        self.initUI(com_port)
+        self.initUI(comPort)
     
-    def initUI(self, com_port):
+    def initUI(self, comPort):
         # Init the GUI window
         self.resize(1000, 750)
         self.center()
@@ -93,9 +123,29 @@ class DataPlotter(QMainWindow):
         self.statusBar().showMessage('Ready')
         self.show()
         
+        # Init the plotter
+        self.mainWindow = QWidget()
+        self.setCentralWidget(self.mainWindow)
+        layout = QVBoxLayout(self.mainWindow)
+        
+        canvas = FigureCanvas(Figure(figsize=(5, 3)))
+        layout.addWidget(canvas)
+        self.addToolBar(QtCore.Qt.BottomToolBarArea,
+                        NavigationToolbar(canvas, self))
+        self.ax = canvas.figure.subplots()
+        
+        self.timeIndexes = np.zeros((100,))
+        self.hasReceivedData = False
+        self.dataBuffer = np.zeros((100,)) # 100 data points to be buffered
+        
         # Init the serial data reader
-        self.serialThread = SerialReader(com_port)
-        self.serialThread.signal_serial_status.connect(self.serial_status_signal_callback)
+        self.serialThread = SerialReader(comPort)
+        self.serialThread.signal_serial_status.connect(
+            self.serial_status_signal_callback
+        )
+        self.serialThread.signal_serial_update.connect(
+            self.serial_update_signal_callback
+        )
         self.serialThread.start()
         
     def center(self):
@@ -107,6 +157,26 @@ class DataPlotter(QMainWindow):
     def serial_status_signal_callback(self, status):
         self.statusBar().showMessage(status)
         
+    def serial_update_signal_callback(self, data):
+        self.ax.clear()
+        
+        # Shift data buffers left, add new data to rightmost slot, then redraw
+        self.timeIndexes = np.roll(self.timeIndexes, -1)
+        if not self.hasReceivedData:
+            self.startTime = QDateTime.currentMSecsSinceEpoch()
+            self.timeIndexes[-1:] = 0
+            self.hasReceivedData = True
+        else:
+            self.timeIndexes[-1:] = (
+                QDateTime.currentMSecsSinceEpoch() - self.startTime
+            )
+            self.ax.set_xlim(self.timeIndexes[0], self.timeIndexes[-1])
+        
+        self.dataBuffer = np.roll(self.dataBuffer, -1)
+        self.dataBuffer[-1:] = data
+        
+        self.ax.plot(self.timeIndexes, self.dataBuffer)
+        self.ax.figure.canvas.draw()
         
 if __name__ == "__main__":
     com = 'COM7'
