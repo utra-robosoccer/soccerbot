ae81edb49a87e92772504e3242f8b51fc03d9273
gokuldharan <gokul.dharan@mail.utoronto.ca>
2018-07-28 14:40:12 -0400

    Adding C++ template

---
+++ Templates/example_cpp.cpp
@@ -0,0 +1,112 @@
+/**
+  *****************************************************************************
+  * @file    template_cpp.c
+  * @author  TODO -- your name here
+  * @brief   TODO -- brief description of file
+  *
+  * @defgroup TODO -- module name
+  * @brief    TODO -- description of module
+  *****************************************************************************
+  */
+
+
+
+
+/********************************* Includes **********************************/
+#include "template.h"
+
+
+
+
+/********************************** Macros ***********************************/
+
+/********************************** Anonymous Namespace **********************/
+namespace {
+          
+/********************************** Local Helpers ****************************/                                              
+
+}
+
+/********************************** Primary Namespace ************************/
+using namespace Namespace;
+
+/********************************* Constants *********************************/
+
+
+
+
+/********************************** Types ************************************/
+
+
+
+
+/****************************** Public Variables *****************************/
+
+
+
+
+/***************************** Private Variables *****************************/
+
+
+
+
+/************************ Private Function Prototypes ************************/
+
+
+
+
+/******************************** Functions **********************************/
+/*****************************************************************************/
+/*  Submodule name                                                           */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/*****************************************************************************/
+/**
+ * @defgroup TODO -- define the submodule (something like "Module_SubmoduleName Submodule name")
+ * @brief    TODO -- describe submodule here
+ *
+ * # TODO -- Page title for submodule #
+ *
+ * TODO -- detailed description of submodule
+ *
+ * @ingroup TODO -- name of the parent module
+ * @{
+ */
+
+// TODO: put functions here
+
+/**
+ * @}
+ */
+/* end TODO -- Module_SubmoduleName */
+
+/*****************************************************************************/
+/*  Submodule name                                                           */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/*                                                                           */
+/*****************************************************************************/
+/**
+ * @defgroup Test Member Functions
+ * @brief    TODO -- describe submodule here
+
+ *
+ * @ingroup Namespace
+ * @{
+ */
+
+int Class::testMe(int a, const char *s) {
+    return 1;
+}
+
+/**
+ * @}
+ */
+/* end Test Member Functions */
---
+++ Templates/example_cpp.h
@@ -0,0 +1,89 @@
+/**
+  *****************************************************************************
+  * @file    template.h
+  * @author  TODO -- your name here
+  * @brief   TODO -- briefly describe this file
+  *
+  * @defgroup TODO -- module name defined in template.c with " (header)" after it
+  * @brief    TODO -- brief description of header
+  * @ingroup  TODO -- module name defined in template.c
+  * @{
+  *****************************************************************************
+  */
+
+
+
+
+/******************** Define to prevent recursive inclusion ******************/
+#ifndef __TEMPLATE_H__
+#define __TEMPLATE_H__
+
+
+
+
+/********************************* Includes **********************************/
+#include <stdint.h>
+
+
+
+
+/********************************** Macros ***********************************/
+
+
+
+/********************************** Namespace ********************************/
+/*!
+ *  \addtogroup Namespace
+ *  @{
+ */
+namespace Namespace {
+/********************************* Constants *********************************/
+
+
+
+
+/********************************** Classes **********************************/
+//!  A test class. 
+/*!
+  A more elaborate class description.
+*/
+class Class {
+
+/****************************** Public Members *******************************/
+  public:
+    //! A constructor.
+    /*!
+      A more elaborate description of the constructor.
+    */
+    Class();
+    //! A destructor.
+    /*!
+      A more elaborate description of the destructor.
+    */
+   ~Class();
+    //! A normal member taking two arguments and returning an integer value.
+    /*!
+      \param a an integer argument.
+      \param s a constant character pointer.
+      \return The test results
+    */
+    int testMe(int a,const char *s);       
+
+/****************************** Protected Members ***************************/
+    protected:
+            
+                       
+/****************************** Private Members *****************************/
+    private:
+
+}; //class Class
+
+
+
+} //namespace Namespace
+/**
+ * @}
+ */
+/* end TODO -- module name defined on line 7 */
+
+#endif /* __TEMPLATE_H__ */
