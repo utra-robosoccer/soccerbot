6b77ac8e291d1a65405e1f9fe526177b86c4389b
han210 <40050591+han210@users.noreply.github.com>
2018-07-28 15:12:11 -0400

    ADC Conversion(1 sensor value)
    -0 to 4027
    -12 bit transmission(2^12)
    
    
    Former-commit-id: 61afd4ed070718978f9ddd9d5289f8add7037b03 [formerly 2f66a770a1cae933d71290eaed14cdb0eeb447f8]
    Former-commit-id: 333f04ecf130d1977230096b49ff9a9d27b9ef12

--- Development/Foot_Pressure_Sensor_Standalone/MCU-Side/Foot_Pressure_Sensor_v1/Src/adc.c
+++ Development/Foot_Pressure_Sensor_Standalone/MCU-Side/Foot_Pressure_Sensor_v1/Src/adc.c
@@ -58,14 +58,14 @@ void MX_ADC1_Init(void)
     /**Common config 
     */
   hadc1.Instance = ADC1;
-  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV2;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV8;
   hadc1.Init.Resolution = ADC_RESOLUTION_12B;
   hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
-  hadc1.Init.ScanConvMode = ADC_SCAN_ENABLE;
+  hadc1.Init.ScanConvMode = ADC_SCAN_DISABLE;
   hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
   hadc1.Init.LowPowerAutoWait = DISABLE;
   hadc1.Init.ContinuousConvMode = ENABLE;
-  hadc1.Init.NbrOfConversion = 4;
+  hadc1.Init.NbrOfConversion = 1;
   hadc1.Init.DiscontinuousConvMode = DISABLE;
   hadc1.Init.NbrOfDiscConversion = 1;
   hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
@@ -82,37 +82,10 @@ void MX_ADC1_Init(void)
     */
   sConfig.Channel = ADC_CHANNEL_5;
   sConfig.Rank = ADC_REGULAR_RANK_1;
-  sConfig.SamplingTime = ADC_SAMPLETIME_640CYCLES_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_47CYCLES_5;
   sConfig.SingleDiff = ADC_SINGLE_ENDED;
   sConfig.OffsetNumber = ADC_OFFSET_NONE;
   sConfig.Offset = 0;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
-
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_6;
-  sConfig.Rank = ADC_REGULAR_RANK_2;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
-
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_8;
-  sConfig.Rank = ADC_REGULAR_RANK_3;
-  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
-  {
-    _Error_Handler(__FILE__, __LINE__);
-  }
-
-    /**Configure Regular Channel 
-    */
-  sConfig.Channel = ADC_CHANNEL_9;
-  sConfig.Rank = ADC_REGULAR_RANK_4;
   if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
@@ -133,15 +106,12 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef* adcHandle)
     __HAL_RCC_ADC_CLK_ENABLE();
   
     /**ADC1 GPIO Configuration    
-    PA0     ------> ADC1_IN5
-    PA1     ------> ADC1_IN6
-    PA3     ------> ADC1_IN8
-    PA4     ------> ADC1_IN9 
+    PA0     ------> ADC1_IN5 
     */
-    GPIO_InitStruct.Pin = LFOOT_FL_Pin|LFOOT_FR_Pin|LFOOT_BL_Pin|LFOOT_BR_Pin;
+    GPIO_InitStruct.Pin = LFOOT_FL_Pin;
     GPIO_InitStruct.Mode = GPIO_MODE_ANALOG_ADC_CONTROL;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+    HAL_GPIO_Init(LFOOT_FL_GPIO_Port, &GPIO_InitStruct);
 
     /* ADC1 DMA Init */
     /* ADC1 Init */
@@ -182,12 +152,9 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef* adcHandle)
     __HAL_RCC_ADC_CLK_DISABLE();
   
     /**ADC1 GPIO Configuration    
-    PA0     ------> ADC1_IN5
-    PA1     ------> ADC1_IN6
-    PA3     ------> ADC1_IN8
-    PA4     ------> ADC1_IN9 
+    PA0     ------> ADC1_IN5 
     */
-    HAL_GPIO_DeInit(GPIOA, LFOOT_FL_Pin|LFOOT_FR_Pin|LFOOT_BL_Pin|LFOOT_BR_Pin);
+    HAL_GPIO_DeInit(LFOOT_FL_GPIO_Port, LFOOT_FL_Pin);
 
     /* ADC1 DMA DeInit */
     HAL_DMA_DeInit(adcHandle->DMA_Handle);
