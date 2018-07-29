f4ecdf819543c3b6435f2be55280ad279fcbc71c
rfairley <rob.t.fairley@gmail.com>
2018-07-29 14:45:14 -0400

    Use static IP rather than DHCP
    This is to reduce complexity while testing.
    Note the host PC needed its network resetting in order to
    detect the IP change.

--- Development/Ethernet/lwip-rtos-config/lwip-rtos-config/Inc/main.h
+++ Development/Ethernet/lwip-rtos-config/lwip-rtos-config/Inc/main.h
@@ -119,25 +119,25 @@
 /* #define USE_FULL_ASSERT    1U */
 
 /* USER CODE BEGIN Private defines */
-#define USE_DHCP       /* enable DHCP, if disabled static address is used*/
+//#define USE_DHCP       /* enable DHCP, if disabled static address is used*/
 
-/*Static IP ADDRESS*/
-#define IP_ADDR0   192
-#define IP_ADDR1   168
-#define IP_ADDR2   0
-#define IP_ADDR3   10
-
-/*NETMASK*/
-#define NETMASK_ADDR0   255
-#define NETMASK_ADDR1   255
-#define NETMASK_ADDR2   255
-#define NETMASK_ADDR3   0
-
-/*Gateway Address*/
-#define GW_ADDR0   192
-#define GW_ADDR1   168
-#define GW_ADDR2   0
-#define GW_ADDR3   1
+///*Static IP ADDRESS*/
+//#define IP_ADDR0   192
+//#define IP_ADDR1   168
+//#define IP_ADDR2   0
+//#define IP_ADDR3   10
+//
+///*NETMASK*/
+//#define NETMASK_ADDR0   255
+//#define NETMASK_ADDR1   255
+//#define NETMASK_ADDR2   255
+//#define NETMASK_ADDR3   0
+//
+///*Gateway Address*/
+//#define GW_ADDR0   192
+//#define GW_ADDR1   168
+//#define GW_ADDR2   0
+//#define GW_ADDR3   1
 
 /* USER CODE END Private defines */
 
--- Development/Ethernet/lwip-rtos-config/lwip-rtos-config/Src/lwip.c
+++ Development/Ethernet/lwip-rtos-config/lwip-rtos-config/Src/lwip.c
@@ -76,6 +76,9 @@ struct netif gnetif;
 ip4_addr_t ipaddr;
 ip4_addr_t netmask;
 ip4_addr_t gw;
+uint8_t IP_ADDRESS[4];
+uint8_t NETMASK_ADDRESS[4];
+uint8_t GATEWAY_ADDRESS[4];
 
 /* USER CODE BEGIN 2 */
 
@@ -86,13 +89,27 @@ ip4_addr_t gw;
   */
 void MX_LWIP_Init(void)
 {
+  /* IP addresses initialization */
+  IP_ADDRESS[0] = 192;
+  IP_ADDRESS[1] = 168;
+  IP_ADDRESS[2] = 0;
+  IP_ADDRESS[3] = 55;
+  NETMASK_ADDRESS[0] = 255;
+  NETMASK_ADDRESS[1] = 255;
+  NETMASK_ADDRESS[2] = 255;
+  NETMASK_ADDRESS[3] = 0;
+  GATEWAY_ADDRESS[0] = 192;
+  GATEWAY_ADDRESS[1] = 168;
+  GATEWAY_ADDRESS[2] = 0;
+  GATEWAY_ADDRESS[3] = 2;
+  
   /* Initilialize the LwIP stack with RTOS */
   tcpip_init( NULL, NULL );
 
-  /* IP addresses initialization with DHCP (IPv4) */
-  ipaddr.addr = 0;
-  netmask.addr = 0;
-  gw.addr = 0;
+  /* IP addresses initialization without DHCP (IPv4) */
+  IP4_ADDR(&ipaddr, IP_ADDRESS[0], IP_ADDRESS[1], IP_ADDRESS[2], IP_ADDRESS[3]);
+  IP4_ADDR(&netmask, NETMASK_ADDRESS[0], NETMASK_ADDRESS[1] , NETMASK_ADDRESS[2], NETMASK_ADDRESS[3]);
+  IP4_ADDR(&gw, GATEWAY_ADDRESS[0], GATEWAY_ADDRESS[1], GATEWAY_ADDRESS[2], GATEWAY_ADDRESS[3]);
 
   /* add the network interface (IPv4/IPv6) with RTOS */
   netif_add(&gnetif, &ipaddr, &netmask, &gw, NULL, &ethernetif_init, &tcpip_input);
@@ -123,9 +140,6 @@ void MX_LWIP_Init(void)
   /* Create the Ethernet link handler thread */
   osThreadDef(LinkThr, ethernetif_set_link, osPriorityNormal, 0, configMINIMAL_STACK_SIZE * 2);
   osThreadCreate (osThread(LinkThr), &link_arg);
-
-  /* Start DHCP negotiation for a network interface (IPv4) */
-  dhcp_start(&gnetif);
 
 /* USER CODE BEGIN 3 */
 
