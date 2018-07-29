59ea800a4746bc9caf239f89e8ad1f3b0bbde68b
rfairley <rob.t.fairley@gmail.com>
2018-07-29 14:07:54 -0400

    Make init thread (defaultTask) same size as example
    Former-commit-id: 3f6d1bd0da0c29c934c4f12d10ba8a5ede6b588d [formerly b074df1c2e15a60cdbaf9201a8bdf48bf6a562b8]
    Former-commit-id: 774fa99fc53b9df10d0eae9f0956d4c0ab1152af

--- Development/Ethernet/lwip-rtos-config/lwip-rtos-config/Src/freertos.c
+++ Development/Ethernet/lwip-rtos-config/lwip-rtos-config/Src/freertos.c
@@ -80,7 +80,23 @@ void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
 
 /* USER CODE END FunctionPrototypes */
 
+/* GetIdleTaskMemory prototype (linked to static allocation support) */
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize );
+
 /* Hook prototypes */
+
+/* USER CODE BEGIN GET_IDLE_TASK_MEMORY */
+static StaticTask_t xIdleTaskTCBBuffer;
+static StackType_t xIdleStack[configMINIMAL_STACK_SIZE];
+  
+void vApplicationGetIdleTaskMemory( StaticTask_t **ppxIdleTaskTCBBuffer, StackType_t **ppxIdleTaskStackBuffer, uint32_t *pulIdleTaskStackSize )
+{
+  *ppxIdleTaskTCBBuffer = &xIdleTaskTCBBuffer;
+  *ppxIdleTaskStackBuffer = &xIdleStack[0];
+  *pulIdleTaskStackSize = configMINIMAL_STACK_SIZE;
+  /* place for user code */
+}                   
+/* USER CODE END GET_IDLE_TASK_MEMORY */
 
 /* Init FreeRTOS */
 
@@ -103,7 +119,7 @@ void MX_FREERTOS_Init(void) {
 
   /* Create the thread(s) */
   /* definition and creation of defaultTask */
-  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 640);
   defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
 
   /* USER CODE BEGIN RTOS_THREADS */
