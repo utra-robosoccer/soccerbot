3bc73ca83392cc23a7b06b319bb5fd90e099db27
rfairley <rob.t.fairley@gmail.com>
2018-09-22 11:25:01 -0400

    add showing distribution of times

--- Testing/Ethernet/plot_from_quick_stats.py
+++ Testing/Ethernet/plot_from_quick_stats.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import math
 import numpy
 import json
 import sys
@@ -8,10 +9,7 @@ import sys
 filenames = sys.argv[1:]
 
 def nearest_decimal_up(num):
-    decimal_up = pow(10, -15) # Unlikely to get values lower than this
-    while decimal_up < num:
-        decimal_up *= 10
-    return decimal_up
+    return round(num, -int(math.ceil(math.log10(abs(num)))))
 
 for filename in filenames:
     json_data = {}
@@ -36,8 +34,9 @@ for filename in filenames:
     y["average"] = numpy.array([json_data[str(k)]["average"] if k in x_collected else numpy.nan for k in x_normalized])
     y["std_dev"] = ([json_data[str(k)]["std_dev"] if k in x_collected else numpy.nan for k in x_normalized])
     y["max"] = numpy.array([json_data[str(k)]["max"] if k in x_collected else numpy.nan for k in x_normalized])
+    y["times"] = [numpy.array(json_data[str(k)]["the_times"]) for k in x_collected]
 
-    plt.figure()
+    plt.figure(figsize=(4, 8))
 
     plt.subplot(211)
 
@@ -57,10 +56,20 @@ for filename in filenames:
     plt.ylabel('max echo time (seconds)')
     plt.title('Average echo time - ' + filename)
     plt.axis([0, numpy.amax(x_collected), 0, nearest_decimal_up(numpy.amax(y_collected["max"]))])
-
-    print (numpy.amax(y_collected["max"]))
+    
+    plt.subplots_adjust(left  = 0.125, right = 0.9, bottom = 0.1, top = 0.9, wspace = 0.2, hspace = 0.360)
     plt.suptitle('Ethernet echo test statistics')
-    plt.show()
 
-    # TODO: add subplot for max, subplot for distribution
 
+    plt.figure(figsize=(4, x_collected.size))
+    
+    for i in range(x_collected.size):
+        plt.subplot(x_collected.size, 1, i + 1)
+        plt.hist(y["times"][i], 50, density=1, facecolor='g', alpha=0.75)
+        plt.title('Histogram of echo times for message size ' + str(x_collected[i]) + ' bytes')
+        plt.xlabel('time (seconds)')
+        plt.ylabel('probability')
+
+    plt.subplots_adjust(left  = 0.125, right = 0.9, bottom = 0.1, top = 0.9, wspace = 0.2, hspace = 0.360)
+
+plt.show()
