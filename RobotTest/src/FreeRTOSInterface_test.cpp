841fce2906b2494b983838dad1c98893654f0262
izaakniksan <izaakniksan@gmail.com>
2018-09-23 18:12:54 -0400

    FreeRTOSInterface.h completed
    Former-commit-id: fb1d6e3c80dc9393caaa480c699066e9ecbf4db1 [formerly c652e3af1c6f5e9ed9241ba0014069edc2396cbb]
    Former-commit-id: 50c4b298aa039258dab150754819867f4bb79309

--- RobotTest/src/FreeRTOSInterface_test.cpp
+++ RobotTest/src/FreeRTOSInterface_test.cpp
@@ -14,21 +14,9 @@
 
 namespace {
 
-class MockUdpInterface : public udp_interface::UdpInterface {
+class MockFreeRTOSInterface : public FreeRTOS_Interface::FreeRTOSInterface {
 public:
-    MOCK_METHOD0(udpNew, bool());
-    MOCK_METHOD0(udpBind, bool());
-    MOCK_METHOD0(udpRecv, bool());
-    MOCK_METHOD0(udpRemove, bool());
-    MOCK_METHOD0(ethernetifInput, bool());
-    MOCK_METHOD0(udpConnect, bool());
-    MOCK_METHOD0(udpSend, bool());
-    MOCK_METHOD0(udpDisconnect, bool());
-    MOCK_METHOD0(pbufFreeRx, bool());
-    MOCK_METHOD0(pbufFreeTx, bool());
-    MOCK_METHOD0(waitRecv, bool());
-    MOCK_METHOD1(packetToBytes, bool(uint8_t*));
-    MOCK_METHOD1(bytesToPacket, bool(const uint8_t*));
+
 };
 
 }
