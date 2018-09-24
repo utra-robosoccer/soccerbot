0cf3d365c98a73d15806ba33628e0cef8d03d6cc
han210 <40050591+han210@users.noreply.github.com>
2018-09-24 01:15:16 -0400

    Revert "Reformatted Tabs to 4 Spaces"
    This reverts commit b1fbf236a20092311e56a444648cf27f2cf95c21.

--- Robot/Inc/rx_helper.h
+++ Robot/Inc/rx_helper.h
@@ -1,10 +1,11 @@
 /**
- *****************************************************************************
- * @file    rx_helper.h
- * @author  Hannah
- * @brief   Header for helper file for function StartRXTask in freertos.cpp
- *****************************************************************************
- */
+  *****************************************************************************
+  * @file    rx_helper.h
+  * @author  Hannah
+  * @brief   Header for helper file for function StartRXTask in freertos.cpp
+  *****************************************************************************
+  */
+
 
 #ifndef RX_HELPER_H
 #define RX_HELPER_H
@@ -12,6 +13,8 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+
 
 /********************************* Includes **********************************/
 #include <stdint.h>
@@ -22,6 +25,8 @@ void initiateDMATransfer(void);
 void updateStatusToPC(void);
 void waitForNotificationRX(void);
 void initializeVars(void);
+
+
 
 #ifdef __cplusplus
 }
