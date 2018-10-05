83a2df4dc0a178cf26ed39b4e74363c37831c38f
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-10-05 01:36:27 -0400

    Revert "Remove robot project as part of merge"
    This reverts commit f9a188f946194baf0ed74c0febce1f1f9fa9cf24 [formerly af23fabc7622c1a171efa7ba1619f980df63ae47] [formerly 27fd26fe4929499971ece3e228a2d21fa90af5ac [formerly fdf5865c752598f40f3ee4abe1f45de4dafa86d9]].
    
    
    Former-commit-id: 4a249a6334887ec782f73c2b1af4c8219353e317 [formerly a7463795b16bd5824b84780e61a8525ecba37657]
    Former-commit-id: 7f57b45f183e7be67a2452771f0ed02a76b4d03e

---
+++ Robot/Inc/imu_helper.h
@@ -0,0 +1,57 @@
+/**
+  *****************************************************************************
+  * @file    imu_helper.h
+  * @author  Tyler Gamvrelis
+  * @brief   Helper file for the IMU thread in the main program flow
+  *
+  * @defgroup Header
+  * @addtogroup Helpers
+  * @{
+  *****************************************************************************
+  */
+
+
+
+
+#ifndef IMU_HELPER_H
+#define IMU_HELPER_H
+
+
+
+
+/********************************* Includes **********************************/
+#include "../Drivers/MPU6050/MPU6050.h"
+
+using namespace IMUnamespace;
+
+
+
+
+/********************************* Helpers ***********************************/
+namespace Helpers{
+// Functions
+// ----------------------------------------------------------------------------
+/**
+ * @brief Initialize angular velocity FIR filters for IMU data
+ */
+void initAngularVelocityFilters();
+
+/**
+ * @brief Reads Vz, Vy, and Vx from the MPU6050 object and writes them into
+ *        their corresponding FIR filters. The output of each FIR filter is
+ *        read and stored in the MPU6050 object where it was previously read
+ * @param IMUdata Reference to MPU6050 object
+ */
+void filterAngularVelocity(IMUStruct& imu);
+
+} // end namespace Helpers
+
+
+
+
+/**
+ * @}
+ */
+/* end - Header */
+
+#endif /* IMU_HELPER_H */
