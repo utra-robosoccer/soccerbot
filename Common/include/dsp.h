53abc4071bd441a0d30e7854d3d90b2f1b6d7fcd
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2018-10-07 21:49:46 -0400

    Move MicroModeler DSP license to file with coefficients
    That's the only thing we are using from the tool anyway.
    
    
    Former-commit-id: 71c5d45c9b1a93a202270c0ecea6e0ff54287532 [formerly 82e70ae37782f93d5a28222291eeed8b329d661b]
    Former-commit-id: ca514b743a57f4e9d800411a27fe1cad60f038ba

--- Common/include/dsp.h
+++ Common/include/dsp.h
@@ -7,23 +7,6 @@
   *****************************************************************************
   */
 
-/******************************* SOURCE LICENSE *********************************
-Copyright (c) 2018 MicroModeler.
-
-A non-exclusive, nontransferable, perpetual, royalty-free license is granted to the Licensee to
-use the following Information for academic, non-profit, or government-sponsored research purposes.
-Use of the following Information under this License is restricted to NON-COMMERCIAL PURPOSES ONLY.
-Commercial use of the following Information requires a separately executed written license agreement.
-
-This Information is distributed WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-
-******************************* END OF LICENSE *********************************/
-// Link with library: libarm_cortexM4_mathL.a (or equivalent)
-// Add CMSIS/Lib/GCC to the library search path
-// Add CMSIS/Include to the include search path
-// A commercial license for MicroModeler DSP can be obtained at http://www.micromodeler.com/launch.jsp
-
 
 
 
