8d179c2b6c53fac86d172bb4c6d71dceb05ef289
rfairley <rob.t.fairley@gmail.com>
2018-10-08 21:08:11 -0400

    Robot_F7: revert PcInterface/OsInterface changes that affect Robot_F7
    Former-commit-id: c6c75f7fb33f274e55e6b50dba1a4222ab21c56d [formerly 83ed16cdae84f6e8fb17dea9ce72fef368e21d2b]
    Former-commit-id: c57844581e48c881aa07a89b27f1fe511475712a

--- Robot_F7/Inc/FreeRTOSConfig.h
+++ Robot_F7/Inc/FreeRTOSConfig.h
@@ -102,15 +102,11 @@
 #define configTICK_RATE_HZ                       ((TickType_t)1000)
 #define configMAX_PRIORITIES                     ( 7 )
 #define configMINIMAL_STACK_SIZE                 ((uint16_t)128)
-#define configTOTAL_HEAP_SIZE                    ((size_t)25600)
+#define configTOTAL_HEAP_SIZE                    ((size_t)15360)
 #define configMAX_TASK_NAME_LEN                  ( 16 )
-#define configUSE_TRACE_FACILITY                 1
-#define configUSE_STATS_FORMATTING_FUNCTIONS     1
 #define configUSE_16_BIT_TICKS                   0
 #define configUSE_MUTEXES                        1
 #define configQUEUE_REGISTRY_SIZE                8
-#define configUSE_RECURSIVE_MUTEXES              1
-#define configUSE_COUNTING_SEMAPHORES            1
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION  1
 
 /* Co-routine definitions. */
@@ -123,11 +119,10 @@ to exclude the API function. */
 #define INCLUDE_uxTaskPriorityGet           1
 #define INCLUDE_vTaskDelete                 1
 #define INCLUDE_vTaskCleanUpResources       0
-#define INCLUDE_vTaskSuspend                0
+#define INCLUDE_vTaskSuspend                1
 #define INCLUDE_vTaskDelayUntil             1
 #define INCLUDE_vTaskDelay                  1
 #define INCLUDE_xTaskGetSchedulerState      1
-#define INCLUDE_xTaskResumeFromISR          0
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
