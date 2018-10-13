bf34c62140f8313a00530c9cb5eb45628fd3a4d4
Jason Wang <vuwij@me.com>
2018-10-14 05:12:22 +0900

    Fixes
    Former-commit-id: 9abeb48bfa127e67b39fd22c1a67b47680a9f88e [formerly 2465b093546cc990c191ac8ffcb288ea693b15de]
    Former-commit-id: 430de6899a41c8e5447efbd83c89222bc8c5d585

--- soccerbot/soccer_hardware/setup.py
+++ soccerbot/soccer_hardware/setup.py
@@ -2,8 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['soccer_hardware'],
-    scripts=['nodes/soccer_hardware.py'],
+    packages=['soccer_communication'],
     package_dir={'': 'src'},
     requires=['soccer_msgs','serial','std_msgs', 'rospy', 'message_filters', 'gps_common', 'sensor_msgs', 'prettytable']
 )
