4f0ff94c5fa74a9a10fe851730cac8b370edee7b
han210 <40050591+han210@users.noreply.github.com>
2018-10-13 14:57:05 -0400

    Class I2CInterface not recognized
    Former-commit-id: e8e18778e1f889b8110c6aa1bded1ada2c030536 [formerly 644a51bb73446100ad3f9d3b1653543cb82e4f0c]
    Former-commit-id: 94a88a51434c1f80c973acc98f39e225fdfb80d3

---
+++ Common/hardware/HalI2cInterface.cpp
@@ -0,0 +1,89 @@
+///**
+//  *****************************************************************************
+//  * @file    template_cpp.cpp
+//  * @author  TODO -- your name here
+//  * @brief   TODO -- brief description of file
+//  *
+//  * @defgroup TODO -- module name
+//  * @brief    TODO -- description of module
+//  * @{
+//  *****************************************************************************
+//  */
+//
+///********************************* Includes **********************************/
+//#include "HalI2CInterface.h"
+//#include "stm32f7xx_hal_i2c.h"
+//
+///********************************** Globals **********************************/
+//// Constants
+//// ----------------------------------------------------------------------------
+//
+///******************************** File-local *********************************/
+//namespace i2c{
+//// Classes and structs
+//// ----------------------------------------------------------------------------
+//
+//// Variables
+//// ----------------------------------------------------------------------------
+//
+//// Functions
+//void HALI2CInterface(){
+//	;
+//}
+//void ~HALI2CInterface(){
+//	;
+//}
+//
+//void assignHandle(I2C_HandleTypeDef *hi2c){
+//	hi2c->Instance = hi2c;
+//}
+//
+//HAL_StatusTypeDef memWrite(uint16_t DevAddress, uint16_t MemAddress,
+//			uint16_t MemAddSize, uint8_t *pData, uint16_t Size,
+//			uint32_t Timeout){ //HAL_I2C_Mem_Write
+//
+//	return HAL_I2C_Mem_Write(DevAddress, MemAddress, MemAddSize, pData, Size, Timeout);
+//}
+//	HAL_StatusTypeDef memRead(uint16_t DevAddress, uint16_t MemAddress,
+//			uint16_t MemAddSize, uint8_t *pData, uint16_t Size,
+//			uint32_t Timeout); //HAL_I2C_Mem_Read
+//	HAL_StatusTypeDef memWriteIT(uint16_t DevAddress, uint16_t MemAddress,
+//			uint16_t MemAddSize, uint8_t *pData, uint16_t Size); //HAL_I2C_Mem_Write_IT
+//	HAL_StatusTypeDef memReadIT(uint16_t DevAddress, uint16_t MemAddress,
+//			uint16_t MemAddSize, uint8_t *pData, uint16_t Size);//HAL_I2C_Mem_Read_IT
+//// ----------------------------------------------------------------------------
+//
+//
+//
+//} // end anonymous namespace
+//
+//
+//
+//
+///************************** insert class name here ***************************/
+//// Public
+//// ----------------------------------------------------------------------------
+//
+//
+//
+//
+//// Protected
+//// ----------------------------------------------------------------------------
+//
+//
+//
+//
+//// Private
+//// ----------------------------------------------------------------------------
+//
+//
+//
+//
+///**
+// * @}
+// */
+///* end - module name */
+//
+//
+//
+//
---
+++ Common/include/I2CInterface.h
@@ -0,0 +1,67 @@
+/*
+ * I2CInterface.h
+ *
+ *  Created on: Oct 6, 2018
+ *      Author: Hannah
+ */
+
+/**
+ *****************************************************************************
+ * @file    template_cpp.h
+ * @author  TODO -- your name here
+ * @brief   TODO -- briefly describe this file
+ *
+ * @defgroup Header
+ * @ingroup  TODO -- module name defined in template_cpp.cpp
+ * @{
+ *****************************************************************************
+ */
+
+#ifndef I2CINTERFACE_H
+#define I2CINTERFACE_H
+
+/********************************* Includes **********************************/
+#include <cstdint>
+#include "SystemConf.h"
+#include "i2c.h"
+
+
+/********************************** Globals **********************************/
+// Constants
+// ----------------------------------------------------------------------------
+/************************** insert module name here **************************/
+namespace i2c {
+// Constanst/ Types & enums
+// ----------------------------------------------------------------------------
+
+// Classes and Structs
+class I2CInterface {
+public:
+	virtual ~I2CInterface(){};
+//	virtual void assignHandle(I2C_HandleTypeDef *hi2c) = 0;
+//
+////	HAL-related members
+//	virtual HAL_StatusTypeDef memWrite(uint16_t DevAddress, uint16_t MemAddress,
+//			uint16_t MemAddSize, uint8_t *pData, uint16_t Size,
+//			uint32_t Timeout) = 0; //HAL_I2C_Mem_Write
+//	virtual HAL_StatusTypeDef memRead(uint16_t DevAddress, uint16_t MemAddress,
+//			uint16_t MemAddSize, uint8_t *pData, uint16_t Size,
+//			uint32_t Timeout) = 0; //HAL_I2C_Mem_Read
+//	virtual HAL_StatusTypeDef memWriteIT(uint16_t DevAddress,
+//			uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData,
+//			uint16_t Size) = 0; //HAL_I2C_Mem_Write_IT
+//	virtual HAL_StatusTypeDef memReadIT(uint16_t DevAddress,
+//			uint16_t MemAddress, uint16_t MemAddSize, uint8_t *pData,
+//			uint16_t Size) = 0;	//HAL_I2C_Mem_Read_IT
+
+};
+
+// ----------------------------------------------------------------------------
+
+// Functions
+// ----------------------------------------------------------------------------
+
+}// end namespace module_name
+
+
+#endif /* COMMON_INCLUDE_I2CINTERFACE_H_ */
