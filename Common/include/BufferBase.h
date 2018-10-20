3185e72473bb59dbb2862bb3daafdcbe73236fa0
gokuldharan <gokul.dharan@mail.utoronto.ca>
2018-10-20 15:12:38 -0400

    Adding Buffer Class
    Former-commit-id: f1b53b12dac3297e734d74f254c00f6eb81f9eb7 [formerly 22e49e37909064a94eb916a25e687fad08b516b6]
    Former-commit-id: f378ffdc6f6d8d77435a103fd428ef07842c2b56

---
+++ Common/include/BufferBase.h
@@ -0,0 +1,91 @@
+/**
+  *****************************************************************************
+  * @file    BufferBase.h
+  * @author  Gokul Dharan
+  * @brief   Defines an abstract buffer class to be implemented and extended as needed
+  *
+  * @defgroup Header
+  * @{
+  *****************************************************************************
+  */
+
+// NOTE: defgroup used above since there is no .cpp file associated with this class.
+
+#ifndef __BUFFER_BASE_H__
+#define __BUFFER_BASE_H__
+
+/********************************** Includes **********************************/
+#include <cstdio>
+#include <memory>
+#include <stdint.h>
+#include "UART_Handler.h"
+
+namespace buffer {
+
+
+/*********************************** Types ************************************/
+/**
+ * @brief Generic templated buffer type
+ */
+
+template <class T>
+class BufferBase
+{
+public:
+    BufferBase()
+    {
+        m_data_buf = std::unique_ptr<T>(new T);
+    }
+    ~BufferBase() {};
+    void write(T item)
+    {
+        *m_data_buf = item;
+        m_empty = false;
+    }
+    T read()
+    {
+        if (m_empty)
+        {
+            return T();
+        }
+        m_empty = true;
+        return *m_data_buf;
+    }
+    void reset()
+    {
+        m_empty = true;
+    }
+    bool is_empty()
+    {
+        return m_empty;
+    }
+
+    // Defining methods here in the declaration for ease of use as a templated class
+private:
+    //TODO: Thread-safety
+	std::unique_ptr<T> m_data_buf;
+	bool m_empty = true;
+};
+
+/**
+ * @brief Easily extendable master class that holds all relevant buffers
+ */
+class BufferMaster
+{
+public:
+    BufferMaster(){};
+    ~BufferMaster(){};
+
+    BufferBase<IMUStruct> IMUBuffer;
+    BufferBase<Dynamixel_HandleTypeDef> MotorBuffer[NUM_MOTORS];
+    // Add buffer items here as necessary
+};
+
+} // end namespace buffer
+
+/**
+ * @}
+ */
+/* end - BufferBase */
+
+#endif /* __BUFFER_BASE_H__ */
