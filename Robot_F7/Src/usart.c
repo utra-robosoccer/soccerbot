199eb16c82a9d37af2fce43c124132e39cae99da
rfairley <rob.t.fairley@gmail.com>
2018-11-27 23:55:13 -0500

    CubeMX: set UART5 RX DMA to circular
    Former-commit-id: a46f1a0b878beff9d649ccb44518ecbea5c6d942 [formerly 2d28c43bf0a0818e19b3dd44649f6d0360e78f0d]
    Former-commit-id: efaeb542c3ea6a418deeea9a217a7f451b689479

--- Robot_F7/Src/usart.c
+++ Robot_F7/Src/usart.c
@@ -322,7 +322,7 @@ void HAL_UART_MspInit(UART_HandleTypeDef* uartHandle)
     hdma_uart5_rx.Init.MemInc = DMA_MINC_ENABLE;
     hdma_uart5_rx.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
     hdma_uart5_rx.Init.MemDataAlignment = DMA_MDATAALIGN_BYTE;
-    hdma_uart5_rx.Init.Mode = DMA_NORMAL;
+    hdma_uart5_rx.Init.Mode = DMA_CIRCULAR;
     hdma_uart5_rx.Init.Priority = DMA_PRIORITY_LOW;
     hdma_uart5_rx.Init.FIFOMode = DMA_FIFOMODE_DISABLE;
     if (HAL_DMA_Init(&hdma_uart5_rx) != HAL_OK)
