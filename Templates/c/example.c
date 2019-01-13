95bfa4659f47d94d064e9a1e370c455f74dbba7b
Tyler Gamvrelis <ty.gamvrelis@gmail.com>
2019-01-13 02:58:15 -0500

    Indent using spaces, correct error in comment, add newline at EOF, remove visual separator between functions

--- Templates/c/example.c
+++ Templates/c/example.c
@@ -25,7 +25,7 @@ const float MIN_POS = 0.0;
 /***************************** Public Functions ******************************/
 bool servo_set_position(servo_t* h_servo, float angle){
     if(angle < MIN_POS || angle > MAX_POS){
-    	return false;
+        return false;
     }
 
     uint16_t raw = (uint16_t)((angle / MAX_POS) * (POS_RESOLUTION - 1));
@@ -36,13 +36,11 @@ bool servo_set_position(servo_t* h_servo, float angle){
     uart_send_byte((raw >> 8) & 0xFF);
 }
 
-// ----------------------------------------------------------------------------
-
 void servo_read_position(servo_t* h_servo){
     uart_send_byte(h_servo->m_id);
-	uart_send_byte(REG_CURRENT_POS);
-	uint16_t raw = uart_recv_byte();
-	uint16_t raw |= uart_recv_byte() << 8;
+    uart_send_byte(REG_CURRENT_POS);
+    uint16_t raw = uart_recv_byte();
+    uint16_t raw |= uart_recv_byte() << 8;
 
-	last_position = (raw / (POS_RESOLUTION - 1)) * MAX_POS;
+    last_position = (raw / (POS_RESOLUTION - 1)) * MAX_POS;
 }
