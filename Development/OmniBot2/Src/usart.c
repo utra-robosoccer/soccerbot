f1a1d1577189fc546ee56b58da2a2a7dee59ae81
sherrychen127 <sherrychen127@gmail.com>
2019-01-19 15:37:51 -0500

    integrated PC interface and motor angle control

--- Development/OmniBot2/Src/usart.c
+++ Development/OmniBot2/Src/usart.c
@@ -135,6 +135,26 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* uartHandle)
 } 
 
 /* USER CODE BEGIN 1 */
+void MX_USART2_UART_Init_X(uint32_t baudRate)
+{
+
+  huart2.Instance = USART2;
+  huart2.Init.BaudRate = baudRate;
+  huart2.Init.WordLength = UART_WORDLENGTH_8B;
+  huart2.Init.StopBits = UART_STOPBITS_1;
+  huart2.Init.Parity = UART_PARITY_NONE;
+  huart2.Init.Mode = UART_MODE_TX_RX;
+  huart2.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+  huart2.Init.OverSampling = UART_OVERSAMPLING_16;
+  huart2.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+  huart2.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+  if (HAL_UART_Init(&huart2) != HAL_OK)
+  {
+    _Error_Handler(__FILE__, __LINE__);
+  }
+
+}
+
 
 /* USER CODE END 1 */
 
