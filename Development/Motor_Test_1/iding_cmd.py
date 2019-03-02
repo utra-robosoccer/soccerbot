9487bcfc9dde9a63663c4fc924197ea3ac112c01
han210 <hans.lee@mail.utoronto.ca>
2019-03-02 14:45:49 -0500

    Improvements on PC Usability
    1. Assigning a device name -> Reading the port name from the first connected port
    2. Moving the argument parser to the beginning of the script
    3. Splitting the id_string argument into motorType and idNumber, and updating the "help" description

--- Development/Motor_Test_1/iding_cmd.py
+++ Development/Motor_Test_1/iding_cmd.py
@@ -6,27 +6,40 @@ import serial.tools.list_ports
 '''
 VALID ID INPUT: _ _ _ _ *4 characters in length*
   - first two characters: AX, M1, M2
+        - AX : IDing AX12As
+        - M1:  IDing MX28s at 57600 bps
+        - M2:  IDing MX28s at 1 Mbps
   - last two characters: 01, 02, ... , 12, 13, ..
   - in the command line, write without quotation marks
 '''
 
+# parse command line 
+parser = argparse.ArgumentParser(description='Process command lines')
+parser.add_argument('motorType', action = 'store', help='stores a string with 2 characters, where the first two characters (must be capital letters) represent a motor type: AX(IDing AX12As), M1(IDing MX28s at 57600 bps), M2(IDing MX28s at 1 Mbps)')
+parser.add_argument('idNumber', action = 'store', help='stores a string with the motor number: (01,02,..,13,14...)')
+
 # port information
-port = 'COM6'
+ports = list(serial.tools.list_ports.comports())
+if (len(ports)==0):
+    raise Exception('No ports detected')
+for port in ports:
+#   Takes in the first connected port
+    dev_name = port.device
+
+port = dev_name
 baud = 115200
   
 ser = serial.Serial(port, baud, timeout=1)
 if ser.isOpen():
      print(ser.name + ' is open...\n')
 
-# parse command line 
-parser = argparse.ArgumentParser(description='Process command lines')
-parser.add_argument('id_string', action = 'store', help='stores a string with 4 characters, where the first two characters (must be capital letters) represent a motor type (AX, M1, M2) and the last two characters represent the motor number (01,02,..,13,14...)')
-
 id_string = None
 
-# parser.add_argument('--id', action='store')
 args = parser.parse_args()
-id_string = args.id_string
+motorType = args.motorType
+idNumber = args.idNumber
+id_string = motorType + idNumber
+
 if(len(id_string)!=4):
     print("ERROR: The motor id argument must be 4 characters")
     exit()
