f36283fa58c1dd006dde2698eed66a8014422f44
Nam Nguyen <lkunelk@gmail.com>
2019-07-07 23:23:27 -0700

    add initial files
    Former-commit-id: f9c1df779a6d990bef9a364e7c05af32e38f1e2c [formerly cb5290ab36cba00535220c5e8fff0a78303a0718]
    Former-commit-id: 6af79d73ba0245581ca1af6609a5b997ab01a1c9

---
+++ soccerbot/soccer_fieldline_detection/include/soccer_fieldline_detection/soccer_fieldline_detector.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+/* The soccer fieldine detector class (TODO)
+ * 1. add geometry_msgs/Pose2d into the Cmakelist.txt and package.xml of soccer_foundations
+ * 2. IN soccer_foundation in the geometry library, create a class called Pose3D that inherits from geometry_msgs::Pose2D
+ * 3. Create Quaternion to RPY function in Pose2D, And then create getYaw, getPitch, getRoll functions in the class, use this library http://wiki.ros.org/tf2/Tutorials/Quaternions. And create a test for it
+ * 4. Create the transformation matrix function that returns a 4x4 transformation matrix from the rpy and quaternion. Google the math
+ * 5. Create the image class with the data members being the same from the matlab code
+ * 5. Create the camera class with the data members being the same from the matlab code
+ * 6. Create the draw frame test function (ask me later)
+ * */
+
+// Camera contains an Image, Camera contains a Pose2d
---
+++ soccerbot/soccer_fieldline_detection/src/soccer_fieldline_detector.cpp
@@ -0,0 +1,6 @@
+#include <ros/ros.h>
+#include <soccer_fieldline_detection/soccer_fieldline_detector.hpp>
+
+int main() {
+    return 0;
+}
