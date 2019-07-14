59e9fdd8976587434ed0e887a019a077bc9c5e77
Nam <lkunelk@gmail.com>
2019-07-14 10:42:31 -0700

    create cpp skeleton for some of the vision classes

---
+++ soccerbot/soccer_fieldline_detection/src/camera.cpp
@@ -0,0 +1,9 @@
+#include <soccer_fieldline_detection/camera.hpp>
+
+Camera::Camera(const Pose3 &pose, int resolution_y, int resolution_x) {
+
+}
+
+void Camera::DrawPixelRayTrace(int pixel_y, int pixel_x) {
+
+}
