a8d2f332f0102cfd900e4dba0529571fbeafdb33
Nam <lkunelk@gmail.com>
2019-07-30 23:20:41 -0400

    set up test to stream video for testing line detection node

--- soccerbot/soccer_fieldline_detection/src/soccer_fieldline_detector.cpp
+++ soccerbot/soccer_fieldline_detection/src/soccer_fieldline_detector.cpp
@@ -1,13 +1,35 @@
 #include <ros/ros.h>
-#include <soccer_fieldline_detection/soccer_fieldline_detector.hpp>
+#include <soccer_fieldline_detection/test_soccer_fieldline_detector.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 SoccerFieldlineDetector::SoccerFieldlineDetector() {
     image_transport::ImageTransport it(nh);
-    image_subscriber = it.subscribe("in_image_base_topic", 1, &SoccerFieldlineDetector::imageCallback, this);
+    image_subscriber = it.subscribe("front_camera/image_raw", 1, &SoccerFieldlineDetector::imageCallback, this);
 }
 
 void SoccerFieldlineDetector::imageCallback(const sensor_msgs::ImageConstPtr &msg) {
+    ROS_INFO_STREAM("received image");
 
+    try {
+        const cv::Mat image = cv_bridge::toCvShare(msg, "bgr8")->image;
+        cv::Mat inverted;
+        cv::bitwise_not(image, inverted);
+        cv::imshow("view", inverted);
+        cv::waitKey(1);
+    }
+    catch (cv_bridge::Exception &e) {
+        ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+    }
+    // Cover Horizon (Ignore for now)
+
+    // Detect Field Lines (Copy from simulink)
+
+    // Organize Fieldlines
+
+    // Project fieldlines from 2d to 3d
+
+    // Publish fieldlines in a LaserScan data format
 }
 
 int main(int argc, char **argv) {
