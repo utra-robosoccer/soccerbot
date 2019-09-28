7db5817d32b5e5638a42dcdbe5b61de7931e2b0f
Jonathan Spraggett <jonathanspraggett@gmail.com>
2019-09-28 15:05:37 -0400

    Added FindFloorCoordinate function
    Former-commit-id: c556e0799ad5df4ec809a479fb8c1147ed1d1200 [formerly 26bb97158b8cbf0239ae27d4e4755edbd0f9a852]
    Former-commit-id: b312c0d5e0f2956d56a7d3cd9f327df56c7d2fe5

--- soccerbot/soccer_fieldline_detection/test/test_soccer_fieldline_detector.cpp
+++ soccerbot/soccer_fieldline_detection/test/test_soccer_fieldline_detector.cpp
@@ -1,12 +1,16 @@
 #include <gtest/gtest.h>
 #include <ros/ros.h>
+#include <soccer_fieldline_detection/camera.hpp>
 
 class SoccerFieldlineDetectorFixture : public ::testing::Test {
 protected:
 };
 
-TEST_F(SoccerFieldlineDetectorFixture, TestAdder) {
-
+TEST_F(SoccerFieldlineDetectorFixture, CameraFindFloorCoordinate) {
+    Camera camera();
+    Point2 p1  (3,4);
+    Point3 p2 = Camera::FindFloorCoordinate(3,4);
+    ASSERT(3dpoint = 3dpoint);
 }
 
 
