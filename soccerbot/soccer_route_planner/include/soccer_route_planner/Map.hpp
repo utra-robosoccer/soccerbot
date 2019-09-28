a5aeaa0086e0342a6bf7f20e519d24f82c8e74f0
luiscameroo <luis.camero@gmail.com>
2019-09-27 23:25:50 -0400

    Added framework for Map class to hold occupancy grid and run RRT

---
+++ soccerbot/soccer_route_planner/include/soccer_route_planner/Map.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <stdio.h>
+#include <vector>
+
+class Map
+{
+private:
+    float m_InflationRadius;
+    float m_Resolution;
+    int m_nNodes;
+    int m_Height;
+    int m_Width;
+
+public:
+    Map();
+};
---
+++ soccerbot/soccer_route_planner/src/Map.cpp
@@ -0,0 +1,11 @@
+#include <soccer_route_planner/Map.hpp>
+
+Map::Map()
+{
+    m_InflationRadius = 0.2;
+    m_Resolution = 0.05;
+    m_nNodes = 2000;
+    m_Height = 9;
+    m_Width = 6;
+    return
+}
