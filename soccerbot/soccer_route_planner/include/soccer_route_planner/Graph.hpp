b374014ccd256869a8ef144ff79050ec797ae37f
ljagodz <ljagodz@gmail.com>
2019-10-03 21:47:24 -0700

    Implemented simple Graph class for RRT star

--- soccerbot/soccer_route_planner/include/soccer_route_planner/Graph.hpp
+++ soccerbot/soccer_route_planner/include/soccer_route_planner/Graph.hpp
@@ -4,25 +4,35 @@
 #include <vector>
 #include <iostream>
 #include <geometry_msgs/Pose2D.h>
+#include <map>
 
 class Graph
 {
 private:
-    std::vector<struct node*> adj_list;
+    std::vector<struct node> adj_list;
+    std::map<int, struct node_info> node_info_map;
+
     int num_nodes;
+    int id_generator;
+
+    int find_vertex(int id); // Returns index of vertex within adjacency list.
 public:
     Graph();
 
     int add_vertex(float x, float y); // Returns id of the vertex.
-    void add_edge(int id_0, int id_1);
-    void delete_vertex();
-    void delete_edge();
+    void add_edge(int id_0, int id_1); // Create edge from node id_0 to node id_1.
+    void delete_edge(int id_0, int id_1);
+
+    struct node_info get_node_info(int id);
+};
+
+struct node_info {
+    bool is_alive = true;
+    geometry_msgs::Pose2D location;
 };
 
 struct node {
-    int id;
-    geometry_msgs::Pose2D location;
-    struct node *next;
+    int id = -1;
+    std::vector<int> neighbours;
 };
-
 
--- soccerbot/soccer_route_planner/src/Graph.cpp
+++ soccerbot/soccer_route_planner/src/Graph.cpp
@@ -1,37 +1,51 @@
-//
-// Created by ljagodz on 9/7/19.
-//
-
 #include <soccer_route_planner/Graph.hpp>
 
 Graph::Graph() {
     num_nodes = 0;
+    id_generator = 0;
 }
 
 int Graph::add_vertex(float x, float y) {
-    struct node* temp = new struct node;
+    struct node new_node;
 
-    temp->id = (adj_list[adj_list.size() - 1]->id)++;
-
-    temp->location.x = x;
-    temp->location.y = y;
-    temp->location.theta = 0;
+    new_node.id = id_generator;
 
     num_nodes++;
+    id_generator++;
 
-    adj_list.push_back(temp);
+    adj_list.push_back(new_node);
 
-    return temp->id;
+    struct node_info temp;
+    temp.location.theta = 0;
+    temp.location.x = x;
+    temp.location.y = y;
+    temp.is_alive = true;
+
+    node_info_map[new_node.id] = temp;
+
+    return new_node.id;
 }
 
 void Graph::add_edge(int id_0, int id_1) {
-
-}
-
-void Graph::delete_vertex(int id) {
-
+    int index = find_vertex(id_0);
+    adj_list[index].neighbours.push_back(id_1);
 }
 
 void Graph::delete_edge(int id_0, int id_1) {
-
+    int index = find_vertex(id_0);
+    int i = 0;
+    while(adj_list[index].neighbours[i] != id_1) i++;
+    adj_list[index].neighbours.erase(adj_list[index].neighbours.begin() + i);
 }
+
+int Graph::find_vertex(int id) {
+    int i = 0;
+    while(adj_list[i].id != id && i < num_nodes) i++;
+    return i;
+}
+
+struct node_info Graph::get_node_info(int id) {
+    return node_info_map[id];
+}
+
+
