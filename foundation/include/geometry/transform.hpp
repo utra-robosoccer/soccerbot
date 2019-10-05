8859d7b00325239bec3a477d36ac2e56e3e47d21
Jonathan Spraggett <jonathanspraggett@gmail.com>
2019-10-05 16:25:30 -0400

    Added the transfrom class and revised the FindFloorCoordinate function
    Former-commit-id: 38c7d59417def5819386c6d6460d64f0e6f00ae5 [formerly 1af333724d8973d30168b9c1fda0141c508fbb80]
    Former-commit-id: 7e4c8f65a5214c59de62ea1fb98ba9f73055a4fe

--- foundation/include/geometry/transform.hpp
+++ foundation/include/geometry/transform.hpp
@@ -6,3 +6,25 @@
 #define SRC_TRANSFORM_HPP
 
 #endif //SRC_TRANSFORM_HPP
+
+class transform{
+
+
+
+public:
+
+    transform(float pos []);
+
+    transform(float pos [], float orient []);
+
+    float * rotm2quat(float (&array) [4][4]);
+
+    float ** quat2tform(float orient []);
+
+    float orientation [4] = {1,0,0,0};
+    float position [3] = {0,0,0};
+
+    transform ApplyTransformation(transform a, transform b);
+
+    float **H(transform c);
+};
--- soccerbot/soccer_fieldline_detection/src/camera.cpp
+++ soccerbot/soccer_fieldline_detection/src/camera.cpp
@@ -1,10 +1,11 @@
 #include <soccer_fieldline_detection/camera.hpp>
 #include <geometry/point3.hpp>
-
+#include <geometry/transform.hpp>
 Camera::Camera(const Pose3 &pose, int resolution_y, int resolution_x) {
     Camera::resolution_x = resolution_x;
     Camera::resolution_y = resolution_y;
     Camera::pose = pose;
+
 
 }
 
@@ -12,14 +13,12 @@ void Camera::DrawPixelRayTrace(int pixel_y, int pixel_x) {
 
 }
 
-
-
 float Camera::VerticalFOV() {
-    return Camera::resolution_y / sqrt(Camera::resolution_x^2 + Camera::resolution_y^2);
+    return Camera::resolution_y / sqrt(Camera::resolution_x ^ 2 + Camera::resolution_y ^ 2);
 }
 
 float Camera::HorizontalFOV() {
-    return Camera::resolution_x / sqrt(Camera::resolution_x^2 + Camera::resolution_y^2);
+    return Camera::resolution_x / sqrt(Camera::resolution_x ^ 2 + Camera::resolution_y ^ 2);
 }
 
 float Camera::ImageSensorHeight() {
@@ -33,46 +32,99 @@ float Camera::ImageSensorWidth() {
 
 }
 
-int Camera::PixelHeight(){
+int Camera::PixelHeight() {
     return Camera::ImageSensorHeight() / Camera::resolution_y;
 
 }
 
-int Camera::PixelWidth(){
+int Camera::PixelWidth() {
     return Camera::ImageSensorWidth() / Camera::resolution_x;
 
 }
 
-int Camera::ImageSensorLocation_X (int pos_x, int pos_y) {
+int Camera::ImageSensorLocation_X(int pos_x, int pos_y) {
 
     return (pos_x - Camera::resolution_x / 2) * PixelWidth();
 
 }
 
-int Camera::ImageSensorLocation_Y (int pos_x, int pos_y) {
+int Camera::ImageSensorLocation_Y(int pos_x, int pos_y) {
 
     return (pos_y - Camera::resolution_y / 2) * PixelHeight();
 
 }
 
 Point3 Camera::FindFloorCoordinate(int pos_x, int pos_y) {
-    int tx = Camera::ImageSensorLocation_X(pos_x,pos_y);
-    int ty = Camera::ImageSensorLocation_Y(pos_x,pos_y);
+    float tx = Camera::ImageSensorLocation_X(pos_x, pos_y);
+    float ty = Camera::ImageSensorLocation_Y(pos_x, pos_y);
 
-   // int  pixelrelLocation3d = Geometry.Transform(Camera::focal_length / 1000, tx / 1000, ty / 1000]);
-    Camera::pose.position.x = focal_length / 1000;
-    Camera::pose.position.y = tx / 1000;
-    Camera::pose.position.z = ty / 1000;
+    float tmp1 [3]= {focal_length / 1000, tx / 1000, ty / 1000};
+    transform pixelrelLocation3d = transform(tmp1);
 
-    float H [4] [4];
+    float tmp2 [3]= {static_cast<float>(Camera::pose.position.x),static_cast<float>(Camera::pose.position.y),static_cast<float>(Camera::pose.position.z)};
+    transform t2 = transform(tmp2);
+
+    transform pixelLocation3d = pixelrelLocation3d.ApplyTransformation(pixelrelLocation3d,t2);
+
+
+    //Raytrace that pixel onto the floor
+    float ratio = (pixelLocation3d.position[2]- Camera::pose.position.z) / Camera::pose.position.z;
+    float xdelta = (pixelLocation3d.position[0] - Camera::pose.position.x) / ratio;
+    float ydelta = (pixelLocation3d.position[1]- Camera::pose.position.y) / ratio;
+
+    Point3  point (Camera::pose.position.x - xdelta,Camera::pose.position.y - ydelta,0);
+
+    return point;
+
+
+
+
+
+
+
+    /*//Initializing the H matrix
+    float H1[4][4];
+    float H2[4][4];
+    float Hnew[4][4];
     for (int i = 0; i < 4; i++) {
         for (int j = 0; j < 4; j++) {
-            H[i][j] = 0;
+            H1[i][j] = 0;
+
+            H2[i][j] = 0;
+            Hnew[i][j] = 0;
         }
     }
-    H[1][4] = Camera::pose.position.x ;
-    H[2][4] = Camera::pose.position.y ;
-    H[3][4] = Camera::pose.position.z ;
+    H1[0][3] = focal_length / 1000;
+    H1[1][3] = tx / 1000;
+    H1[2][3] = ty / 1000;
+
+    H2[0][3] = Camera::pose.position.x;
+    H2[1][3] = Camera::pose.position.y;
+    H2[2][3] = Camera::pose.position.z;
+
+    //Matrix multiplication
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+           for (int k = 0; k< 4; k++){
+               Hnew[i][j] +=  H1[i][k] * H2[k][j];
+           }
+
+        }
+    }
+
+    //Coordinate of pixel in real space
+    float pixelLocation3d [3] = {Hnew[0][3],Hnew[1][3],Hnew[2][3]};
+
+    //Raytrace that pixel onto the floor
+    float ratio = (pixelLocation3d[2] - Camera::pose.position.z) / Camera::pose.position.z;
+    float xdelta = (pixelLocation3d[0] - Camera::pose.position.x) / ratio;
+    float ydelta = (pixelLocation3d[1] - Camera::pose.position.y) / ratio;
+
+    Point3  point (Camera::pose.position.x - xdelta,Camera::pose.position.y - ydelta,0);
+
+    return point;*/
+
+
 
 }
 /*
