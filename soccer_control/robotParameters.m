edef81141a2b4aaa280fdb7b1783e64938929c42
Jason Wang <vuwij@me.com>
2019-10-12 13:46:35 +0900

    Jw simulink model (#46)
    * First commit
    
    * Auto build urdf
    
    * Cleans up gazebo faster
    
    * Cleans up gazebo faster
    
    * Cleanup
    
    * Cleaning up
    
    * Cleanup cmake
    
    * Cleaning up
    
    * Cleaning up
    
    * Wrong location
    
    * Added control code
    
    
    Former-commit-id: a811628a57a107ca9c800fcc608dee073410371b [formerly a9dc603a7d26e3655a834643e9e62e206f5749f8]
    Former-commit-id: 81e816db04e6a6da12f4a668a496c30aa378981c

---
+++ soccer_control/robotParameters.m
@@ -0,0 +1,30 @@
+% General World parameters
+density = 1000;
+
+% Field parameters
+plane_dim = [9 6 0.025];
+
+% Contact/friction parameters
+contact_stiffness = 2500;
+contact_damping = 100;
+mu_k = 0.6;
+mu_s = 0.8;
+mu_vth = 0.1;
+contact_point_radius = 1e-4;
+
+% Foot Parameters
+cleat_length = 0.022;
+foot_box = [0.09 0.07 0.01474 + cleat_length];
+left_collision_center = [0.00385 0.00401 -0.00737];
+
+left_foot_to_corner_1 = left_collision_center + [foot_box(1)/2 foot_box(2)/2 -foot_box(3)];
+left_foot_to_corner_2 = left_collision_center + [foot_box(1)/2 -foot_box(2)/2 -foot_box(3)];
+left_foot_to_corner_3 = left_collision_center + [-foot_box(1)/2 -foot_box(2)/2 -foot_box(3)];
+left_foot_to_corner_4 = left_collision_center + [-foot_box(1)/2 foot_box(2)/2 -foot_box(3)];
+
+right_collision_center = left_collision_center;
+right_collision_center(2) = -right_collision_center(2);
+right_foot_to_corner_1 = right_collision_center + [foot_box(1)/2 foot_box(2)/2 -foot_box(3)];
+right_foot_to_corner_2 = right_collision_center + [foot_box(1)/2 -foot_box(2)/2 -foot_box(3)];
+right_foot_to_corner_3 = right_collision_center + [-foot_box(1)/2 -foot_box(2)/2 -foot_box(3)];
+right_foot_to_corner_4 = right_collision_center + [-foot_box(1)/2 foot_box(2)/2 -foot_box(3)];
