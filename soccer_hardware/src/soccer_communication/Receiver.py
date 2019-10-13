6770846c10aebafd2f7082179716e0774c188fb8
Henry Prickett-Morgan <hlewispm2@gmail.com>
2019-10-12 22:38:20 -0400

    Remove unused imports

--- soccer_hardware/src/soccer_communication/Receiver.py
+++ soccer_hardware/src/soccer_communication/Receiver.py
@@ -6,14 +6,9 @@ import struct
 from transformations import *
 
 try:
-    import rospy
-    from soccer_msgs.msg import RobotGoal
     from soccer_msgs.msg import RobotState
     from sensor_msgs.msg import Imu
     from geometry_msgs.msg import Vector3
-    from geometry_msgs.msg import Quaternion
-    from tf.msg import tfMessage
-    from tf.transformations import quaternion_from_euler
 except ImportError:
     pass
 
