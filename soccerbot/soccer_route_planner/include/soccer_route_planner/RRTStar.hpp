1247654a9714856d9d2f9f9e66312158654f064d
ljagodz <ljagodz@gmail.com>
2019-10-26 11:01:30 -0700

    initial skeleton of rrtstar class

--- soccerbot/soccer_route_planner/include/soccer_route_planner/RRTStar.hpp
+++ soccerbot/soccer_route_planner/include/soccer_route_planner/RRTStar.hpp
@@ -1,14 +1,19 @@
 #pragma once
 
 #include <stdio.h>
+#include <soccer_route_planner/Graph.hpp>
+#include <soccer_route_planner/Map.hpp>
+#include <geometry_msgs/Pose2D.h>
 
 class RRTStar {
+private:
+    Map M;
 public:
     RRTStar();
-    void m_RRTStar(int r, int n);
-    void m_nearestNeighbour();
-    void m_nearVertices();
-    void m_steering();
-    void m_collisionTest();
-    void m_sample();
+    void RRTStar_func(int r, int n, geometry_msgs::Pose2D x_init, geometry_msgs::Pose2D x_goal);
+    void nearestNeighbour();
+    void nearVertices();
+    void steering();
+    void collisionTest();
+    geometry_msgs::Pose2D sample();
 };
--- soccerbot/soccer_route_planner/src/RRTStar.cpp
+++ soccerbot/soccer_route_planner/src/RRTStar.cpp
@@ -1,29 +1,40 @@
 #include <soccer_route_planner/RRTStar.hpp>
 
+#include <stdlib.h>
+#include <ctime>
+
 RRTStar::RRTStar() {
 
 }
 
-void RRTStar::m_RRTStar(int r, int n) {
+void RRTStar::RRTStar_func(int r, int n, geometry_msgs::Pose2D x_init, geometry_msgs::Pose2D x_goal) {
+    Graph G;
+    G.add_vertex(x_init.x, x_init.y);
+
+    int i = 0;
+
+    while (i < n) {
+//        geometry_msgs::Pose2D x_rand = sample();
+        i++;
+    }
+}
+
+void RRTStar::nearestNeighbour() {
 
 }
 
-void RRTStar::m_nearestNeighbour() {
+void RRTStar::nearVertices() {
 
 }
 
-void RRTStar::m_nearVertices() {
+void RRTStar::steering() {
 
 }
 
-void RRTStar::m_steering() {
+void RRTStar::collisionTest() {
 
 }
 
-void RRTStar::m_collisionTest() {
-
-}
-
-void RRTStar::m_sample() {
+geometry_msgs::Pose2D RRTStar::sample() {
 
 }
