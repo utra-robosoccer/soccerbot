6d63cee952d5cff52c32115ec5d2ca61c1fc9c75
Nam <lkunelk@gmail.com>
2019-10-29 10:18:31 -0400

    ball detector node prints out class names

--- soccer_fieldline_detection/src/ball_detector.cpp
+++ soccer_fieldline_detection/src/ball_detector.cpp
@@ -1,13 +1,21 @@
 #include <ros/ros.h>
+#include <darknet_ros_msgs/BoundingBoxes.h>
+#include <darknet_ros_msgs/BoundingBox.h>
+#include <vector>
 
-void ballDetectorCallback() {
+void ballDetectorCallback(const darknet_ros_msgs::BoundingBoxes::ConstPtr &msg) {
+    std::vector <darknet_ros_msgs::BoundingBox> b = msg->bounding_boxes;
+    for (darknet_ros_msgs::BoundingBox box: b) {
+        ROS_INFO("%s", box.Class.data());
+    }
+
     ROS_INFO("received image");
 }
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "ball_detector");
     ros::NodeHandle n;
-//    ros::Subscriber sub = n.subscribe("chatter", 1000, ballDetectorCallback);
+    ros::Subscriber sub = n.subscribe("darknet_ros/bounding_boxes", 1000, ballDetectorCallback);
     ROS_INFO("hello i'm ball detector!");
     ros::spin();
     return 0;
