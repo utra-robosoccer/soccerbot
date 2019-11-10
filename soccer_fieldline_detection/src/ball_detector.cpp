0705cfa8574fd3198262b2c217ba9a0987c0b9e3
Jason Wang <vuwij@me.com>
2019-11-09 23:31:33 -0500

    Add
    Former-commit-id: c507d4a31e5fe8536a96e310e578afa6e8ffd899 [formerly 7bb0e4f0077be1253bdf10f733f60f9be7c60793]
    Former-commit-id: 0478ec013cec45549540405c43893d1d76b08d45

--- soccer_fieldline_detection/src/ball_detector.cpp
+++ soccer_fieldline_detection/src/ball_detector.cpp
@@ -6,52 +6,85 @@
 #include <geometry/pose3.hpp>
 #include <geometry_msgs/Pose.h>
 #include <soccer_fieldline_detection/camera.hpp>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
 
 class BallDetector {
 public:
     ros::NodeHandle n;
     ros::Subscriber darknet_pose_sub;
-    ros::Publisher ball_pub;
-    BallDetector() {
+    std::unique_ptr<Camera> camera;
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener;
+    tf2_ros::TransformBroadcaster br;
+
+    BallDetector() : tfListener(tfBuffer) {
         darknet_pose_sub = n.subscribe("darknet_ros/bounding_boxes", 1000, &BallDetector::ballDetectorCallback, this);
-        ball_pub = n.advertise<geometry_msgs::Pose>("ball_pose", 1);
+
+        geometry_msgs::TransformStamped camera_pose;
+
+        while(ros::ok()) {
+            try{
+                camera_pose = tfBuffer.lookupTransform("camera", "base_link",
+                                                            ros::Time(0), ros::Duration(1.0));
+                break;
+            }
+            catch (tf2::TransformException &ex) {
+                std::string s = ex.what();
+            }
+        }
+
+        // Initalize Camera
+        Pose3 camera_position;
+        camera_position.position.x = camera_pose.transform.translation.x;
+        camera_position.position.y = camera_pose.transform.translation.y;
+        camera_position.position.z = camera_pose.transform.translation.z;
+        camera_position.orientation.w = camera_pose.transform.rotation.w;
+        camera_position.orientation.x = camera_pose.transform.rotation.x;
+        camera_position.orientation.y = camera_pose.transform.rotation.y;
+        camera_position.orientation.z = camera_pose.transform.rotation.z;
+        camera = std::make_unique<Camera>(camera_position,240,360);
     }
 
 private:
     void ballDetectorCallback(const darknet_ros_msgs::BoundingBoxes::ConstPtr &msg) {
-        std::vector <darknet_ros_msgs::BoundingBox> b = msg->bounding_boxes;
+        // Get transformation
+        geometry_msgs::TransformStamped camera_pose;
+        try{
+            camera_pose = tfBuffer.lookupTransform("camera", "base_link",
+                                                   ros::Time(0), ros::Duration(0.1));
+        }
+        catch (tf2::TransformException &ex) {
+            return;
+        }
 
-        // Project fieldlines from 2d to 3d
-        Pose3 camera_position;
-        camera_position.position.x = -0.5;
-        camera_position.position.y = -0.5;
-        camera_position.position.z = 0.5;
-
-        camera_position.orientation.w = 0.8536;
-        camera_position.orientation.x = -0.1464;
-        camera_position.orientation.y = 0.3536;
-        camera_position.orientation.z = 0.3536;
-
-        Camera cam (camera_position,240,360);
-
-        for (darknet_ros_msgs::BoundingBox box: b) {
+        for (const darknet_ros_msgs::BoundingBox& box: msg->bounding_boxes) {
             //ROS_INFO("found a %s", box.Class.data());
             std::string objectClass = box.Class;
-            if(objectClass != "person") {
+            if(objectClass != "sports ball") {
                 continue;
             }
             // For now take the center of the box
             int xavg = (box.xmin + box.xmax) / 2;
             int yavg = (box.ymin + box.ymax) / 2;
-            Point3 floor_coordinate =  cam.FindFloorCoordinate(xavg,yavg);
-            geometry_msgs::Pose ball_pose;
-            ball_pose.position.x = floor_coordinate.x;
-            ball_pose.position.y = floor_coordinate.y;
-            ball_pose.position.z = floor_coordinate.z;
-            ball_pub.publish(ball_pose);
+            Point3 floor_coordinate =  camera->FindFloorCoordinate(xavg,yavg);
+
+            geometry_msgs::TransformStamped camera_pose;
+            camera_pose.header.frame_id = "base_link";
+            camera_pose.child_frame_id = "ball";
+            camera_pose.transform.translation.x = floor_coordinate.x;
+            camera_pose.transform.translation.y = floor_coordinate.y;
+            camera_pose.transform.translation.z = floor_coordinate.z;
+            camera_pose.transform.rotation.x = 0;
+            camera_pose.transform.rotation.y = 0;
+            camera_pose.transform.rotation.z = 0;
+            camera_pose.transform.rotation.w = 1;
+
+            BallDetector::br.sendTransform(camera_pose);
         }
     }
 };
+
 
 int main(int argc, char **argv) {
     ros::init(argc, argv, "ball_detector");
