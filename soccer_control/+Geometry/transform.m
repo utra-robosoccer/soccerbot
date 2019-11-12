efe6964ae07b339c9cc288adf31baea2c0815908
Jason Wang <vuwij@me.com>
2019-11-11 19:32:52 -0500

    Jw walking (#55)
    * Fixed slx
    
    * Manual inverse kinematics done
    
    * Finished making path for robot
    
    * Add all
    
    * add stf
    
    
    Former-commit-id: b7f74e5234ff81d68d20d2071c6206e8330956ee [formerly 6f311c61b316f77452c1b1c6290fbe4da712bde7]
    Former-commit-id: 1baae85ed3e4cad222e47d8ffd8cf8e3c64e83c1

--- soccer_control/+Geometry/transform.m
+++ soccer_control/+Geometry/transform.m
@@ -2,37 +2,62 @@ classdef transform < handle
     %Transform a general position on the field
     
     properties
-        % Position
-        position = [0,0,0];
-
-        % In Quaternion Format
-        orientation = [1,0,0,0];
+        % H Matrix
+        H = [1 0 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
     end
     
     methods
-        function obj = transform(position, orientation)
-            if nargin >= 2
-                obj.position = position;
-            end
-            if nargin >= 2
-                obj.orientation = orientation;
+        function obj = transform(arg1, arg2, arg3, arg4)
+            if nargin == 1
+                if (size(arg1) == 4)
+                    obj.H = arg1;
+                else
+                    obj.setPosition(arg1);
+                end
+            elseif nargin == 2
+                obj.setPosition(arg1);
+                obj.setOrientation(arg2);
+            elseif nargin == 4
+                a = arg1;
+                alpha = arg2;
+                d = arg3;
+                theta = arg4;
+                
+                obj.H = [cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta); ...
+                    sin(theta) cos(theta)*cos(alpha) -cos(theta)*sin(alpha) a*sin(theta); ...
+                    0 sin(alpha) cos(alpha) d; ...
+                    0 0 0 1];
             end
         end
         
         function objout = mtimes(obj, t2)
-            Hnew = (t2.H) * (obj.H);
-            objout = Geometry.Transform([Hnew(1,4), Hnew(2,4), Hnew(3,4)], rotm2quat(Hnew(1:3, 1:3)));
+            objout = Geometry.transform();
+            objout.H = obj.H * t2.H;
         end
         
-        function H = H(obj)
-            H = quat2tform(obj.orientation);
-            H(1,4) = obj.position(1);
-            H(2,4) = obj.position(2);
-            H(3,4) = obj.position(3);
+        function quat = orientation(obj)
+            quat = tform2quat(obj.H);
+        end
+        
+        function setOrientation(obj, rot)
+            obj.H(1:3,1:3) = quat2rotm(rot);
+        end
+        
+        function pos = position(obj)
+            pos = obj.H(1:3,4)';
+        end
+        
+        function setPosition(obj, pos)
+            obj.H(1:3,4) = pos';
         end
         
         function rpy = rpy(obj)
             rpy = quat2eul(obj.orientation);
+        end
+    end
+    methods(Static)
+        function distance = distance(p1, p2)
+            distance = norm(p1.H(1:3,4) - p2.H(1:3,4));
         end
     end
 end
