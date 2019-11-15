142fdf25b10d1fc84d1d70f174285811e14da6e6
Jonathan <jonathanspraggett@gmail.com>
2019-11-15 16:20:36 -0500

    clean

--- soccer_fieldline_detection/include/soccer_fieldline_detection/test_soccer_fieldline_detector.hpp
+++ soccer_fieldline_detection/include/soccer_fieldline_detection/test_soccer_fieldline_detector.hpp
@@ -6,41 +6,33 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/LaserScan.h>
 #include <geometry_msgs/PoseWithCovarianceStamped.h>
-
-/* The soccer fieldine detector class (TODO)
- * 1. add geometry_msgs/Pose2d into the Cmakelist.txt and package.xml of soccer_foundations
- * 2. IN soccer_foundation in the geometry library, create a class called Pose3D that inherits from geometry_msgs::Pose2D
- * 3. Create Quaternion to RPY function in Pose2D, And then create getYaw, getPitch, getRoll functions in the class, use this library http://wiki.ros.org/tf2/Tutorials/Quaternions. And create a test for it
- * 4. Create the transformation matrix function that returns a 4x4 transformation matrix from the rpy and quaternion. Google the math
- * 5. Create the image class with the data members being the same from the matlab code
- * 5. Create the camera class with the data members being the same from the matlab code
- * 6. Create the draw frame test function (ask me later)
- * */
-
-
-/*
- * int test create image_transport pub
- * use opncv to load video file
- * use image_transport to convert video to image_transport and publish video
- * result subscriber print received image
- */
-// Camera contains an Image, Camera contains a Pose2d
+#include <opencv-3.3.1-dev/opencv2/core/cvdef.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/transform_broadcaster.h>
 
 class SoccerFieldlineDetector {
 
     image_transport::Subscriber image_subscriber;
-    Pose3 pose;
+    ros::Publisher point_cloud_publisher;
+    std::unique_ptr<Camera> camera;
 
+    int cannythreshold1 = 200;
+    int cannythreshold2 = 400;
+
+    double rho = 1;
+    double theta = CV_PI / 180;
+    int threshold = 50;
+    int minLineLength = 50;
+    int maxLineGap = 50;
+
+    // Camera Pose subscriber
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener;
+    tf2_ros::TransformBroadcaster br;
 public:
     ros::NodeHandle nh;
 
     SoccerFieldlineDetector();
 
     void imageCallback(const sensor_msgs::ImageConstPtr &msg);
-
-    void initPose (double x, double y, double theta);
-
-    void callback(const sensor_msgs::PointCloud2ConstPtr& msg);
-
-    void pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg);
 };
