e3038c49792ab5ff4658abbc86cbb38c8f738173
Jason Wang <vuwij@me.com>
2019-11-24 14:40:06 -0500

    Reformatted folders and files
    Former-commit-id: b95ef48dde90d36b63e3ac6f76cec07e9b53f2a9 [formerly 3670eedebce4487927b82c6452dd06a8dd122d3e]
    Former-commit-id: 02dda58f74272050ef62643cd59d1474c7a79e93

--- soccer_hardware/src/soccer_hardware/comm.py
+++ soccer_hardware/src/soccer_hardware/comm.py
@@ -1,11 +1,9 @@
-# Comm.py
+# comm.py
 
 import time
-import os
-import numpy as np
 from prettytable import PrettyTable
-from Transmitter import Transmitter
-from Receiver import Receiver
+from transmitter import Transmitter
+from receiver import Receiver
 from utility import *
 
 try:
@@ -17,28 +15,29 @@ try:
 except ImportError:
     pass
 
+
 class Comm:
     def __init__(self):
         self.first = True
-        
+
     def start_up(self, ser, ros_is_on, traj, step_is_on):
         self.last_print_time = time.time()
-        
-        if(not self.first):
+
+        if (not self.first):
             self.ser = ser
             self.tx.change_port(ser)
             self.rx.change_port(ser)
             return
-        
+
         self.first = False
         self.ros_is_on = ros_is_on
         self.step_is_on = step_is_on
         self.use_trajectory = False
-        
+
         self.tx = Transmitter(ser)
         self.rx = Receiver(ser, ros_is_on)
-        
-        if(self.ros_is_on == True):
+
+        if (self.ros_is_on == True):
             rospy.init_node('soccer_hardware', anonymous=True)
             rospy.Subscriber("robotGoal", RobotGoal, self.trajectory_callback, queue_size=1)
             self.rx.pub = rospy.Publisher('soccerbot/imu', Imu, queue_size=1)
@@ -51,7 +50,7 @@ class Comm:
                     delimiter=",",
                     skiprows=0
                 )
-                
+
                 logString("Opened trajectory {0}".format(traj))
                 self.use_trajectory = True
             except IOError as err:
@@ -65,42 +64,42 @@ class Comm:
         '''
         assert sent.shape[0] == received.shape[0]
         t = PrettyTable(['Motor Number', 'Sent', 'Received'])
-        
+
         for i in range(sent.shape[0]):
             t.add_row([str(i + 1), round(sent[i][0], 4), round(received[i][0], 2)])
-        
+
         print(t)
-    
+
     def print_imu(self, received):
         ''' Prints out a numpy vector interpreted as data from the IMU, in the
             order X-gyro, Y-gyro, Z-gyro, X-accel, Y-accel, Z-accel.
         '''
-        
+
         t = PrettyTable(['', 'Gyro (deg/s)', 'Accel (m/s^2)'])
-        
+
         t.add_row(["X", round(received[0][0], 2), round(received[3][0], 2)])
         t.add_row(["Y", round(received[1][0], 2), round(received[4][0], 2)])
         t.add_row(["Z", round(received[2][0], 2), round(received[5][0], 2)])
-        
+
         print(t)
 
     def print_handler(self, goal_angles):
         current_time = time.time()
-        if(current_time - self.last_print_time >= 1):
+        if (current_time - self.last_print_time >= 1):
             self.last_print_time = current_time
             print('\n')
             logString("Received: {0}".format(self.rx.num_receptions))
             logString("Transmitted: {0}\n".format(self.tx.num_transmissions))
-            if(self.rx.num_receptions > 0):
+            if (self.rx.num_receptions > 0):
                 # Prints the last valid data received
                 self.print_angles(goal_angles[0:12], self.rx.received_angles[0:12])
                 self.print_imu(self.rx.received_imu)
-    
+
     def communicate(self, goal_angles):
         self.tx.transmit(goal_angles)
         self.rx.receive()
         self.print_handler(goal_angles)
-    
+
     def trajectory_callback(self, robotGoal):
         '''
         Used by ROS. Converts the motor array from the order and sign convention
@@ -110,26 +109,26 @@ class Comm:
         goalangles = m.dot(robotGoal.trajectories[0:18])
         goalangles = goalangles[:, np.newaxis]
         self.communicate(goalangles)
-    
+
     def begin_event_loop(self):
-        if(self.ros_is_on == True):
-            rospy.spin() 
+        if (self.ros_is_on == True):
+            rospy.spin()
         else:
-            while(True):
-                if(self.use_trajectory):
+            while (True):
+                if (self.use_trajectory):
                     # Iterate through the static trajectory forever
                     # TODO: If the static trajectories are ever re-generated, we will need
                     # TODO: to dot the trajectories with the ctrlToMcuAngleMap to shuffle
                     # TODO: them properly
                     for i in range(self.trajectory.shape[1]):
-                        if(self.step_is_on):
+                        if (self.step_is_on):
                             input('Press enter to send next pose')
-                        
-                        goal_angles = self.trajectory[:, i:i+1]
+
+                        goal_angles = self.trajectory[:, i:i + 1]
                         self.communicate(goal_angles)
                 else:
-                     # Send standing pose
-                    if(self.step_is_on):
+                    # Send standing pose
+                    if (self.step_is_on):
                         input('Press enter to send next pose')
 
-                    self.communicate(np.zeros((18,1)))
+                    self.communicate(np.zeros((18, 1)))
--- soccer_hardware/src/soccer_hardware/soccer_hardware.py
+++ soccer_hardware/src/soccer_hardware/soccer_hardware.py
@@ -8,42 +8,44 @@ Author: Tyler and Jason
 
 import sys
 import time
-from Comm import Comm
+from comm import Comm
 from utility import *
-        
+
+
 def main():
     args = parse_args()
-    
+
     ros_is_on = args['ros']
     port = args['port']
     baud = args['baud']
     traj = args['traj']
     step_is_on = args['step']
-    
+
     logString(list_ports())
-    
+
     logString("Attempting connection to embedded")
     logString("\tPort: " + port)
     logString("\tBaud rate: " + str(baud))
-    
+
     num_tries = 0
     comm = Comm()
-    while(True):
+    while (True):
         try:
             with serial.Serial(port, baud, timeout=0) as ser:
                 logString("Connected")
                 comm.start_up(ser, ros_is_on, traj, step_is_on)
                 comm.begin_event_loop()
-        
+
         except serial.serialutil.SerialException as e:
-            if(num_tries % 100 == 0):
-                if(str(e).find("FileNotFoundError")):
+            if (num_tries % 100 == 0):
+                if (str(e).find("FileNotFoundError")):
                     logString("Port not found. Retrying...(attempt {0})".format(num_tries))
                 else:
                     logString("Serial exception. Retrying...(attempt {0})".format(num_tries))
-            
+
             time.sleep(0.01)
             num_tries = num_tries + 1
+
 
 if __name__ == "__main__":
     try:
