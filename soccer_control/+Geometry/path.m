d6db0333de9ed7f154b0e9a8fee71db97702ee4e
Jason Wang <vuwij@me.com>
2019-11-28 21:40:37 -0500

    Dc walking (#58)
    * Fixed slx
    
    * Manual inverse kinematics done
    
    * Finished making path for robot
    
    * Add all
    
    * add stf
    
    * added path for foot
    
    * accounting for first step left
    
    * Temp
    
    * Accomplished enough today
    
    * Missing robot path
    
    * Main and geometry
    
    * Forgot robot
    
    * Working
    
    * Fixed inverse kinematics, fucking pain in the pass
    
    * Added simply motor publisher node
    
    * Added simple control node for ros
    
    * Fixed ikine yet again. Added more realistic hip trajectories
    
    * Added media test
    
    * Made faster stronger and better
    
    * Sure
    
    * Convetions fixed

--- soccer_control/+Geometry/path.m
+++ soccer_control/+Geometry/path.m
@@ -1,8 +1,13 @@
 classdef path < handle
     properties
-        step_size = 0.05; % Not absolutely fixed, will be modified slightly when
+        bodystep_size = 0.05;   % Not absolutely fixed, will be modified slightly when
         speed = 0.05;
-        turn_duration = 4; % Number of steps to turn
+        turn_duration = 4;      % Number of body steps to turn
+        step_size = 0.03;        % Time for a single step
+        
+        pre_footstep_ratio = 0.2;   % Ratio of fullstep duration to keep foot on ground on prefootstep
+        post_footstep_ratio = 0.2;  % Ratio of fullstep duration to keep foot on ground on postfootstep
+        
         start_transform;
         end_transform;
         
@@ -13,11 +18,17 @@ classdef path < handle
     
     methods
         function obj = path(start_transform, end_transform)
+            
+            if nargin == 0
+                start_transform = Geometry.transform([0 0 0]);
+                end_transform = Geometry.transform([0 0 0]);
+            end
+            
             obj.start_transform = start_transform;
             obj.end_transform = end_transform;
             
             % Compute approximate distance and distance map
-            precision = 0.05 * obj.step_size;
+            precision = 0.05 * obj.bodystep_size;
             precisions = precision:precision:1;
             obj.distance = 0;
             prev_pose = obj.positionAtRatio(0);
@@ -35,29 +46,30 @@ classdef path < handle
             end
             
             % Round to nearest step
-            s_count = obj.stepCount;
-            if mod(obj.distance, obj.step_size) < obj.step_size / 2
-                obj.step_size = obj.distance / s_count;
+            s_count = obj.bodyStepCount;
+            if mod(obj.distance, obj.bodystep_size) < obj.bodystep_size / 2
+                obj.bodystep_size = obj.distance / s_count;
             else
-                obj.step_size = obj.distance / (s_count + 1);
+                obj.bodystep_size = obj.distance / (s_count + 1);
             end
         end
         
-        function step = getStep(obj, n)
-            [~, idx] = min(abs((n * obj.step_size) - obj.distanceMap(:,2)));
-            step = obj.positionAtRatio(obj.distanceMap(idx, 1));
+        function bodystep = getBodyStep(obj, n)
+            [~, idx] = min(abs((n * obj.bodystep_size) - obj.distanceMap(:,2)));
+            
+            bodystep = obj.positionAtRatio(obj.distanceMap(idx, 1));
         end
         
-        function stepCount = stepCount(obj)
-            stepCount = fix(obj.distance / obj.step_size);
+        function bodyStepCount = bodyStepCount(obj)
+            bodyStepCount = fix(obj.distance / obj.bodystep_size);
         end
         
         function duration = duration(obj)
             duration = obj.distance / obj.speed;
         end
         
-        function stepTime = stepTime(obj)
-            stepTime = obj.duration / obj.stepCount;
+        function bodyStepTime = bodyStepTime(obj)
+            bodyStepTime = obj.duration / obj.bodyStepCount;
         end
         
         function pose = positionAtRatio(obj, t)
@@ -96,8 +108,8 @@ classdef path < handle
         end
         
         function show(obj)
-            for i = 0:1:obj.stepCount
-                step = obj.getStep(i);
+            for i = 0:1:obj.bodyStepCount
+                step = obj.getBodyStep(i);
                 position(i+1,1:3) = step.position;
                 orientation(i+1,1:3) = step.H(1:3,1:3) * [0.005; 0; 0];
             end
---
+++ soccer_control/+Geometry/robotpath.m
@@ -0,0 +1,130 @@
+classdef robotpath < Geometry.crotchpath
+    % Nice video for reference
+    % https://www.youtube.com/watch?v=vq9A5FD8G5w
+    
+    properties
+    end
+    
+    methods
+        function obj = robotpath(start_transform, end_transform, foot_center_to_floor)
+            obj@Geometry.crotchpath(start_transform, end_transform, foot_center_to_floor);
+        end
+
+        function show(obj)
+            show@Geometry.path(obj);
+            hold on;
+            show@Geometry.crotchpath(obj);
+            show@Geometry.footpath(obj);
+            hold off;
+        end
+        
+        function showTimingDiagram(obj)
+            
+            times = 0:obj.step_size:obj.duration;
+            i = 1;
+            for t = times
+                
+                [step_num(i), right_foot_step_ratio(i), left_foot_step_ratio(i)] = obj.footHeightRatio(t);
+                [right_foot_action, left_foot_action] = whatIsTheFootDoing(obj, step_num(i));
+                if (length(right_foot_action) == 1)
+                    right_foot_body_pose(i) = right_foot_action;
+                else
+                    right_foot_body_pose(i) = (right_foot_action(2) - right_foot_action(1))  * right_foot_step_ratio(i) + right_foot_action(1);
+                end
+                
+                if (length(left_foot_action) == 1)
+                    left_foot_body_pose(i) = left_foot_action;
+                else
+                    left_foot_body_pose(i) = (left_foot_action(2) - left_foot_action(1))  * left_foot_step_ratio(i) + left_foot_action(1);
+                end
+                
+                i = i + 1;
+            end
+            
+            % Foot Step ratio
+            subplot(3,2,1);
+            plot(times, right_foot_step_ratio);
+            hold on;
+            plot(times, left_foot_step_ratio);
+            title('Foot step Ratio');
+            xlabel('time (t)');
+            ylabel('Ratio');
+            grid off;
+            grid minor;
+            legend('Left', 'Right');
+            
+            % Foot step
+            subplot(3,2,3);
+            plot(times, step_num);
+            hold on;
+            plot(times, right_foot_body_pose);
+            plot(times, left_foot_body_pose);
+            title('Foot body pose');
+            xlabel('time (t)');
+            ylabel('Body Pose');
+            grid off;
+            grid minor;
+            legend('Step Num', 'Left', 'Right');
+            
+            % Music
+            times = 0:obj.step_size:obj.duration;
+            i = 1;
+            for t = times
+                [lfp(:,:,i), rfp(:,:,i)] = obj.footPosition(t);
+                crp(:,:,i) = obj.crotchPosition(t);
+                diff_right_foot(:,:,i) = lfp(:,:,i) / crp(:,:,i);
+                diff_left_foot(:,:,i) = rfp(:,:,i) / crp(:,:,i);
+                i = i + 1;
+            end
+            
+            subplot(3,2,2);
+            plot(times, squeeze(lfp(1,4,:)));
+            hold on;
+            plot(times, squeeze(rfp(1,4,:)));
+            plot(times, squeeze(crp(1,4,:)));
+            
+            title('X position of left, right and body');
+            xlabel('time (t)');
+            ylabel('Torso to feet (x)');
+            grid off;
+            grid minor;
+            legend('Left','Right', 'Crotch');
+            
+            subplot(3,2,5);
+            plot(times, vecnorm(squeeze(diff_right_foot(1:3,4,:))));
+            hold on;
+            plot(times, vecnorm(squeeze(diff_left_foot(1:3,4,:))));
+            title('Absolute distance between torso and foot');
+            xlabel('time (t)');
+            ylabel('Torso to feet (abs)');
+            grid off;
+            grid minor;
+            legend('Left','Right');
+            
+            subplot(3,2,4);
+            plot(times, squeeze(diff_right_foot(2,4,:)));
+            hold on;
+            plot(times, squeeze(diff_left_foot(2,4,:)));
+            title('Diff between feet and body');
+            xlabel('time (t)');
+            ylabel('Torso to feet (y)');
+            grid off;
+            grid minor;
+            legend('Left','Right');
+
+            subplot(3,2,6);
+            plot(times, squeeze(crp(3,4,:)));
+            hold on;
+            plot(times, squeeze(lfp(3,4,:)));
+            plot(times, squeeze(rfp(3,4,:)));
+            title('Z position of left, right and body');
+            xlabel('time (t)');
+            ylabel('Torso to feet (z)');
+            grid off;
+            grid minor;
+            legend('Crotch', 'Left','Right');
+
+        end
+    end
+end
+
