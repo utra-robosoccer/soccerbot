d6031f1569ae5c0d30d6fc1770d463fa1289b375
Jason Wang <vuwij@me.com>
2019-12-02 02:30:07 -0500

    AMCL and all the localization finished (#71)
    * Fixed slx
    
    * Manual inverse kinematics done
    
    * Finished making path for robot
    
    * Add all
    
    * add stf
    
    * added path for foot
    
    * accounting for first step left
    
    * Temp
    
    * Accomplished enough today
    
    * Missing robot path
    
    * Main and geometry
    
    * Forgot robot
    
    * Working
    
    * Fixed inverse kinematics, fucking pain in the pass
    
    * Added simply motor publisher node
    
    * Added simple control node for ros
    
    * Fixed ikine yet again. Added more realistic hip trajectories
    
    * Added media test
    
    * Made faster stronger and better
    
    * Sure
    
    * Convetions fixed
    
    * current file changes
    
    * Temp
    
    * Walks
    
    * Speed
    
    * Added simply motor publisher node
    
    * Added simply motor publisher node
    
    * System
    
    * used wrong transform names, now head rotates and stops when sees ball
    
    
    Former-commit-id: 30e495794eac9592448e350c6f66ee2ad29a85cf [formerly ed248605e637a1c1ce22b6de77b658bff7789349]
    Former-commit-id: b06a636ec248736d5cafecc8397ac45afb3af5eb

--- soccer_control/main.m
+++ soccer_control/main.m
@@ -1,17 +1,19 @@
 % Setup the publishers and subscribers
+close all; clear; clc;
 rosshutdown;
-rosinit('192.168.0.100', 'NodeName', '/soccer_control');
-motor_list = rosparam("get", "soccer_hardware/motor_mapping");
+rosinit('192.168.0.120', 'NodeName', 'robot1/soccer_control');
+motor_list = rosparam("get", "robot1/soccer_hardware/motor_mapping");
 
 motors = fieldnames(motor_list);
 pubs = containers.Map;
 subs = containers.Map;
 for i = 1:numel(motors)
-    pubs(motors{i}) = rospublisher(strcat(motors{i}, "/command"), "std_msgs/Float64");
-    subs(motors{i}) = rossubscriber(strcat(motors{i}, "/state"), "control_msgs/JointControllerState");
+    pubs(motors{i}) = rospublisher(strcat("robot1/", motors{i}, "/command"), "std_msgs/Float64");
+%     subs(motors{i}) = rossubscriber(strcat("robot1/", motors{i}, "/state"), "control_msgs/JointControllerState");
 end
-imu_sub = rossubscriber("imu", "sensor_msgs/Imu");
 
+imu_sub = rossubscriber("robot1/imu", "sensor_msgs/Imu");
+odom_pub = rospublisher("robot1/odom", "nav_msgs/Odometry");
 robotParameters;
 foot_center_to_floor = -right_collision_center(3) + foot_box(3);
 robot = Robot.soccerbot([-0.5, 0, hip_height], foot_center_to_floor);
@@ -21,18 +23,53 @@ end_position = Geometry.transform([0 0 0]);
 
 % Create path of the robot
 robot_path = robot.getPath(end_position);
-robot_path.show();
-robot_path.showTimingDiagram();
-
+% robot.show();
+% hold on;
+% robot_path.show();
+% figure;
+% robot_path.showTimingDiagram();
+% 
+% figure;
 % Create the path of the robot into a timeseries
 rate = rateControl(1/robot_path.step_size);
-for t = 0:robot_path.step_size:robot_path.duration
-    robot.stepPath(t, robot_path);
+for t = 0:robot_path.step_size:10000%robot_path.duration
     for i = 1:numel(robot.configuration)
         msg = pubs(robot.configuration(i).JointName).rosmessage;
         msg.Data = robot.configuration(i).JointPosition;
+        if contains(robot.configuration(i).JointName, "head")
+            continue
+        end
+        
         p = pubs(robot.configuration(i).JointName);
         p.send(msg);
     end
-    waitfor(rate);
+%     waitfor(rate);
+%     robot.show();
+%     view(-90,0);
+%     campos([-6,0,0]);
+    
+    robot_position = robot.pose.position;
+    robot_orientation = robot.pose.orientation;
+    msg = odom_pub.rosmessage;
+    msg.Header.Stamp = rostime('now');
+    msg.Header.FrameId = "odom";
+    msg.ChildFrameId = "base_footprint";
+    msg.Pose.Pose.Position.X = robot_position(1);
+    msg.Pose.Pose.Position.Y = robot_position(2);
+    msg.Pose.Pose.Position.Z = robot_position(3);
+    
+    msg.Pose.Pose.Orientation.W = robot_orientation(1);
+    msg.Pose.Pose.Orientation.X = robot_orientation(2);
+    msg.Pose.Pose.Orientation.Y = robot_orientation(3);
+    msg.Pose.Pose.Orientation.Z = robot_orientation(4);
+    msg.Pose.Covariance(6 * 0 + 1) = 0.01;
+    msg.Pose.Covariance(6 * 1 + 2) = 0.01;
+    msg.Pose.Covariance(6 * 2 + 3) = 0.01;
+    msg.Pose.Covariance(6 * 3 + 4) = 0.01;
+    msg.Pose.Covariance(6 * 4 + 5) = 0.01;
+    msg.Pose.Covariance(6 * 5 + 6) = 0.01;
+    
+    odom_pub.send(msg);
+    
+%    robot.stepPath(t, robot_path);
 end
