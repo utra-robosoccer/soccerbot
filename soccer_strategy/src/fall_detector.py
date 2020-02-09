5d68daad0af0081f5efc29aa4264ce1e7deb96c5
Nam <lkunelk@gmail.com>
2020-02-09 10:22:47 -0500

    create node to detect if robot falls

---
+++ soccer_strategy/src/fall_detector.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
+from sensor_msgs.msg import Imu
+from tf.transformations import euler_from_quaternion
+
+angle_threshold = 1 # in radians
+
+def imu_callback(msg, pub):
+    q = msg.orientation
+    roll , pitch, yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
+
+    if pitch > angle_threshold:
+        message = String()
+        message.data = "I_fell_front"
+        pub.publish(message)
+    if pitch < -angle_threshold:
+        message = String()
+        message.data = "I_fell_back"
+        pub.publish(message)
+
+def main():
+    rospy.init_node('soccer_strategy', anonymous=True)
+    pub = rospy.Publisher('fall_state', String, queue_size=10)
+    rospy.Subscriber('robot1/imu', Imu, imuCallback, pub)
+    rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
