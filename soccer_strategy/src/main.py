f7d8b8569192da1bcc90052454ab5f4e63b2657b
Manx52 <jonathanspraggett@gmail.com>
2020-02-14 15:00:57 -0500

    Semi-finish successor function
    Former-commit-id: 044bc229dc52cd6f88d8618bc819640c6efcfadf [formerly 174c28e5c87669828428bd03c752d1970eefc9bd]
    Former-commit-id: eca4048bceda60ed94360ce0f0fecec0fa8be839

--- soccer_strategy/src/main.py
+++ soccer_strategy/src/main.py
@@ -3,7 +3,18 @@ import rospy
 import tf2_ros
 import geometry_msgs.msg
 from geometry_msgs.msg import PoseArray
+from tf.transformations import quaternion_from_euler
 import math
+from enum import Enum
+
+
+class Status(Enum):
+    standing = 0
+    walking = 1
+    fallen_back = 2
+    fallen_forward = 3
+    getting_up = 4
+
 
 # Define the publisher and subscribers here
 
@@ -35,6 +46,8 @@ class State:
         # ball["position"] = (2,3)
 
         has_ball = False
+        ball_pose = geometry_msgs.msg.TransformStamped()
+        ball_pose.header.stamp = rospy.Time.now()
 
         try:
             ball_pose = tfBuffer.lookup_transform('ball', 'base_footprint', rospy.Time(0))
@@ -46,13 +59,19 @@ class State:
         last_pose = rospy.Time.now() - ball_pose.header.stamp;
 
         if has_ball and (last_pose < rospy.Duration(1)):
-            position = geometry_msgs.msg.Point()
+            position = geometry_msgs.msg.PoseWithCovarianceStamped()
 
-            position.x = ball_pose.transform.translation.x
-            position.y = ball_pose.transform.translation.y
-            position.z = 0
+            position.pose.pose.position.x = ball_pose.transform.translation.x
+            position.pose.pose.position.y = ball_pose.transform.translation.y
+            position.pose.pose.position.z = 0
+
+            position.pose.pose.orientation.x = 0.0
+            position.pose.pose.orientation.y = 0.0
+            position.pose.pose.orientation.z = 0.0
+            position.pose.pose.orientation.w = 1.0
 
             self.ball["position"] = position
+            self.successors()
 
         pass
 
@@ -61,35 +80,70 @@ class State:
         # Retrieve list of actions in future (robot can move in radius 1m, and front half circle (slices) 10 degrees
         # If ball is within 1m. robot can also move directly to the ball, slightly to the left and the right because of foot
         # If robot.Status = Fallen, then the only thing you can do is get back up (front and back)
+        self.pose_array.poses = []
+        self.pose_array.header.frame_id = 'world'
+        self.pose_array.header.stamp = rospy.Time.now()
 
-        pose_array = PoseArray()
-        pose_array.header.frame_id = 'world'
-        pose_array.header.stamp = rospy.Time.now()
+        self.cost = []
+        future_pose = geometry_msgs.msg.Pose()
+        q = [0, 0, 0, 1]
+        future_pose.orientation.x = q[0]  # maybe add a real orientation based off of quaternions
+        future_pose.orientation.y = q[1]
+        future_pose.orientation.z = q[2]
+        future_pose.orientation.w = q[3]
 
         dist = self.distance((self.ball["position"]))
 
-        if self.robot_pose[1]["status"] == 1:
+        if self.robots[1]["status"] == Status.fallen_back:
+            pass
+
+        elif self.robots[1]["status"] == Status.fallen_forward:
             pass
         # if ball not in 1m
-        elif dist > 1.0:
-            for i in range(0, 190, 10):
-                future_pose = geometry_msgs.msg.Pose()
-                if i <= 90:
-                    future_pose.position.y = (self.robot_pose[1]["position"].y - math.cos(math.radians(i)))
-                else:
-                    future_pose.position.y = (self.robot_pose[1]["position"].y + math.cos(math.radians(i)))
-                future_pose.position.x = (self.robot_pose[1]["position"].x - math.sin(math.radians(i)))
-                #pose_array.poses.append((future_pose, ))
-            pass
-        # if ball is in 1m but to far x and y
-        else:
-            # right in front of the ball
-            if abs((self.ball["position"].y - self.robot_pose[1]["position"].y)) <= 0.1:
-                # goes forward
-                pass
-            # to the side of the ball
+        elif self.robots[1]["status"] == Status.standing:
+            # if abs((self.ball["position"].pose.pose.position.y - self.robots[1]["position"].pose.pose.position.y)) <= 0.05:
+            # goes forward and kick
 
-            pass
+            #   pass
+            if dist > 1.0:
+                for i in range(0, 190, 10):
+
+                    if i > 90:
+                        future_pose.position.y = (
+                                self.robots[1]["position"].pose.pose.position.y - math.cos(math.radians(i - 90)))
+                        future_pose.position.x = (
+                                self.robots[1]["position"].pose.pose.position.x + math.sin(math.radians(i - 90)))
+                    else:
+                        future_pose.position.y = (
+                                self.robots[1]["position"].pose.pose.position.y + math.cos(math.radians(i)))
+                        future_pose.position.x = (
+                                self.robots[1]["position"].pose.pose.position.x + math.sin(math.radians(i)))
+                    future_pose.position.z = 0.0
+                    self.pose_array.poses.append(future_pose)
+                    self.cost.append(0)
+                self.future_pose.publish(self.pose_array)
+                self.value()
+
+            elif dist < 1.0:
+                for i in range(0, 190, 10):
+
+                    if i > 90:
+                        future_pose.position.y = (
+                                self.robots[1]["position"].pose.pose.position.y - (
+                                    math.cos(math.radians(i - 90)) / dist))
+                        future_pose.position.x = (
+                                self.robots[1]["position"].pose.pose.position.x + (
+                                    math.sin(math.radians(i - 90)) / dist))
+                    else:
+                        future_pose.position.y = (
+                                self.robots[1]["position"].pose.pose.position.y + (math.cos(math.radians(i)) / dist))
+                        future_pose.position.x = (
+                                self.robots[1]["position"].pose.pose.position.x + (math.sin(math.radians(i)) / dist))
+                    future_pose.position.z = 0.0
+                    self.pose_array.poses.append(future_pose)
+                    self.cost.append(0)
+                self.future_pose.publish(self.pose_array)
+                self.value()
 
         pass
 
@@ -102,25 +156,34 @@ class State:
         pass
 
     def robot_pose_callback(self, data):
-        self.robot_pose[1]["position"] = data.position
+        self.robots[1]["position"] = data
         pass
 
     def distance(self, position):
-        dist = (position.x ** 2 + (position.y) ** 2) ** (1 / 2)
+        dist = (position.pose.pose.position.x ** 2 + position.pose.pose.position.y ** 2) ** (1 / 2)
         return dist
         pass
 
     def __init__(self):
-        self.robot_pose = rospy.Subscriber("amcl_pose", geometry_msgs.msg.Pose, self.robot_pose_callback)
-        # self.ball_position = rospy.Publisher('ball_position', geometry_msgs.msg.Point, queue_size=1)
+        self.robot_pose = rospy.Subscriber("amcl_pose", geometry_msgs.msg.PoseWithCovarianceStamped,
+                                           self.robot_pose_callback)
+        self.future_pose = rospy.Publisher('successor_states', geometry_msgs.msg.PoseArray, queue_size=1)
 
-        position = geometry_msgs.msg.Point()
-        position.x = 0.0
-        position.y = 0.0
-        position.z = 0.0
+        position = geometry_msgs.msg.PoseWithCovarianceStamped()
 
-        self.robots = {1: {'position': position, 'status': 0}}
+        position.pose.pose.position.x = 0.0
+        position.pose.pose.position.y = 0.0
+        position.pose.pose.position.z = 0.0
+
+        position.pose.pose.orientation.x = 0.0
+        position.pose.pose.orientation.y = 0.0
+        position.pose.pose.orientation.z = 0.0
+        position.pose.pose.orientation.w = 1.0
+
+        self.robots = {1: {'position': position, 'status': Status.standing}}
         self.ball = {'position': position}
+        self.pose_array = PoseArray()
+        self.cost = []
         pass
 
 
