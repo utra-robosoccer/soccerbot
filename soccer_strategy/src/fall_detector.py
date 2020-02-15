f0a95d6744a30d5640731bd6a03d651d7daf4285
Nam <lkunelk@gmail.com>
2020-02-15 18:41:33 -0500

    Modify fall detection to wait before publishing standing state, when we are sure the robot is standing
    Former-commit-id: d0cb5dc26fb4f01c4f1a447f57f1d25f238da0a1 [formerly 1d6b666f2701a9a43782837946150980232efd37]
    Former-commit-id: fe3a584f07623cdbcf4ea35935618b8b97f9317c

--- soccer_strategy/src/fall_detector.py
+++ soccer_strategy/src/fall_detector.py
@@ -12,30 +12,42 @@ class State(Enum):
 
 ANGLE_THRESHOLD = 1 # in radians
 ROBOT_STATE = State.STANDING
-
+FIRST_STANDING = None
+STANDING_THRESHOLD = 10
+PUBLISHED_STANDING = False
 
 def imu_callback(msg, pub):
-    global ROBOT_STATE, ANGLE_THRESHOLD
+    global ROBOT_STATE, ANGLE_THRESHOLD, FIRST_STANDING, STANDING_THRESHOLD, PUBLISHED_STANDING
     q = msg.orientation
     roll , pitch, yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
 
     # We want to publish once on state transition
     if pitch > ANGLE_THRESHOLD and ROBOT_STATE == State.STANDING:
         ROBOT_STATE = State.FALL_FRONT
+        PUBLISHED_STANDING = False
+        FIRST_STANDING = None
         pub.publish(ROBOT_STATE.value)
 
     if pitch < -ANGLE_THRESHOLD and ROBOT_STATE == State.STANDING:
         ROBOT_STATE = State.FALL_BACK
+        PUBLISHED_STANDING = False
+        FIRST_STANDING = None
         pub.publish(ROBOT_STATE.value)
 
-    if abs(pitch) < ANGLE_THRESHOLD and ROBOT_STATE in [State.FALL_BACK, State.FALL_FRONT]:
+    if abs(pitch) < ANGLE_THRESHOLD and not PUBLISHED_STANDING:
         ROBOT_STATE = State.STANDING
-        pub.publish(ROBOT_STATE.value)
+        if FIRST_STANDING == None:
+            print('Pose is vertical')
+            FIRST_STANDING = rospy.Time.now()
+        if rospy.Time.now() - FIRST_STANDING > rospy.Duration(STANDING_THRESHOLD):
+            pub.publish(ROBOT_STATE.value)
+            FIRST_STANDING = None
+            PUBLISHED_STANDING = True
 
 def main():
     rospy.init_node('fall_detector')
     pub = rospy.Publisher('fall_state', String, queue_size=10, latch=True)
-    rospy.Subscriber('imu', Imu, imu_callback, pub)
+    rospy.Subscriber('/robot1/imu', Imu, imu_callback, pub)
     rospy.spin()
 
 if __name__ == '__main__':
