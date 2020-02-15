0b4b35c51abc74f03d8ea7b515b51599bbb7e8ab
Nam <lkunelk@gmail.com>
2020-02-15 15:53:55 -0500

    make fall_detector publish once on state transition, add node to localization launch
    Former-commit-id: 52f1cc36804aa301e56256eb907ac078aae55599 [formerly 001e9f09f78566fff221a799e41ed22b663503d9]
    Former-commit-id: e66ddbb39b5af297ce8928b184c4417bab9429f5

--- soccer_strategy/src/fall_detector.py
+++ soccer_strategy/src/fall_detector.py
@@ -3,25 +3,38 @@ import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Imu
 from tf.transformations import euler_from_quaternion
+from enum import Enum
 
-angle_threshold = 1 # in radians
+class State(Enum):
+    FALL_FRONT = 'fell_front'
+    FALL_BACK = 'fell_back'
+    STANDING = 'standing'
+
+ANGLE_THRESHOLD = 1 # in radians
+ROBOT_STATE = State.STANDING
+
 
 def imu_callback(msg, pub):
+    global ROBOT_STATE, ANGLE_THRESHOLD
     q = msg.orientation
     roll , pitch, yaw = euler_from_quaternion([q.x, q.y, q.z, q.w])
 
-    if pitch > angle_threshold:
-        message = String()
-        message.data = "I_fell_front"
-        pub.publish(message)
-    if pitch < -angle_threshold:
-        message = String()
-        message.data = "I_fell_back"
-        pub.publish(message)
+    # We want to publish once on state transition
+    if pitch > ANGLE_THRESHOLD and ROBOT_STATE == State.STANDING:
+        ROBOT_STATE = State.FALL_FRONT
+        pub.publish(ROBOT_STATE.value)
+
+    if pitch < -ANGLE_THRESHOLD and ROBOT_STATE == State.STANDING:
+        ROBOT_STATE = State.FALL_BACK
+        pub.publish(ROBOT_STATE.value)
+
+    if abs(pitch) < ANGLE_THRESHOLD and ROBOT_STATE in [State.FALL_BACK, State.FALL_FRONT]:
+        ROBOT_STATE = State.STANDING
+        pub.publish(ROBOT_STATE.value)
 
 def main():
-    rospy.init_node('soccer_strategy', anonymous=True)
-    pub = rospy.Publisher('fall_state', String, queue_size=10)
+    rospy.init_node('fall_detector')
+    pub = rospy.Publisher('fall_state', String, queue_size=10, latch=True)
     rospy.Subscriber('imu', Imu, imu_callback, pub)
     rospy.spin()
 
