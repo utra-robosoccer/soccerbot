60600fb826c06362182ebff00f3031e87ac45f7c
Jason <vuwij@me.com>
2020-03-07 15:43:35 -0500

    Fix

--- soccer_control/scripts/run.sh
+++ soccer_control/scripts/run.sh
@@ -9,4 +9,6 @@ fi
 source ~/catkin_ws/devel/setup.bash
 roscd soccer_control
 
-matlab -nodesktop -r 'try; run_from_bash=1; main; catch; end; quit' || exit
+matlab -nodesktop -r 'try; run_from_bash=1; main; catch; end; quit'
+
+read -n 1 k <&1
