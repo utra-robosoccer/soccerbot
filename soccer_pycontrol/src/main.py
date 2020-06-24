4814bc2df7a43e42c32555b261af8786c65b9aa2
Shahryar Rajabzadeh <shahryar_rajabzadeh@yahoo.com>
2020-06-23 21:07:46 -0400

    Latest June 23
    Former-commit-id: a690606071c4721cf63feb03a7fc4b897a7499f7 [formerly 2e3a270ca6aa68737a522a09a19fc4aebd75043e]
    Former-commit-id: 7c553d153508160c6e1616521249fb01ac25eea9

--- soccer_pycontrol/src/main.py
+++ soccer_pycontrol/src/main.py
@@ -1,22 +1,21 @@
-import pybullet as p
+import pybullet as pb
 import pybullet_data
 from time import sleep
 
 from robot.soccerbot import Robot
-from robot.environment import Ramp
+from robot.ramp import Ramp
 
 if __name__ == '__main__':
 
     # Pybullet Setup
-    p.connect(p.GUI)
-    p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
+    pb.connect(pb.GUI)
+    pb.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
     ramp = Ramp("plane.urdf", (0, 0, 0), (0, 0, 0))
     myrobot = Robot()
-    p.setGravity(0, 0, -10)
+    pb.setGravity(0, 0, -10)
 
     # Step through simulation
     while (1):
         sleep(0.01)
-        pos, orn = p.getBasePositionAndOrientation(myrobot.body)
-        myrobot.stabilize()
-        p.stepSimulation()
+        pos, orn = pb.getBasePositionAndOrientation(myrobot.body)
+        pb.stepSimulation()
--- soccer_pycontrol/src/robot/ramp.py
+++ soccer_pycontrol/src/robot/ramp.py
@@ -1,18 +1,6 @@
-"""
-Environment
-"""
-
-import numpy as np
 import pybullet as p
-import matplotlib as plt
-from time import sleep
-
 
 class Ramp:
-    """
-    creates a ramp using a plane at location path with basePosition = position and baseOrientation = orientation
-    where orientation is in yaw, pitch, roll
-    """
 
     def __init__(self, path, position, orientation):
         self.orientation = orientation
