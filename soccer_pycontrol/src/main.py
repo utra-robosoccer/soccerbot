2bc1da4cb433ef06a625b259b841f8f63bea8130
Shah-Rajab <shahryar_rajabzadeh@yahoo.com>
2021-03-25 05:24:05 -0400

    Actually Walking! + Basic IMU function added + Basic foot pressure sensor added
    Former-commit-id: 9eba0d8960dc4a5e635f59e91e2ef5cac1dfabc0 [formerly 97bc45e97611a77bf46923b7669cd31661dcf896]
    Former-commit-id: 4cc1b80d75dcc39f040e0855362d382047d3b583

--- soccer_pycontrol/src/main.py
+++ soccer_pycontrol/src/main.py
@@ -11,6 +11,11 @@ import matplotlib as plt
 PYBULLET_STEP = 0.004
 
 
+# Set the destination!
+GOAL_X = 0.
+GOAL_Y = 1.
+
+
 def wait(steps):
     for i in range(steps):
         sleep(PYBULLET_STEP)
@@ -25,8 +30,8 @@ if __name__ == '__main__':
     pb.resetDebugVisualizerCamera(cameraDistance=0.5, cameraYaw=0, cameraPitch=0, cameraTargetPosition=[0, 0, 0.25])
     pb.setGravity(0, 0, -9.81)
 
-    soccerbot = Soccerbot([0, 0, 0], useFixedBase=True)
-    ramp = Ramp("plane.urdf", (0, 0, 0), (0, 0, 0))
+    soccerbot = Soccerbot([0, 0, 0], useFixedBase=False)
+    ramp = Ramp("plane.urdf", (0, 0, 0), (0, 0, 0), lateralFriction=0.9, spinningFriction=0.9, rollingFriction=0.0)
 
     # Move to the standing position
     wait(100)
@@ -34,15 +39,17 @@ if __name__ == '__main__':
 
     # Start walking
     wait(1000)
-    soccerbot.getPath(Transformation([0.3, 0, 0]), show=True)
+    soccerbot.getPath(Transformation([GOAL_X, GOAL_Y, 0]), show=False)
     # soccerbot.calculate_angles(show=True)
     t = 0
     while True:
         if soccerbot.current_step_time <= t <= soccerbot.robot_path.duration():
-            soccerbot.stepPath(t)
+            soccerbot.stepPath(t, verbose=True)
             pb.setJointMotorControlArray(bodyIndex=soccerbot.body, controlMode=pb.POSITION_CONTROL, jointIndices=list(range(0, 18, 1)), targetPositions=soccerbot.configuration)
             soccerbot.current_step_time = soccerbot.current_step_time + soccerbot.robot_path.step_size
 
         pb.stepSimulation()
+        soccerbot.get_imu(verbose=True)
+        print(f'right foot: {soccerbot.get_feet(ramp.plane)[:4]} \t left foot: {soccerbot.get_feet(ramp.plane)[4:]}')
         t = t + PYBULLET_STEP
         sleep(PYBULLET_STEP)
