ed81078dc48465d6835d4eb4330de898bdb530fe
Shah-Rajab <shahryar_rajabzadeh@yahoo.com>
2021-04-28 08:03:24 -0400

    new env with orientation & height & velocity feedback

--- soccer_rlcontrol/gym-soccerbot/gym_soccerbot/__init__.py
+++ soccer_rlcontrol/gym-soccerbot/gym_soccerbot/__init__.py
@@ -36,6 +36,11 @@ register(
 )
 
 register(
+    id='walk-forward-velocity-v2',
+    entry_point='gym_soccerbot.envs:WalkingForwardV6',
+)
+
+register(
     id='walk-forward-norm-v1',
     entry_point='gym_soccerbot.envs:WalkingForwardNormAgn',
 )
--- soccer_rlcontrol/gym-soccerbot/gym_soccerbot/envs/__init__.py
+++ soccer_rlcontrol/gym-soccerbot/gym_soccerbot/envs/__init__.py
@@ -4,5 +4,6 @@ from gym_soccerbot.envs.walking_forward_env_2 import WalkingForwardV2
 from gym_soccerbot.envs.walking_forward_env_3 import WalkingForwardV3
 from gym_soccerbot.envs.walking_forward_env_4 import WalkingForwardV4
 from gym_soccerbot.envs.walking_forward_env_5 import WalkingForwardV5
+from gym_soccerbot.envs.walking_forward_env_6 import WalkingForwardV6
 from gym_soccerbot.envs.walking_forward_norm_env import WalkingForwardNorm
 from gym_soccerbot.envs.walking_forward_norm_agn_env import WalkingForwardNormAgn
