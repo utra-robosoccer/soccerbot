e3509661a4ba03e596aa208fb16f6d767f7ee4be
Nam <lkunelk@gmail.com>
2021-05-08 21:44:51 -0700

    add namespace to node
    Former-commit-id: b670aae72ed19d6a1ddc792ea000cb633234110f [formerly de0256b16cd0e353f2d8fa0951fe46d31ace3425]
    Former-commit-id: 3848026124b82eb0e64eb412912f175be7f0f2bf

--- soccer_object_detection/src/object_detect_node.py
+++ soccer_object_detection/src/object_detect_node.py
@@ -1,9 +1,18 @@
+import os
+if "ROS_NAMESPACE" not in os.environ:
+    os.environ["ROS_NAMESPACE"] = "/robot1"
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import cv2
-import os
+import torch
 import numpy as np
+from model import CNN, init_weights
+from my_dataset import initialize_loader
+from train import Trainer
+import util
+import torchvision
+from model import find_batch_bounding_boxes, Label
 
 class ObjectDetectionNode(object):
     '''
@@ -17,15 +26,18 @@ class ObjectDetectionNode(object):
         self.image = None
         self.br = CvBridge()
         self.loop_rate = rospy.Rate(10) # Hz
-        self.pub = rospy.Publisher('imagetimer', Image, queue_size=10)
-        rospy.Subscriber("robot1/camera/image_raw",Image, self.callback)
+        self.pub = rospy.Publisher('detection_image', Image, queue_size=10)
+        rospy.Subscriber("camera/image_raw",Image, self.callback)
+
+        self.model = CNN(kernel=3, num_features=8)
+        self.model.load_state_dict(torch.load('outputs/model_ker3_feat8'))
+        self.model.eval()
 
     def callback(self, msg):
         # width x height x channels (bgra8)
         self.image = self.br.imgmsg_to_cv2(msg)
 
     def start(self):
-        rospy.loginfo("Timing images")
         while not rospy.is_shutdown():
             rospy.loginfo('publishing image')
             br = CvBridge()
@@ -45,11 +57,20 @@ class ObjectDetectionNode(object):
                 crop_img = img[int(y):int(y+h), int(x):int(x+w)]
                 rospy.loginfo(crop_img.shape)
 
-                self.pub.publish(br.cv2_to_imgmsg(crop_img))
+                img_torch = util.cv_to_torch(crop_img)
+
+                outputs, _ = self.model(torch.tensor(np.expand_dims(img_torch, axis=0)).float())
+                bbxs = find_batch_bounding_boxes(outputs)[0]
+                # img = util.draw_bounding_boxes(img, bbxs[Label.ROBOT.value], (0, 0, 255))
+                img_torch = util.draw_bounding_boxes(img_torch, bbxs[Label.BALL.value], (255, 0, 0))
+
+                img = util.torch_to_cv(img_torch)
+
+                self.pub.publish(br.cv2_to_imgmsg(img))
             self.loop_rate.sleep()
 
 
 if __name__ == '__main__':
-    rospy.init_node("imagetimer111", anonymous=True)
+    rospy.init_node("object_detector")
     my_node = ObjectDetectionNode()
     my_node.start()
