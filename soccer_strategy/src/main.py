06901ed95af27b5de36e67e07828ff881f3568ef
ericdywang <ericdywang@gmail.com>
2021-05-29 23:59:53 -0400

    fix soccer strategy

--- soccer_strategy/src/main.py
+++ soccer_strategy/src/main.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python3
-import rospy
-from game_engine_ros import GameEngineRos
+import sys
+from game_engine import GameEngine
 
-RUN_IN_ROS = False
+RUN_IN_ROS = True
 
 if __name__ == '__main__':
-    rospy.init_node("soccer_strategy")
-    g = GameEngineRos()
-    g.run()
+    if (len(sys.argv) > 2 and sys.argv[1] == '__name:=soccer_strategy') or RUN_IN_ROS:
+        import rospy
+        from game_engine_ros import GameEngineRos
+
+        rospy.init_node("soccer_strategy")
+        #r = rospy.Rate(10)
+        #while not rospy.has_param("walking_engine_ready"):
+        #    r.sleep()
+
+        g = GameEngineRos()
+        g.run()
+    else:
+        g = GameEngine()
+        g.run()
