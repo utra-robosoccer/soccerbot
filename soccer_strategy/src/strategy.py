049e2c2dd947470fb668d0b470024f4ad8901c1a
ericdywang <ericdywang@gmail.com>
2021-05-29 23:59:53 -0400

    fix soccer strategy
    Former-commit-id: bd11872eafba0d0eb306456a2ac91ee3a560412b [formerly 06901ed95af27b5de36e67e07828ff881f3568ef]
    Former-commit-id: af82815ff8be95c67029c7bbbd607605b6920917

--- soccer_strategy/src/strategy.py
+++ soccer_strategy/src/strategy.py
@@ -6,6 +6,17 @@ class Strategy:
     def __init__(self):
         pass
 
+    def update_both_team_strategy(self, robots, ball):
+        friendly = []
+        opponent = []
+        for robot in robots:
+            if robot.team == Robot.Team.FRIENDLY or robot.Team == Robot.Team.FRIENDLY:
+                friendly.append(robot)
+            else:
+                opponent.append(robot)
+
+        self.update_next_strategy(friendly, opponent, ball)
+
     def update_next_strategy(self, friendly, opponent, ball):
         raise NotImplementedError
 
@@ -20,6 +31,9 @@ class DummyStrategy(Strategy):
         closest_dist = math.inf
         current_closest = None
         for robot in robots:
+            if robot.status != Robot.Status.READY:
+                continue
+
             dist = np.linalg.norm(ball.get_position()[0:2] - robot.get_position()[0:2])
             if dist < closest_dist:
                 closest_dist = dist
@@ -30,22 +44,27 @@ class DummyStrategy(Strategy):
         # Guess who has the ball
         current_closest = self.who_has_the_ball(friendly, ball)
 
+        if current_closest == None:
+            return
+
         a = current_closest.get_position()
         b = ball.get_position()
         if np.linalg.norm(current_closest.get_position()[0:2] - ball.get_position()) < 0.2:
             # Stop moving
             current_closest.set_navigation_position(current_closest.get_position())
 
+            if current_closest.team == Robot.Team.FRIENDLY:
+                opponent_goal = np.array([0, 4.5])
+            else:
+                opponent_goal = np.array([0, -4.5])
+
             # Kick the ball towards the goal
-            opponent_goal = current_closest.get_opponent_net_position()
             delta = opponent_goal - ball.get_position()
             unit = delta / np.linalg.norm(delta)
 
             current_closest.set_kick_velocity(unit * current_closest.max_kick_speed)
             current_closest.status = Robot.Status.KICKING
         else:
-            # if current_closest.status != Robot.Status.READY:
-            #     return
             current_closest.set_navigation_position(np.append(ball.get_position(), 0))
             current_closest.status = Robot.Status.WALKING
 
