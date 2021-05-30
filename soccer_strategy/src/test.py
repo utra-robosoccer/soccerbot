e3d71e46b0fe24b1d608c83dee51ae4d28b7c566
vuwij <vuwij@system76-pc.localdomain>
2021-05-30 10:53:57 +0900

    Change test to a different file
    Former-commit-id: deb6bc2c72fba0b388d5acebd23289cbd7f58630 [formerly 02d77591efdeecbd32779a0a9a6f4cb1ec8f70b5]
    Former-commit-id: 952828a4a0b3dfa72a655bc4db52c468972d7765

---
+++ soccer_strategy/src/test.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from soccer_strategy.src.game_engine import GameEngine
+
+
+class Test(TestCase):
+    def test_game(self):
+        friendly_wins = 0
+        opponent_wins = 0
+        for i in range(10):
+            g = GameEngine(display=False)
+            friendly_points, opponent_points = g.run()
+            if friendly_points > opponent_points:
+                friendly_wins += 1
+            elif friendly_points < opponent_points:
+                opponent_wins += 1
+        print(f'Friendly: {friendly_wins}, opponent: {opponent_wins}')
+
