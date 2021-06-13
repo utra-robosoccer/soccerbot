f7ebce3a346d2ab57162ff7333854feb3e17d92a
ericdywang <ericdywang@gmail.com>
2021-06-13 00:27:37 -0400

    update game engine with gamestate update
    Former-commit-id: 66cdf2f348d4df1ed45d35080cc3a45ffce9fe66 [formerly cf050f442581453e83abee72e448a0c51e184c11]
    Former-commit-id: 453d015ffc114f9c24b441de373d4cd73bfb3edc

--- soccer_strategy/src/main.py
+++ soccer_strategy/src/main.py
@@ -1,23 +1,12 @@
 #!/usr/bin/env python3
 import sys
 from game_engine import GameEngine
-from game_engine_comp import GameEngineComp
-
-RUN_IN_ROS = True
+from game_engine_competition import GameEngineCompetition
+import rospy
+_IN_ROS = True
 
 if __name__ == '__main__':
-    if (len(sys.argv) > 2 and sys.argv[1] == '__name:=soccer_strategy') or RUN_IN_ROS:
-        import rospy
-        from game_engine_ros import GameEngineRos
+    rospy.init_node("soccer_strategy")
 
-        rospy.init_node("soccer_strategy")
-        rospy.sleep(1)
-
-        g = GameEngineComp()
-        g.run()
-
-        #g = GameEngineRos()
-        #g.run()
-    else:
-        g = GameEngine()
-        g.run()
+    g = GameEngineCompetition()
+    g.run()
