7ea501d1e837535170b0d3a74b1fba5d3d9aae8a
manx52 <jonathanspraggett@gmail.com>
2022-01-02 19:55:02 -0500

    Added goal post detection, bounding boxes and projection into detector_fieldline.py Fixed Tranformation error in detector_objects.py

--- soccer_object_localization/src/detector_fieldline.py
+++ soccer_object_localization/src/detector_fieldline.py
@@ -6,7 +6,8 @@ import time
 if "ROS_NAMESPACE" not in os.environ:
     os.environ["ROS_NAMESPACE"] = "/robot1"
 import rospy
-
+import tf2_ros
+from geometry_msgs.msg import PointStamped, TransformStamped, PoseStamped
 import numpy as np
 import cv2
 from sensor_msgs.msg import Image, PointCloud2
@@ -14,6 +15,8 @@ from std_msgs.msg import Bool, Header
 from detector import Detector
 import sensor_msgs.point_cloud2 as pcl2
 from cv_bridge import CvBridge
+import math
+
 
 class DetectorFieldline(Detector):
     CANNY_THRESHOLD_1 = 400
@@ -30,7 +33,9 @@ class DetectorFieldline(Detector):
         self.image_subscriber = rospy.Subscriber("camera/image_raw", Image, self.image_callback, queue_size=1)
         self.image_publisher = rospy.Publisher("camera/line_image", Image, queue_size=1)
         self.point_cloud_publisher = rospy.Publisher("field_point_cloud", PointCloud2, queue_size=1)
-        self.trajectory_complete_subscriber = rospy.Subscriber("trajectory_complete", Bool, self.trajectory_complete_callback)
+        self.goal_post_publisher = rospy.Publisher("goal_post", Bool, queue_size=1)
+        self.trajectory_complete_subscriber = rospy.Subscriber("trajectory_complete", Bool,
+                                                               self.trajectory_complete_callback)
         self.trajectory_complete = True
 
         cv2.setRNGSeed(12345)
@@ -56,7 +61,7 @@ class DetectorFieldline(Detector):
         hsv = cv2.cvtColor(src=image, code=cv2.COLOR_BGR2HSV)
 
         h = self.camera.calculateHorizonCoverArea()
-        cv2.rectangle(image, [0, 0], [640, h], [0, 0, 0], cv2.FILLED)
+        cv2.rectangle(image, [0, 0], [640, int(h )], [0, 0, 0], cv2.FILLED)
 
         # Original Method using contours
 
@@ -103,21 +108,43 @@ class DetectorFieldline(Detector):
         retval, dst = cv2.threshold(cdst, 127, 255, cv2.THRESH_BINARY)
         edges = cv2.Canny(dst, 50, 150)
 
-        lines = cv2.HoughLinesP(edges,rho=DetectorFieldline.HOUGH_RHO,
+        lines = cv2.HoughLinesP(edges, rho=DetectorFieldline.HOUGH_RHO,
                                 theta=DetectorFieldline.HOUGH_THETA,
                                 threshold=DetectorFieldline.HOUGH_THRESHOLD,
                                 minLineLength=DetectorFieldline.HOUGH_MIN_LINE_LENGTH,
                                 maxLineGap=DetectorFieldline.HOUGH_MAX_LINE_GAP)
+
         ccdst = cv2.cvtColor(cdst, cv2.COLOR_GRAY2RGB)
 
         if lines is None:
             return
 
+        computed_lines = []
+        vert_x_max, vert_x_min, vert_y_max, vert_y_min = 0, 1000, 0, 1000
         for l in lines:
             x1, y1, x2, y2 = l[0]
+            # computing magnitude and angle of the line
+            mag = np.sqrt((x2 - x1) ** 2. + (y2 - y1) ** 2.)
+            if x2 == x1:
+                angle = 0
+            else:
+                angle = np.rad2deg(np.arctan((y2 - y1) / (x2 - x1)))
+
+            computed_lines += [(mag, angle)]
+
             pt1 = (x1, y1)
             pt2 = (x2, y2)
-            cv2.line(ccdst, pt1, pt2, (0, 0, 255), thickness=3, lineType=cv2.LINE_AA)
+            if abs(angle) < 10:  # Horizontal
+                cv2.line(ccdst, pt1, pt2, (255, 0, 0), thickness=3, lineType=cv2.LINE_AA)
+            elif abs(abs(angle) - 90) < 10:  # Vertical
+                cv2.line(ccdst, pt1, pt2, (0, 255, 0), thickness=3, lineType=cv2.LINE_AA)
+                vert_x_max = max(vert_x_max, x1, x2)
+                vert_x_min = min(vert_x_min, x1, x2)
+                vert_y_max = max(vert_y_max, y1, y2)
+                vert_y_min = min(vert_y_min, y1, y2)
+                print('Vertical line: ', pt1, pt2)
+            else:
+                cv2.line(ccdst, pt1, pt2, (0, 0, 255), thickness=3, lineType=cv2.LINE_AA)
 
             if (pt2[0] - pt1[0]) == 0:
                 continue
@@ -128,6 +155,74 @@ class DetectorFieldline(Detector):
                 y = slope * i + b
                 pt = [i, y]
                 pts.append(pt)
+
+        computed_lines = np.array(computed_lines)
+
+        # an image has a goalpost if two perpendicular lines with 0 degrees and 90 degrees intersect
+        horizontal_line = len(computed_lines[(abs(computed_lines[:, 1]) < 10)]) > 0
+        vertical_line = len(computed_lines[(abs(abs(computed_lines[:, 1]) - 90) < 10)]) > 0
+        if vertical_line:
+            w = vert_y_max - vert_y_min
+            l = vert_x_max - vert_x_min
+            area = w*l
+            if area < 50000:
+                cv2.rectangle(ccdst, [vert_x_min, vert_y_min], [vert_x_max, vert_y_max], [0, 255, 255], thickness=2)
+                # boundingBoxes = [[vert_x_min, vert_y_min], [vert_x_max, vert_y_max]]
+                # position = self.camera.calculateBallFromBoundingBoxes(0.5, boundingBoxes)
+                #
+                # br = tf2_ros.TransformBroadcaster()
+                # ball_pose = TransformStamped()
+                # ball_pose.header.frame_id = self.robot_name + "/base_camera"
+                # ball_pose.child_frame_id = self.robot_name + "/goal_post"
+                # ball_pose.header.stamp = img.header.stamp
+                # ball_pose.header.seq = img.header.seq
+                # ball_pose.transform.translation.x = position.get_position()[0]
+                # ball_pose.transform.translation.y = position.get_position()[1]
+                # ball_pose.transform.translation.z = 0
+                # ball_pose.transform.rotation.x = 0
+                # ball_pose.transform.rotation.y = 0
+                # ball_pose.transform.rotation.z = 0
+                # ball_pose.transform.rotation.w = 1
+                # br.sendTransform(ball_pose)
+                x_avg = (vert_x_max + vert_x_min)/2
+                [floor_center_x, floor_center_y, _] = self.camera.findFloorCoordinate([x_avg, vert_y_max])
+                [floor_close_x, floor_close_y, _] = self.camera.findFloorCoordinate([x_avg, vert_y_max])
+
+                camera_pose = self.camera.pose
+
+                distance = ((floor_center_x - camera_pose.get_position()[0]) ** 2 + (
+                        floor_center_y - camera_pose.get_position()[1]) ** 2) ** 0.5
+                theta = math.atan2(distance, camera_pose.get_position()[2])
+                ratio = math.tan(theta) ** 2
+                ratio2 = 1 / (1 + ratio)
+                if 1 < ratio2 < 0:
+                    print('here')
+
+                floor_x = floor_close_x * (1 - ratio2) + floor_center_x * ratio2
+                floor_y = floor_close_y * (1 - ratio2) + floor_center_y * ratio2
+                br = tf2_ros.TransformBroadcaster()
+                robot_pose = TransformStamped()
+                robot_pose.header.frame_id = self.robot_name + "/base_camera"
+                robot_pose.child_frame_id = self.robot_name + "/goal_post"
+                robot_pose.header.stamp = img.header.stamp
+                robot_pose.header.seq = img.header.seq
+                robot_pose.transform.translation.x = floor_x
+                robot_pose.transform.translation.y = floor_y
+                robot_pose.transform.translation.z = 0
+                robot_pose.transform.rotation.x = 0
+                robot_pose.transform.rotation.y = 0
+                robot_pose.transform.rotation.z = 0
+                robot_pose.transform.rotation.w = 1
+                br.sendTransform(robot_pose)
+            print('Area: ', area)
+        print('Horiz: ', len(computed_lines[(abs(computed_lines[:, 1]) < 10)]))
+        print('Vert: ', len(computed_lines[(abs(abs(computed_lines[:, 1]) - 90) < 10)]))
+
+        has_goalpost = Bool()
+        has_goalpost.data = horizontal_line and vertical_line
+
+        # Publish Goal post in image
+        self.goal_post_publisher.publish(has_goalpost)
 
         if self.image_publisher.get_num_connections() > 0:
             img_out = CvBridge().cv2_to_imgmsg(ccdst)
@@ -148,6 +243,7 @@ class DetectorFieldline(Detector):
 
         t_end = time.time()
         rospy.loginfo_throttle(20, "Fieldline detection rate: " + str(t_end - t_start))
+
 
 if __name__ == '__main__':
     rospy.init_node("detector_fieldline")
