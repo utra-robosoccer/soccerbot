63b8307cf7a998610ced5de1942ce23748793cb7
vuwij <vuwij@me.com>
2022-03-04 10:09:36 +0900

    Now you can docker inside
    Former-commit-id: 6a0708c1d394fc4adebe1d5f1bbcae175518d96b [formerly f839eea706193067ab91bb0843937702dfadf9b9]
    Former-commit-id: 841ba1460bee975003e13905ba93d1b099f6df74

--- soccer_strategy/src/team.py
+++ soccer_strategy/src/team.py
@@ -20,8 +20,8 @@ class Team():
             "initial": {
                 Robot.Role.GOALIE: [4.5, 0, 0],
                 Robot.Role.STRIKER: [0.5, 0, 0],
-                Robot.Role.RIGHT_WING: [1, 1.5, 0],
-                Robot.Role.LEFT_WING: [1, -1.5, 0]
+                Robot.Role.RIGHT_WING: [1, 1, 0],
+                Robot.Role.LEFT_WING: [1, -1, 0]
             },
             "attack": {
                 Robot.Role.GOALIE: [4.5, 0, 0],
