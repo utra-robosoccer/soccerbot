1db1728a71fbed2bc60ad2cc72e94bb0fec974e9
Nam Nguyen <lkunelk@gmail.com>
2022-03-25 19:44:12 -0700

    rosbag logging to separate files
    Former-commit-id: b7d75a9764cec890c59756ddbbac7c1a66a9f37d [formerly 11204f29f1d8eba7aadf85d2202f16cc26cb03bf]
    Former-commit-id: 6c90d65c66cd260f065bf97973ed90bc909ea384

--- soccerbot/bags/view_rosbag.sh
+++ soccerbot/bags/view_rosbag.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# example: ./view_rosbag.sh red_robot2
 
-sudo chown -R $USER robot1.bag.active
-sudo chgrp -R $(id -g $USER) robot1.bag.active
+sudo chown -R $USER $1.bag.active
+sudo chgrp -R $(id -g $USER) $1.bag.active
 
-rosbag reindex -f robot1.bag.active
+rosbag reindex -f $1.bag.active
 
-roscore | ( sleep 1 && rqt_bag robot1.bag.active )
+roscore | ( sleep 1 && rqt_bag $1.bag.active )
