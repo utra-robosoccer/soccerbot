9877b3fbe2ea98b5001b7950312d47fa4db56dd1
vuwij <vuwij@me.com>
2022-04-07 23:45:44 +0900

    Test working

--- soccer_strategy/src/test_game_engine_2d.py
+++ soccer_strategy/src/test_game_engine_2d.py
@@ -20,11 +20,17 @@ from strategy.strategy_stationary import StrategyStationary
 
 
 class Test(TestCase):
+    def setUp(self) -> None:
+        super().setUpClass()
+        self.display = True
+        if "pytest" in sys.argv[0]:
+            self.display = False
+
     def test_dummy_strategy(self):
         friendly_wins = 0
         opponent_wins = 0
         for i in range(1):
-            g = GameEngine2D(team_1_strategy=StrategyDummy, team_2_strategy=StrategyDummy)
+            g = GameEngine2D(display=self.display, team_1_strategy=StrategyDummy, team_2_strategy=StrategyDummy)
             friendly_points, opponent_points = g.run()
             if friendly_points > opponent_points:
                 friendly_wins += 1
@@ -38,7 +44,7 @@ class Test(TestCase):
         friendly_wins = 0
         opponent_wins = 0
         for i in range(10):
-            g = GameEngine2D(team_1_strategy=StrategyDecisionTree, team_2_strategy=StrategyStationary)
+            g = GameEngine2D(display=self.display, team_1_strategy=StrategyDecisionTree, team_2_strategy=StrategyStationary)
 
             friendly_points, opponent_points = g.run()
             if friendly_points > opponent_points:
