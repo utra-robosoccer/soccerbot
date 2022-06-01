a6a88429fa006218189812ff88500a8b49c2e762
Jason Wang <vuwij1@gmail.com>
2022-06-02 05:32:28 +0900

    Missing dependency

--- soccerbot/scripts/install_mxnet.sh
+++ soccerbot/scripts/install_mxnet.sh
@@ -6,5 +6,6 @@ if [ "$(uname -m)" = "x86_64" ]; then
     exit 0
 fi
 
+apt update && apt-fast install -y libopenblas-dev
 pip install gdown && gdown 1BoUq15BZC5tKuaBKg5wNg6NowwP6IsHz && pip install mxnet_jetson-1.9.1-py3-none-any.whl && rm -rf mxnet_jetson-1.9.1-py3-none-any.whl
 exit 0
