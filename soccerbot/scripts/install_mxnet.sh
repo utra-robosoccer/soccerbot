e23a1732f2b73d1334fecb885eb3ddd943d86b11
manx52 <jonathanspraggett@gmail.com>
2022-07-03 07:35:18 -0400

    Fix graphviz

--- soccerbot/scripts/install_mxnet.sh
+++ soccerbot/scripts/install_mxnet.sh
@@ -6,6 +6,6 @@ if [ "$(uname -m)" = "x86_64" ]; then
     exit 0
 fi
 
-apt update && apt-fast install -y libopenblas-dev
+apt update && apt-fast install -y graphviz==0.8.1 libopenblas-dev
 pip install -i https://test.pypi.org/simple/ mxnet-jetson==1.9.1
 exit 0
