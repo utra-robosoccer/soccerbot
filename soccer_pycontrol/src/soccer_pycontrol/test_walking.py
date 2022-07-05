8cd412be36f46e261b7c56b11d9d3f73e8195450
manx52 <jonathanspraggett@gmail.com>
2022-07-05 17:06:47 -0400

    i dont remember# Please enter the commit message for your changes. Lines starting

--- soccer_pycontrol/src/soccer_pycontrol/test_walking.py
+++ soccer_pycontrol/src/soccer_pycontrol/test_walking.py
@@ -11,8 +11,8 @@ if "ROS_NAMESPACE" not in os.environ:
 
 from soccer_common.transformation import Transformation
 
-real_robot = False
-run_in_ros = False
+real_robot = True
+run_in_ros = True
 display = False
 robot_model = "bez1"
 TEST_TIMEOUT = 60
