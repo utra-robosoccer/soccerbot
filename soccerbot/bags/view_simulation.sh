c80ba379bb9f67bb9dc3b3d036ba40cc533817bf
Jason Wang <vuwij1@gmail.com>
2022-07-08 13:12:31 +0900

    Record 3d animation on each webots run

---
+++ soccerbot/bags/view_simulation.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# example: ./view_rosbag.sh red_robot2
+
+google-chrome --disable-web-security --user-data-dir=.chrome --disable-fre ../../external/webots/projects/samples/contests/robocup/controllers/referee/recording.html
