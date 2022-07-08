449fbd92c98ec77703177df4585c6edc32c8eb3a
Jason Wang <vuwij@me.com>
2022-07-08 20:30:51 +0900

    Fix yet another initial falling issue (#566)
    Co-authored-by: Jason Wang <vuwij1@gmail.com>

--- soccer_webots/src/soccer_webots/bridge_webots.py
+++ soccer_webots/src/soccer_webots/bridge_webots.py
@@ -174,6 +174,7 @@ class GameControllerBridge:
         self.pub_camera = rospy.Publisher("camera/image_raw", Image, queue_size=1)
         self.pub_camera_info = rospy.Publisher("camera/camera_info", CameraInfo, queue_size=1, latch=True)
         self.pub_imu = rospy.Publisher("imu_raw", Imu, queue_size=1)
+        self.pub_imu_first = True
         self.pressure_sensors_pub = {i: rospy.Publisher("foot_contact_{}".format(i), Bool, queue_size=10) for i in range(8)}
         self.pub_joint_states = rospy.Publisher("joint_states", JointState, queue_size=1)
 
@@ -282,6 +283,12 @@ class GameControllerBridge:
                 imu_msg.angular_velocity.x = ((value.X + 32768) / 65535) * (8.7266 * 2) - 8.7266
                 imu_msg.angular_velocity.y = ((value.Y + 32768) / 65535) * (8.7266 * 2) - 8.7266
                 imu_msg.angular_velocity.z = ((value.Z + 32768) / 65535) * (8.7266 * 2) - 8.7266
+
+        if self.pub_imu_first:
+            if imu_msg.linear_acceleration.z > 10 or imu_msg.linear_acceleration.z < 8:
+                return
+            else:
+                self.pub_imu_first = False
 
         if imu_accel and imu_gyro:
             self.pub_imu.publish(imu_msg)
