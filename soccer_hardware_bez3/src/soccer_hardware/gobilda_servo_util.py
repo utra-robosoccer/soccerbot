e9f7c94d0074dc06c2e68fd74dc6b2fbcd14eb89
Jonathan Spraggett <jonathanspraggett@gmail.com>
2022-07-11 19:09:57 +0700

    Soccer hardware bez3 4 (#584)
    * Work in progress
    
    * Deleted unnecessary files
    
    * removed swap files
    
    * Delete bdock
    
    * Delete dock
    
    * Delete docker-commit
    
    * Delete docker-compose.robot.bak.yaml
    
    * Delete docker-compose.robot.new.yaml
    
    * Delete docker-run
    
    * added some changes so it will run
    
    * added
    
    * Delete setup_serial
    
    Co-authored-by: Jason Wang <vuwij@me.com>

---
+++ soccer_hardware_bez3/src/soccer_hardware/gobilda_servo_util.py
@@ -0,0 +1,8 @@
+from common_motor_util import bytepack, crc32mpeg2, le_crc, pack6
+
+
+def uart_transact(ser, B):
+    bs = pack6(B)
+    crc = (1 << 6) | (le_crc(bs) & 0x3F)
+    # print(bs, crc)
+    ser.write(bs + bytepack(crc))
---
+++ soccer_hardware_bez3/src/soccer_hardware/imu_receiver.py
@@ -0,0 +1,41 @@
+import struct
+import time
+from threading import Event, Lock, Thread
+
+import rospy as rp
+import serial
+from receiver import Receiver
+from transformations import *
+from utility import log_string
+
+NAN_HEAD = b"\x00\x00\xc0\x7f"
+# looking specifically for the NAN constant from STM32 C libs
+# generalized IEEE754 NaN is all-1s exponent with any non-zero significand
+# so math.isnan is too porous a filter
+
+
+class IMUReceiver(Receiver):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._working_buf = b""
+
+    def _receive_packet_from_mcu(self, timeout):
+        #print(self._working_buf)
+        self._ser.timeout = timeout
+        self._working_buf += self._ser.read(28)
+        #print("IMU RECEIVER: ", self._working_buf)
+        D = self._working_buf.split(NAN_HEAD)
+        ret = (False, None)
+        if len(D) > 0:
+            # if len(D[-1]) >= 24: # clear the buffer if it's readable
+            # 	self._working_buf = b''
+            # else:
+            # 	self._working_buf = D[-1]
+
+            for d in D[::-1]:
+                if len(d) >= 24:
+                    ret = (True, struct.unpack("<" + "f" * 6, d[:24]))
+                    break
+            self._working_buf = D[-1]
+
+        return ret
