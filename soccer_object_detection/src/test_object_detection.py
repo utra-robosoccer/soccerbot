8656ac49594a0136bea392795d7809fa1eed9774
Nam Nguyen <lkunelk@gmail.com>
2022-07-14 19:27:40 -0700

    add unit test

--- soccer_object_detection/src/test_object_detection.py
+++ soccer_object_detection/src/test_object_detection.py
@@ -3,54 +3,55 @@ from unittest import TestCase
 
 import cv2
 import torch
-from PIL import Image
-
-from soccer_object_detection.msg import BoundingBox, BoundingBoxes
 
 
-def copy_attr(a, b, include=(), exclude=()):
-    # Copy attributes from b to a, options to only include [...] and to exclude [...]
-    for k, v in b.__dict__.items():
-        if (len(include) and k not in include) or k.startswith("_") or k in exclude:
-            continue
-        else:
-            setattr(a, k, v)
+def IoU(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    xA = max(boxA[0], boxB[0])
+    yA = max(boxA[1], boxB[1])
+    xB = min(boxA[2], boxB[2])
+    yB = min(boxA[3], boxB[3])
+    # compute the area of intersection rectangle
+    interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+    # compute the area of both the prediction and ground-truth
+    # rectangles
+    boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+    boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / float(boxAArea + boxBArea - interArea)
+    # return the intersection over union value
+    return iou
 
 
 class Test(TestCase):
     def test_ball_image(self):
-        print("hi -------------------->")
-        # training set: 'https://universe.roboflow.com/nihal-saxena/my-ball/dataset/1'
-        # model = torch.hub.load("ultralytics/yolov5", "yolov5m")
-        model_path = os.path.dirname(os.path.realpath(__file__)) + "/" + "July14.pt"
+        CONFIDENCE_THRESHOLD = 0.6
+        BALL_CLASS = 0
+        src_path = os.path.dirname(os.path.realpath(__file__))
+        model_path = src_path + "/small_model/July14.pt"
+        test_path = src_path + "/test_image"
+
         model = torch.hub.load("ultralytics/yolov5", "custom", path=model_path)
-        # checkpoint_ = torch.load('/home/nam/catkin_ws/src/soccerbot/soccer_object_detection/src/yolov5/runs/train/exp4/weights/best.pt')['model']
-        # model.load_state_dict(checkpoint_.state_dict())
 
-        # copy_attr(model, checkpoint_, include=('yaml', 'nc', 'hyp', 'names', 'stride'), exclude=())
-
-        # model = model.fuse().autoshape()
         if torch.cuda.is_available():
             model.cuda()
 
-        # 1. preprocess image
-        img = cv2.imread("test_image/ball1.png")
-        # img = cv2.imread('/home/nam/catkin_ws/src/soccerbot/soccer_object_detection/src/test_image/81-test_nagoya_game_c_00050.png')
-        # img = cv2.imread('/home/nam/catkin_ws/src/soccerbot/soccer_object_detection/src/datasets/balls1/train/images/100--83-_jpg.rf.06a9f110a44529544e9976c9858f6d68.jpg')
-        # img = cv2.imread('/home/nam/catkin_ws/src/soccerbot/soccer_object_detection/src/yolov5/runs/train/exp4/val_batch2_pred.jpg')
-        cv2.imshow("name", img)
-        cv2.waitKey(0)
-        # img = image[:, :, :3]  # get rid of alpha channel
-        # img = img[..., ::-1]  # convert bgr to rgb
+        for file_name in os.listdir(test_path):
+            img = cv2.imread(os.path.join(test_path, file_name))  # ground truth box = (68, 89) (257, 275)
+            ground_truth_boxes = file_name[:-4].split("_")[1:]  # strip name and .png
+            ground_truth_boxes = list(map(int, ground_truth_boxes))
 
-        # 2. inference
-        results = model(img)
+            results = model(img)  # you can print(results) for more info
 
-        print(results)
+            for prediction in results.xyxy[0]:
+                x1, y1, x2, y2, confidence, img_class = prediction.cpu().numpy()
+                if img_class == BALL_CLASS:
+                    iou = IoU([x1, y1, x2, y2], ground_truth_boxes)
+                    self.assertGreater(iou, CONFIDENCE_THRESHOLD, "bounding boxes are off by too much!")
 
-        for prediction in results.xyxy[0]:
-            x1, y1, x2, y2, confidence, img_class = prediction.cpu().numpy()
-            print(x1, y1, x2, y2, confidence, img_class)
-            img_detect = cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
-            cv2.imshow("res", img_detect)
-            cv2.waitKey(0)
+                    # uncomment to view bounding boxes
+                    # img_detect = cv2.rectangle(img, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+                    # cv2.imshow("res", img_detect)
+                    # cv2.waitKey(0)
