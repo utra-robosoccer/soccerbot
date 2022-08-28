3de13f9198641f7114811dce7591aba23ef684f0
Jason Wang <vuwij1@gmail.com>
2022-08-28 21:46:22 +0900

    Fix paths

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../../../devel/lib/python3/dist-packages/'))
 sys.path.insert(0, os.path.abspath('/opt/ros/noetic/lib/python3/dist-packages/'))
-sys.path.insert(0, os.path.abspath('..'))
 
 
 
