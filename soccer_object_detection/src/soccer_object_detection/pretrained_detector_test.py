4151b089a061eabee94966d72a753dd47351b95d
Nam Nguyen <lkunelk@gmail.com>
2022-09-18 19:23:54 -0700

    Clean Up Object Detection (#627)
    * update readme
    
    * fix error, img not published
    
    looks like ims was renamed to imgs
    
    * delete outdated code
    
    * fix error
    
    * fix error 2 - pytorch version related

--- soccer_object_detection/src/soccer_object_detection/pretrained_detector_test.py
+++
@@ -1,4 +0,0 @@
-# https://cv.gluon.ai/build/examples_detection/skip_fintune.html#load-a-pretrained-model
-
-
-# https://pyimagesearch.com/2021/08/02/pytorch-object-detection-with-pre-trained-networks/
