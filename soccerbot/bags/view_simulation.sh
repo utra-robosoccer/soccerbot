174564ffdd93ff6b72f5f975f42f5087a6b7e237
Jason Wang <vuwij1@gmail.com>
2022-10-02 22:19:53 +0900

    Fixes

--- soccerbot/bags/view_simulation.sh
+++ soccerbot/bags/view_simulation.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 # Simply run this file using ./view_simulation.sh to replay the last recorded webots session
 
-if [ -f "recording.html" ]; then
-    google-chrome --disable-web-security --user-data-dir=.chrome --disable-fre recording.html
-else
-    google-chrome --disable-web-security --user-data-dir=.chrome --disable-fre ../../external/webots/projects/samples/contests/robocup/controllers/referee/recording.html
-fi
+# If coming from CI, simply copy the recording.json into the folder (../../external/webots/projects/samples/contests/robocup/controllers/referee/) and then run
+# fix_recording_json.py and then run this file
+
+google-chrome --disable-web-security --user-data-dir=.chrome --disable-fre ../../external/webots/projects/samples/contests/robocup/controllers/referee/recording.html
