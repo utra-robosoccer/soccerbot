d15551c2738484f2e18d026cd85be44eaf8fe48e
Jason Wang <vuwij@me.com>
2022-10-02 21:38:21 +0900

    Graceful exit (#683)
    Co-authored-by: Jason Wang <vuwij1@gmail.com>

--- soccerbot/bags/view_simulation.sh
+++ soccerbot/bags/view_simulation.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 # Simply run this file using ./view_simulation.sh to replay the last recorded webots session
 
-google-chrome --disable-web-security --user-data-dir=.chrome --disable-fre ../../external/webots/projects/samples/contests/robocup/controllers/referee/recording.html
+if [ -f "recording.html" ]; then
+    google-chrome --disable-web-security --user-data-dir=.chrome --disable-fre recording.html
+else
+    google-chrome --disable-web-security --user-data-dir=.chrome --disable-fre ../../external/webots/projects/samples/contests/robocup/controllers/referee/recording.html
+fi
