434d67bca5f8711ba9e6ba5d19cc344c1940a092
Jason Wang <vuwij1@gmail.com>
2022-11-27 00:55:29 +0900

    Fix more tests

--- soccer_trajectories/src/soccer_trajectories/test_trajectory.py
+++ soccer_trajectories/src/soccer_trajectories/test_trajectory.py
@@ -1,40 +1,29 @@
 import os
 
+os.environ["ROS_NAMESPACE"] = "/robot1"
+
 import pytest
 import rospy
 
-if "ROS_NAMESPACE" not in os.environ:
-    os.environ["ROS_NAMESPACE"] = "/robot1"
-
-real_robot = False
-
-display = False
-TEST_TIMEOUT = 60
-
-os.system(
-    "/bin/bash -c 'source /opt/ros/noetic/setup.bash && rosnode kill /robot1/soccer_strategy /robot1/soccer_pycontrol /robot1/soccer_trajectories'"
-)
-
-robot_model = "bez1"
-if real_robot:
-    from soccer_msgs.msg import FixedTrajectoryCommand
-else:
-    from soccer_common.utils_rosparam import set_rosparam_from_yaml_file
-
-    file_path = os.path.dirname(os.path.abspath(__file__))
-    config_path = f"{file_path}/../../../{robot_model}_description/config/motor_mapping.yaml"
-
-    from unittest.mock import MagicMock
-
-    set_rosparam_from_yaml_file(param_path=config_path)
-    FixedTrajectoryCommand = MagicMock()
-
-from soccer_trajectories.soccer_trajectories import SoccerTrajectoryClass
+from soccer_common.utils_rosparam import set_rosparam_from_yaml_file
+from soccer_msgs.msg import FixedTrajectoryCommand
+from soccer_trajectories.soccer_trajectories import SoccerTrajectoryClass, Trajectory
 
 
 class TestTrajectory:
+    @pytest.mark.parametrize("robot_model", ["bez1", "bez1_sim"])
     @pytest.mark.parametrize("trajectory_name", ["getupfront", "getupback", "getupside", "rightkick"])
-    def test_all_trajectories(self, trajectory_name: str):
+    def test_all_trajectories(self, robot_model: str, trajectory_name: str):
+        os.system(
+            "/bin/bash -c 'source /opt/ros/noetic/setup.bash && rosnode kill /robot1/soccer_strategy /robot1/soccer_pycontrol /robot1/soccer_trajectories'"
+        )
+        robot_model = "bez1"
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        config_path = f"{file_path}/../../../{robot_model}_description/config/motor_mapping.yaml"
+        set_rosparam_from_yaml_file(param_path=config_path)
+
+        if "DISPLAY" not in os.environ:
+            Trajectory.RATE = 10000
         c = SoccerTrajectoryClass()
         rospy.init_node("test")
         msg = FixedTrajectoryCommand()
