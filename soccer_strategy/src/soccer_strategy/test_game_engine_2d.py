36a7108cdd462777c390a9680c5b034c451e2f0a
Daniel <danielchen7890@gmail.com>
2022-12-18 00:04:35 -0500

    WIP

--- soccer_strategy/src/soccer_strategy/test_game_engine_2d.py
+++ soccer_strategy/src/soccer_strategy/test_game_engine_2d.py
@@ -5,10 +5,15 @@ os.environ["ROS_NAMESPACE"] = "/robot1"
 from unittest import TestCase
 from unittest.mock import MagicMock
 
-import numpy
 import numpy as np
 import rospy
-from vispy import app, scene
+
+from soccer_strategy.game_engine_2d import GameEngine2D
+from soccer_strategy.robot import Robot
+from soccer_strategy.robot_controlled_2d import RobotControlled2D
+from soccer_strategy.strategy.strategy_dummy import StrategyDummy
+from soccer_strategy.strategy.strategy_stationary import StrategyStationary
+from soccer_strategy.team import Team
 
 
 class TestGameEngine2D(TestCase):
@@ -22,10 +27,6 @@ class TestGameEngine2D(TestCase):
     def test_dummy_vs_stationary_strategy(self):
         rospy.init_node("test")
 
-        from soccer_strategy.game_engine_2d import GameEngine2D
-        from soccer_strategy.strategy.strategy_dummy import StrategyDummy
-        from soccer_strategy.strategy.strategy_stationary import StrategyStationary
-
         g = GameEngine2D(display=self.display, team_1_strategy=StrategyDummy, team_2_strategy=StrategyStationary, game_duration=2)
         friendly_points, opponent_points = g.run()
         print(f"Friendly: {friendly_points}, opponent: {opponent_points}")
@@ -33,9 +34,6 @@ class TestGameEngine2D(TestCase):
 
     def test_dummy_vs_dummy_strategy(self):
         rospy.init_node("test")
-
-        from soccer_strategy.game_engine_2d import GameEngine2D
-        from soccer_strategy.strategy.strategy_dummy import StrategyDummy
 
         g = GameEngine2D(display=self.display, team_1_strategy=StrategyDummy, team_2_strategy=StrategyDummy, game_duration=2)
         friendly_points, opponent_points = g.run()
@@ -45,10 +43,10 @@ class TestGameEngine2D(TestCase):
         from soccer_strategy.strategy.utils import Utility
 
         robot = MagicMock()
-        goal_position = numpy.array([5, 0])
+        goal_position = np.array([5, 0])
 
-        robot.position = goal_position - numpy.array([0.4, 0.2])
-        ball_position = goal_position - numpy.array([0.2, 0.4])
+        robot.position = goal_position - np.array([0.4, 0.2])
+        ball_position = goal_position - np.array([0.2, 0.4])
         Utility.navigate_to_position_with_offset = MagicMock()
         Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
         assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
@@ -59,10 +57,10 @@ class TestGameEngine2D(TestCase):
         from soccer_strategy.strategy.utils import Utility
 
         robot = MagicMock()
-        goal_position = numpy.array([5, 0])
+        goal_position = np.array([5, 0])
 
-        robot.position = goal_position - numpy.array([1, 1.0])
-        ball_position = goal_position - numpy.array([0.4, 1.2])
+        robot.position = goal_position - np.array([1, 1.0])
+        ball_position = goal_position - np.array([0.4, 1.2])
         Utility.navigate_to_position_with_offset = MagicMock()
         Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
         assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
@@ -73,10 +71,10 @@ class TestGameEngine2D(TestCase):
         from soccer_strategy.strategy.utils import Utility
 
         robot = MagicMock()
-        goal_position = numpy.array([5, 0])
+        goal_position = np.array([5, 0])
 
-        robot.position = goal_position - numpy.array([0.4, 1.0])
-        ball_position = goal_position - numpy.array([0.4, 1.2])
+        robot.position = goal_position - np.array([0.4, 1.0])
+        ball_position = goal_position - np.array([0.4, 1.2])
         Utility.navigate_to_position_with_offset = MagicMock()
         Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
         assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
@@ -88,10 +86,10 @@ class TestGameEngine2D(TestCase):
         from soccer_strategy.strategy.utils import Utility
 
         robot = MagicMock()
-        goal_position = numpy.array([5, 0])
+        goal_position = np.array([5, 0])
 
-        robot.position = goal_position - numpy.array([0.5, 1.0])
-        ball_position = goal_position - numpy.array([0.4, 1.4])
+        robot.position = goal_position - np.array([0.5, 1.0])
+        ball_position = goal_position - np.array([0.4, 1.4])
         Utility.navigate_to_position_with_offset = MagicMock()
         Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
         assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
@@ -102,10 +100,10 @@ class TestGameEngine2D(TestCase):
         from soccer_strategy.strategy.utils import Utility
 
         robot = MagicMock()
-        goal_position = numpy.array([5, 0])
+        goal_position = np.array([5, 0])
 
-        robot.position = goal_position - numpy.array([1.0, 1.0])
-        ball_position = goal_position - numpy.array([0.8, 0.5])
+        robot.position = goal_position - np.array([1.0, 1.0])
+        ball_position = goal_position - np.array([0.8, 0.5])
         Utility.navigate_to_position_with_offset = MagicMock()
         Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
         assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
@@ -116,12 +114,47 @@ class TestGameEngine2D(TestCase):
         from soccer_strategy.strategy.utils import Utility
 
         robot = MagicMock()
-        for goal_position in [numpy.array([5, 0]), numpy.array([-5, 0])]:
+        for goal_position in [np.array([5, 0]), np.array([-5, 0])]:
             for robot_x in np.linspace(-6, 6, 5):
                 for robot_y in np.linspace(-3, 3, 5):
                     for ball_x in np.linspace(-6, 6, 5):
                         for ball_y in np.linspace(-3, 3, 5):
-                            robot.position = goal_position - numpy.array([robot_x, robot_y])
-                            ball_position = goal_position - numpy.array([ball_x, ball_y])
+                            robot.position = goal_position - np.array([robot_x, robot_y])
+                            ball_position = goal_position - np.array([ball_x, ball_y])
                             Utility.navigate_to_position_with_offset = MagicMock()
                             Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
+
+    def test_obstacle_detection(self):
+        rospy.init_node("test")
+
+        team1 = Team(
+            [
+                RobotControlled2D(
+                    robot_id=1,
+                    team=Robot.Team.FRIENDLY,
+                    role=Robot.Role.STRIKER,
+                    status=Robot.Status.READY,
+                    position=np.array([-0.5, 0, 0]),
+                )
+            ]
+        )
+
+        team2 = Team(
+            [
+                RobotControlled2D(
+                    robot_id=2,
+                    team=Robot.Team.OPPONENT,
+                    role=Robot.Role.GOALIE,
+                    status=Robot.Status.READY,
+                    position=np.array([2.0, 0, -3.14]),
+                )
+            ]
+        )
+
+        g = GameEngine2D(
+            display=self.display, team_1_strategy=StrategyDummy, team_2_strategy=StrategyStationary, team_1=team1, team_2=team2, game_duration=2
+        )
+
+        friendly_points, opponent_points = g.run()
+        print(f"Friendly: {friendly_points}, opponent: {opponent_points}")
+        assert not (friendly_points == 0 and opponent_points == 0)
