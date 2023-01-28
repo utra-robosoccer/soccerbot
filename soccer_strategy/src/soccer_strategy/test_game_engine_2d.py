5ae337dc2aca2fddc1eabaef6122c2ec839f4788
Jason Wang <vuwij1@gmail.com>
2023-01-29 01:23:15 +0900

    Fix pttest

--- soccer_strategy/src/soccer_strategy/test_game_engine_2d.py
+++ soccer_strategy/src/soccer_strategy/test_game_engine_2d.py
@@ -13,6 +13,7 @@ from soccer_strategy.robot import Robot
 from soccer_strategy.robot_controlled_2d import RobotControlled2D
 from soccer_strategy.strategy.strategy_dummy import StrategyDummy
 from soccer_strategy.strategy.strategy_stationary import StrategyStationary
+from soccer_strategy.strategy.utils import Utility
 from soccer_strategy.team import Team
 
 
@@ -27,7 +28,7 @@ class TestGameEngine2D(TestCase):
     def test_dummy_vs_stationary_strategy(self):
         rospy.init_node("test")
 
-        g = GameEngine2D(display=self.display, team_1_strategy=StrategyDummy, team_2_strategy=StrategyStationary, game_duration=6)
+        g = GameEngine2D(display=self.display, team_1_strategy=StrategyDummy, team_2_strategy=StrategyStationary, game_duration=4)
         friendly_points, opponent_points = g.run()
         print(f"Friendly: {friendly_points}, opponent: {opponent_points}")
         assert not (friendly_points == 0 and opponent_points == 0)
@@ -35,94 +36,9 @@ class TestGameEngine2D(TestCase):
     def test_dummy_vs_dummy_strategy(self):
         rospy.init_node("test")
 
-        g = GameEngine2D(display=self.display, team_1_strategy=StrategyDummy, team_2_strategy=StrategyDummy, game_duration=4)
+        g = GameEngine2D(display=self.display, team_1_strategy=StrategyDummy, team_2_strategy=StrategyDummy, game_duration=2)
         friendly_points, opponent_points = g.run()
         print(f"Friendly: {friendly_points}, opponent: {opponent_points}")
-
-    def test_navigate_to_scoring_position_with_offset_case_1(self):
-        from soccer_strategy.strategy.utils import Utility
-
-        robot = MagicMock()
-        goal_position = np.array([5, 0])
-
-        robot.position = goal_position - np.array([0.4, 0.2])
-        ball_position = goal_position - np.array([0.2, 0.4])
-        Utility.navigate_to_position_with_offset = MagicMock()
-        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
-        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], np.array([5, -0.6]))
-
-    def test_navigate_to_scoring_position_with_offset_case_2a(self):
-        from soccer_strategy.strategy.utils import Utility
-
-        robot = MagicMock()
-        goal_position = np.array([5, 0])
-
-        robot.position = goal_position - np.array([1, 1.0])
-        ball_position = goal_position - np.array([0.4, 1.2])
-        Utility.navigate_to_position_with_offset = MagicMock()
-        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
-        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], np.array([5, -1.2]))
-
-    def test_navigate_to_scoring_position_with_offset_case_2b(self):
-        from soccer_strategy.strategy.utils import Utility
-
-        robot = MagicMock()
-        goal_position = np.array([5, 0])
-
-        robot.position = goal_position - np.array([0.4, 1.0])
-        ball_position = goal_position - np.array([0.4, 1.2])
-        Utility.navigate_to_position_with_offset = MagicMock()
-        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
-        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], np.array([5, -1.2]))
-        assert Utility.navigate_to_position_with_offset.call_args.kwargs["offset"] == 0.3
-
-    def test_navigate_to_scoring_position_with_offset_case_3_out_of_the_edge(self):
-        from soccer_strategy.strategy.utils import Utility
-
-        robot = MagicMock()
-        goal_position = np.array([5, 0])
-
-        robot.position = goal_position - np.array([0.5, 1.0])
-        ball_position = goal_position - np.array([0.4, 1.4])
-        Utility.navigate_to_position_with_offset = MagicMock()
-        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
-        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], goal_position)
-
-    def test_navigate_to_scoring_position_with_offset_case_3_regular(self):
-        from soccer_strategy.strategy.utils import Utility
-
-        robot = MagicMock()
-        goal_position = np.array([5, 0])
-
-        robot.position = goal_position - np.array([1.0, 1.0])
-        ball_position = goal_position - np.array([0.8, 0.5])
-        Utility.navigate_to_position_with_offset = MagicMock()
-        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
-        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
-        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], goal_position)
-
-    def test_navigate_to_scoring_position_no_crash(self):
-        from soccer_strategy.strategy.utils import Utility
-
-        robot = MagicMock()
-        for goal_position in [np.array([5, 0]), np.array([-5, 0])]:
-            for robot_x in np.linspace(-6, 6, 5):
-                for robot_y in np.linspace(-3, 3, 5):
-                    for ball_x in np.linspace(-6, 6, 5):
-                        for ball_y in np.linspace(-3, 3, 5):
-                            robot.position = goal_position - np.array([robot_x, robot_y])
-                            ball_position = goal_position - np.array([ball_x, ball_y])
-                            Utility.navigate_to_position_with_offset = MagicMock()
-                            Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
 
     def test_obstacle_detection(self):
         rospy.init_node("test")
@@ -164,3 +80,95 @@ class TestGameEngine2D(TestCase):
 
         friendly_points, opponent_points = g.run()
         print(f"Friendly: {friendly_points}, opponent: {opponent_points}")
+        assert not (friendly_points == 0 and opponent_points == 0)
+
+    def test_navigate_to_scoring_position_with_offset_case_1(self):
+
+        robot = MagicMock()
+        goal_position = np.array([5, 0])
+
+        robot.position = goal_position - np.array([0.4, 0.2])
+        ball_position = goal_position - np.array([0.2, 0.4])
+        navigate_to_position_with_offset_original = Utility.navigate_to_position_with_offset
+        Utility.navigate_to_position_with_offset = MagicMock()
+        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
+        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], np.array([5, -0.6]))
+        Utility.navigate_to_position_with_offset = navigate_to_position_with_offset_original
+
+    def test_navigate_to_scoring_position_with_offset_case_2a(self):
+
+        robot = MagicMock()
+        goal_position = np.array([5, 0])
+
+        robot.position = goal_position - np.array([1, 1.0])
+        ball_position = goal_position - np.array([0.4, 1.2])
+        navigate_to_position_with_offset_original = Utility.navigate_to_position_with_offset
+        Utility.navigate_to_position_with_offset = MagicMock()
+        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
+        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], np.array([5, -1.2]))
+        Utility.navigate_to_position_with_offset = navigate_to_position_with_offset_original
+
+    def test_navigate_to_scoring_position_with_offset_case_2b(self):
+
+        robot = MagicMock()
+        goal_position = np.array([5, 0])
+
+        robot.position = goal_position - np.array([0.4, 1.0])
+        ball_position = goal_position - np.array([0.4, 1.2])
+        navigate_to_position_with_offset_original = Utility.navigate_to_position_with_offset
+        Utility.navigate_to_position_with_offset = MagicMock()
+        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
+        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], np.array([5, -1.2]))
+        assert Utility.navigate_to_position_with_offset.call_args.kwargs["offset"] == 0.3
+        Utility.navigate_to_position_with_offset = navigate_to_position_with_offset_original
+
+    def test_navigate_to_scoring_position_with_offset_case_3_out_of_the_edge(self):
+
+        robot = MagicMock()
+        goal_position = np.array([5, 0])
+
+        robot.position = goal_position - np.array([0.5, 1.0])
+        ball_position = goal_position - np.array([0.4, 1.4])
+        navigate_to_position_with_offset_original = Utility.navigate_to_position_with_offset
+        Utility.navigate_to_position_with_offset = MagicMock()
+        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
+        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], goal_position)
+        Utility.navigate_to_position_with_offset = navigate_to_position_with_offset_original
+
+    def test_navigate_to_scoring_position_with_offset_case_3_regular(self):
+
+        robot = MagicMock()
+        goal_position = np.array([5, 0])
+
+        robot.position = goal_position - np.array([1.0, 1.0])
+        ball_position = goal_position - np.array([0.8, 0.5])
+        navigate_to_position_with_offset_original = Utility.navigate_to_position_with_offset
+        Utility.navigate_to_position_with_offset = MagicMock()
+        Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
+        assert Utility.navigate_to_position_with_offset.call_args[0][0] == robot
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][1], ball_position)
+        assert np.array_equal(Utility.navigate_to_position_with_offset.call_args[0][2], goal_position)
+        Utility.navigate_to_position_with_offset = navigate_to_position_with_offset_original
+
+    def test_navigate_to_scoring_position_no_crash(self):
+
+        robot = MagicMock()
+        for goal_position in [np.array([5, 0]), np.array([-5, 0])]:
+            for robot_x in np.linspace(-6, 6, 5):
+                for robot_y in np.linspace(-3, 3, 5):
+                    for ball_x in np.linspace(-6, 6, 5):
+                        for ball_y in np.linspace(-3, 3, 5):
+                            robot.position = goal_position - np.array([robot_x, robot_y])
+                            ball_position = goal_position - np.array([ball_x, ball_y])
+                            navigate_to_position_with_offset_original = Utility.navigate_to_position_with_offset
+                            Utility.navigate_to_position_with_offset = MagicMock()
+                            Utility.navigate_to_scoring_position(robot, ball_position, goal_position)
+                            Utility.navigate_to_position_with_offset = navigate_to_position_with_offset_original
