9983bf4dbaac462bb897b15aee0720a870263a4e
manx52 <jonathanspraggett@gmail.com>
2023-05-06 03:06:45 -0400

    Started testing for bez2 trajectories

--- soccer_trajectories/src/soccer_trajectories/test_trajectory.py
+++ soccer_trajectories/src/soccer_trajectories/test_trajectory.py
@@ -11,17 +11,16 @@ from soccer_trajectories.soccer_trajectories import Trajectory, TrajectoryManage
 
 
 class TestTrajectory:
-    @pytest.mark.parametrize("robot_model", ["bez1", "bez1_sim"])
-    @pytest.mark.parametrize("trajectory_name", ["getupfront", "getupback", "getupside", "rightkick"])
+    @pytest.mark.parametrize("robot_model", ["bez2_sim"])
+    @pytest.mark.parametrize("trajectory_name", ["getupfront"])  # , "getupback", "getupside", "rightkick"])
     def test_all_trajectories(self, robot_model: str, trajectory_name: str):
         rospy.init_node("test")
 
         os.system(
             "/bin/bash -c 'source /opt/ros/noetic/setup.bash && rosnode kill /robot1/soccer_strategy /robot1/soccer_pycontrol /robot1/soccer_trajectories'"
         )
-        robot_model = "bez1"
         file_path = os.path.dirname(os.path.abspath(__file__))
-        config_path = f"{file_path}/../../../{robot_model}_description/config/motor_mapping.yaml"
+        config_path = f"{file_path}/../../../{robot_model.replace('_sim','')}_description/config/motor_mapping.yaml"
         set_rosparam_from_yaml_file(param_path=config_path)
 
         if "DISPLAY" not in os.environ:
@@ -32,4 +31,4 @@ class TestTrajectory:
         msg.trajectory_name = trajectory_name
         msg.mirror = True
         c.command_callback(command=msg)
-        c.trajectory.run(real_time=False)
+        c.trajectory.run(real_time=True)
