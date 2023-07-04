17e242cf781aeffa875b5811ac80349c0b64849b
Bez3 SJC <derek@lam.io>
2023-07-03 22:08:39 -0700

    Fix usb.sh self-insert into sudoers
    usb.sh comes with functionality to shove itself into sudoers so that any
    user can run it, so it only has to be run with sudo the first time.
    Wasn't working before, now it is.

--- soccer_hardware_bez3/src/soccer_hardware/usb.sh
+++ soccer_hardware_bez3/src/soccer_hardware/usb.sh
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 set -e
-setserial /dev/ttyUSB* low_latency && setserial /dev/ttyACM* low_latency
 SUDOERD_BASENAME="sudo_setserial"
 SUDOERD_FILE="/etc/sudoers.d/$SUDOERD_BASENAME"
 SUDOER_LINE="$(logname) ALL=(root) NOPASSWD: $(realpath "$0")"
 EDIT_CMD="echo '$SUDOER_LINE' >> $SUDOERD_FILE"
-if [ -e SUDOERD_FILE ]; then
-	[ -z "$(grep -F EDIT_CMD SUDOERD_FILE)" ] && exit 0;
-else
-	touch SUDOERD_FILE
+if ! [ -e "$SUDOERD_FILE" ]; then
+	echo "!!!"
+	sudo touch "$SUDOERD_FILE"
 fi
 
-echo "$SUDOER_LINE" | sudo EDITOR='tee -a' visudo -f "$SUDOERD_FILE"
+if [ -z "$(grep -F "$SUDOER_LINE" "$SUDOERD_FILE")" ]; then
+	echo "$SUDOER_LINE" | sudo EDITOR='tee -a' visudo -f "$SUDOERD_FILE"
+fi
+
+set +e
+setserial /dev/ttyUSB* low_latency
+setserial /dev/ttyACM* low_latency
+setserial /dev/ttyTHS* low_latency
+echo "Set serials to low latency"
