6857db503712168a380ec39d3bf1fa63d683cde0
manx52 <jonathanspraggett@gmail.com>
2024-04-13 20:14:50 -0400

    so close to understanding the equations

--- soccer_control/soccer_pycontrol/src/soccer_pycontrol/pinnoccio_robot_wrapper.py
+++ soccer_control/soccer_pycontrol/src/soccer_pycontrol/pinnoccio_robot_wrapper.py
@@ -28,20 +28,20 @@ robot = RobotWrapper.BuildFromURDF(urdf_model_path, mesh_dir)
 # alias
 model = robot.model
 data = robot.data
-q = [0] * 18  # pinocchio.randomConfiguration(model)
+# q = [0] * 18  # pinocchio.randomConfiguration(model)
 # q = pinocchio.randomConfiguration(model)
 # for i in model.names:
 #     print(i, model.getJointId(i))
-q[model.getJointId("right_leg_motor_0") - 1 : model.getJointId("right_leg_motor_5")] = [
-    -0.0,
-    -2.220446049250313e-16,
-    0.6207569371022629,
-    -1.2971196567286807,
-    0.6763627196264181,
-    0.0,
-]
-q = np.array(q)
-print("q: %s" % q.T)
+# q[model.getJointId("right_leg_motor_0") - 1 : model.getJointId("right_leg_motor_5")] = [
+#     -0.0,
+#     -2.220446049250313e-16,
+#     0.6207569371022629,
+#     -1.2971196567286807,
+#     0.6763627196264181,
+#     0.0,
+# ]
+# q = np.array(q)
+# print("q: %s" % q.T)
 # print(model.getJointId("right_leg_motor_0"))
 # for i in model.names:
 #     print(i, model.getJointId(i))
@@ -60,44 +60,46 @@ print("q: %s" % q.T)
 # Show model with a visualizer of your choice
 import time
 
-for i in range(1000):
-    sart = time.time()
-    JOINT_ID = model.getJointId("right_leg_motor_5")
-    oMdes = pinocchio.SE3(np.eye(3), np.array([0.013500063680112362, -0.03500000014901161, -0.292939692735672]))
+# for i in range(1000):
+sart = time.time()
+JOINT_ID = model.getJointId("right_leg_motor_5")
+i = np.array([[1.0, 0.0, 0.0], [0.0, 0.0, -1.0], [0.0, 1.0, 0.0]])
+oMdes = pinocchio.SE3(i, np.array([0.013500063680112362, -0.13500000014901161, -0.292939692735672]))
+# oMdes = pinocchio.SE3(np.eye(3), np.array([0.0135, -0.135, -0.29289]))
 
-    # TODO try pink since this is doing a full body
-    q = pinocchio.neutral(model)
-    eps = 1e-4
-    IT_MAX = 1000
-    DT = 1e-1
-    damp = 1e-12
+# TODO try pink since this is doing a full body
+q = pinocchio.neutral(model)
+eps = 1e-4
+IT_MAX = 1000
+DT = 1e-1
+damp = 1e-12
 
-    i = 0
-    while True:
-        pinocchio.forwardKinematics(model, data, q)
-        iMd = data.oMi[JOINT_ID].actInv(oMdes)
-        err = pinocchio.log(iMd).vector  # in joint frame
-        if norm(err) < eps:
-            success = True
-            break
-        if i >= IT_MAX:
-            success = False
-            break
-        J = pinocchio.computeJointJacobian(model, data, q, JOINT_ID)  # in joint frame
-        J = -np.dot(pinocchio.Jlog6(iMd.inverse()), J)
-        v = -J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
-        q = pinocchio.integrate(model, q, v * DT)
-        # if not i % 10:
-        #     print('%d: error = %s' % (i, err.T))
-        i += 1
+i = 0
+while True:
+    pinocchio.forwardKinematics(model, data, q)
+    iMd = data.oMi[JOINT_ID].actInv(oMdes)
+    err = pinocchio.log(iMd).vector  # in joint frame
+    if norm(err) < eps:
+        success = True
+        break
+    if i >= IT_MAX:
+        success = False
+        break
+    J = pinocchio.computeJointJacobian(model, data, q, JOINT_ID)  # in joint frame
+    J = -np.dot(pinocchio.Jlog6(iMd.inverse()), J)
+    v = -J.T.dot(solve(J.dot(J.T) + damp * np.eye(6), err))
+    q = pinocchio.integrate(model, q, v * DT)
+    # if not i % 10:
+    #     print('%d: error = %s' % (i, err.T))
+    i += 1
 
-    print((time.time() - sart) * 1000)  # 0.001184225082397461
-    # if success:
-    #     print("Convergence achieved!")
-    # else:
-    #     print("\nWarning: the iterative algorithm has not reached convergence to the desired precision")
-    # print('\nresult: %s' % q.flatten().tolist())
-    # print('\nfinal error: %s' % err.T)
+print((time.time() - sart) * 1000)  # 0.001184225082397461
+if success:
+    print("Convergence achieved!")
+else:
+    print("\nWarning: the iterative algorithm has not reached convergence to the desired precision")
+print("\nresult: %s" % q.flatten().tolist())
+print("\nfinal error: %s" % err.T)
 
 if VISUALIZER:
     robot.setVisualizer(VISUALIZER())
--- soccer_control/soccer_pycontrol/src/soccer_pycontrol/soccerbot/ik.py
+++ soccer_control/soccer_pycontrol/src/soccer_pycontrol/soccerbot/ik.py
@@ -1,5 +1,8 @@
+import time
+
 import numpy as np
 import scipy
+from scipy.spatial.transform import Rotation as R
 
 from soccer_common import Transformation
 
@@ -17,7 +20,7 @@ class IK:
             ]
         )
 
-    def inverseKinematicsRightFoot(self, transformation):
+    def inverseKinematicsRightFoot(self, transformation: Transformation):
         """
         # Does the inverse kinematics calculation for the right foot
 
@@ -41,6 +44,8 @@ class IK:
         assert np.linalg.norm([Xd, Yd, Zd]) <= (d3 + d4)
 
         theta6 = -np.arctan2(Yd, Zd)
+
+        start = time.process_time_ns()
         tmp1 = Zd / np.cos(theta6)
         tmp2 = Xd
         D = (((((tmp1**2) + (tmp2**2)) - ((d3**2) + (d4**2))) / 2) / d3) / d4  # TODO what the fuck is this
@@ -54,7 +59,7 @@ class IK:
         assert theta4 < 4.6
 
         alp = np.arctan2(tmp1, tmp2)
-        beta = np.arctan2(-d3 * np.cos(tmp3), d4 + (d3 * np.sin(tmp3)))
+        beta = np.arctan2(-d3 * np.cos(tmp3), d4 + (d3 * np.sin(tmp3)))  # TODO why
         theta5 = np.pi / 2 - (alp - beta)
 
         H34 = Transformation(dh=[self.DH[3, 0], self.DH[3, 1], self.DH[3, 2], theta4])
@@ -66,17 +71,117 @@ class IK:
         assert np.linalg.norm(H03[0:3, 3]) - d3 < 0.03
 
         angles = Transformation(rotation_matrix=scipy.linalg.inv(H03[0:3, 0:3])).orientation_euler
-        theta3 = np.pi / 2 - angles[0]
+
         theta1 = -angles[1]
         theta2 = angles[2] + np.pi / 2
+        theta3 = np.pi / 2 - angles[0]
 
-        return [theta1, theta2, theta3, theta4, theta5, theta6]
+        H01 = Transformation(dh=[0, np.pi / 2, 0, theta1])
+        H12 = Transformation(dh=[0, np.pi / 2, 0, theta2 + np.pi / 2])
+        H23 = Transformation(dh=[d3, 0, 0, theta3])
+        H03_1 = np.matmul(H01, np.matmul(H12, H23))
+
+        H34 = Transformation(dh=[d4, 0, 0, theta4])
+        H45 = Transformation(dh=[0, -np.pi / 2, 0, theta5])
+        H56 = Transformation(dh=[0, 0, 0, theta6])
+        H36_2 = np.matmul(H34, np.matmul(H45, H56))
+        H03_2 = np.matmul(transformation, scipy.linalg.inv(H36_2))
+        # H03_2 = np.matmul(transformation, scipy.linalg.inv(H36))
+        # H03_2 = np.matmul(np.matmul(transformation, Transformation(euler=[0, np.pi / 2, 0])), scipy.linalg.inv(H36_2))
+        R12 = -np.sin(theta2) * np.cos(theta3)  # H03_2[0,1]
+        R22 = np.cos(theta2) * np.cos(theta3)  # H03_2[1,1]
+        R32 = np.sin(theta3)  # H03_2[2,1]
+        R31 = -np.cos(theta3) * np.sin(theta4)  # H03_2[2,0]
+        R33 = np.cos(theta3) * np.cos(theta4)  # H03_2[2,2]
+        R12 = H03_2[0, 1]
+        R22 = H03_2[1, 1]
+        R32 = H03_2[2, 1]
+        # R31 =  H03_2[2,0]
+        # R33 =  H03_2[2,2]
+        # t1 = np.arctan2(-R12, R22)
+        # t2 = np.arctan2(R32, -R12*np.sin(theta2) +R22*np.cos(theta2))
+        # t3 = np.arctan2(-R31, R33)
+        # # print(H03_2)
+        # print(np.isclose(H03_2, H03_1))
+        # print(np.isclose(H03_2, H03))
+        # angles_2 = Transformation(rotation_matrix=scipy.linalg.inv(H03_2[0:3, 0:3])).orientation_euler
+        # # H06 = np.matmul(H03_1,H36_2)
+        # # print(H06[0:3, 0:3])
+        # # print("here: ", np.matmul(H06, final_rotation)[0:3, 0:3])
+        #
+        # theta1_2 = -angles_2[2] - np.pi / 2
+        # theta2_2 = -angles_2[1]
+        # theta3_2 = -angles_2[0]
+        r1 = R.from_euler("zxy", [theta1, theta2, theta3])
+        r2 = R.from_euler("yx", [theta4 + theta5, theta6])
+        r3 = r1 * r2
+        # print(r3.as_matrix())
+        print(transformation[0:3, 0:3])
+
+        # -6.060296883738752e-14, 0.6307211857938324, 0.15238157118365828,
+        # print(np.matmul(transformation, final_rotation)[0:3, 0:3])
+        # print(theta1, theta2, theta3)
+        x1 = transformation[2, 0] * (np.sin(theta4) * np.sin(theta5) * np.sin(theta6) - np.cos(theta4) * np.cos(theta5) * np.cos(theta6))
+        x2 = transformation[2, 1] * (-np.sin(theta4) * np.sin(theta5) * np.sin(theta6) + np.cos(theta4) * np.cos(theta5) * np.sin(theta6))
+        x3 = transformation[2, 2] * (-np.sin(theta5) * np.cos(theta5) - np.sin(theta4) * np.cos(theta5))
+
+        # (-transformation[1,0] * np.sin(theta6) - transformation[1,1] * np.cos(theta6))/(np.sin(theta2 + np.pi/2))
+        t2 = np.arccos(transformation[2, 0] * np.sin(theta6) + transformation[2, 1] * np.cos(theta6)) - np.pi / 2
+        t1 = np.arcsin((-transformation[1, 0] * np.sin(theta6) - transformation[1, 1] * np.cos(theta6)) / (np.sin(t2 + np.pi / 2)))
+        t3 = np.arccos((x1 + x2 + x3) / (np.sin(t2 + np.pi / 2)))
+        print("hi")
+        # -2.220446049250313e-16 0.6309126945505448 0.15353047774239625
+        #
+        # return [theta1, theta2, theta3, theta4, theta5, theta6]
 
 
 ik = IK()
-t = Transformation(position=[0.0135, -0.035, -0.29289])
-import time
+t = Transformation(position=[0.0135, -0.135, -0.29289])  # , quaternion=(0.7, 0.0, 0.0, 0.7))
+# print(t.rotation_matrix)
+#  -0.3188203886880778, 0.16530755885403078, -0.6302562811565202]
 
+# -6.060296883738752e-14, 0.6307211857938324, 0.15238157118365828, -0.31647168907303647, 0.16409011788943692, -2.20142051098441]
 start = time.process_time_ns()
 ik.inverseKinematicsRightFoot(np.copy(t))
-print((time.process_time_ns() - start) / 1e6)
+# print((time.process_time_ns() - start) / 1e6)
+
+
+# def tmp():
+#     print((time.process_time_ns() - start) / 1e6)
+#     print(theta4,theta5,theta6)
+#     start = time.process_time_ns()
+#     r_2 = Xd ** 2 + Yd ** 2 + Zd ** 2
+#     num = (d3 ** 2 + d4 ** 2 - r_2)
+#     denom = (2 * d3 * d4)
+#     theta4_2 = np.arccos(num / denom) - np.pi  # TODO not sure why its neg
+#
+#     num = d3 * np.sin(np.pi + theta4_2)
+#     denom = np.sqrt(r_2)
+#     alpha = np.arcsin(num / denom)
+#     theta5_2 = np.arctan2(Xd, np.sign(Zd) * np.sqrt(Yd ** 2 + Zd ** 2)) + alpha
+#     print((time.process_time_ns() - start) / 1e6)
+#     print(theta4_2, theta5_2, theta6)
+#     -1.2971196567286807 0.6763627196264181 0.0
+#     [
+#         -0.0,
+#         -2.220446049250313e-16,
+#         0.6207569371022629,
+#         -1.2971196567286807,
+#         0.6763627196264181,
+#         0.0,
+#     ]
+
+
+# H34 = Transformation(dh=[d4, 0,0, theta4])
+# H45 = Transformation(dh=[0, -np.pi /2, 0, theta5])
+# H56 = Transformation(dh=[0,0,0, theta6])
+# H362 = np.matmul(H34, np.matmul(H45, H56))
+# print(H36, H362)
+# H03_2 = np.matmul(transformation, scipy.linalg.inv(H362))
+#
+# angles_2 = Transformation(rotation_matrix=H03_2[0:3, 0:3]).orientation_euler
+#
+# theta1_2 = -angles_2[2] + np.pi / 2
+# theta2_2 = angles_2[0]
+# theta3_2 = -angles_2[1]
+# print(theta1_2, theta2_2, theta3_2)
