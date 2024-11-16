2034f139dff1243430abe2d9b6a3932f736662ec
Yifei Zhou <75226267+yf-zhou@users.noreply.github.com>
2024-11-16 17:46:50 -0500

    891 vis walking metrics (#902)
    * visualize imu and position
    
    * small changes
    
    * small changes
    
    * reshape footprint rectangles
    
    * clean up pt 3???
    
    * remove if statements - graph everything

--- soccer_control/soccer_pycontrol/src/soccer_pycontrol/model/sensors.py
+++ soccer_control/soccer_pycontrol/src/soccer_pycontrol/model/sensors.py
@@ -19,13 +19,13 @@ class Sensors:
         self.imu_ready = False
         self.get_imu()  # to init
 
-    def get_pose(self) -> Transformation:
+    def get_pose(self, link=None) -> Transformation:
         """
         Get the 3D pose of the robot
 
         :return: The 3D pose of the robot
         """
-        [position, quaternion] = pb.getLinkState(self.body, linkIndex=self.imu_link)[4:6]  # TODO double check
+        [position, quaternion] = pb.getLinkState(self.body, linkIndex=self.imu_link if not link else link)[4:6]  # TODO double check
         return Transformation(position=position, quaternion=quaternion)
 
     def get_imu(self) -> Transformation:
